8e9f8b304413ddfd8803227790b0bf69115ee239
Hiroyuki Yamada <mogwaing@gmail.com>
2019-07-11 11:30:41 +0900

    add documentation for the start methods

--- src/main/java/com/scalar/database/api/DistributedTransactionManager.java
+++ src/main/java/com/scalar/database/api/DistributedTransactionManager.java
@@ -4,12 +4,39 @@ public interface DistributedTransactionManager {
 
   void with(String namespace, String tableName);
 
+  /**
+   * Starts a new transaction.
+   *
+   * @return {@link DistributedTransaction}
+   */
   DistributedTransaction start();
 
+  /**
+   * Starts a new transaction with the specified transaction ID. It is a users' responsibility to
+   * guarantee uniqueness of the ID so it is not recommended to use this method unless you know
+   * exactly what you are doing.
+   *
+   * @param txId
+   * @return {@link DistributedTransaction}
+   */
   DistributedTransaction start(String txId);
 
+  /**
+   * Starts a new transaction with the specified {@link Isolation} level.
+   *
+   * @param isolation
+   * @return {@link DistributedTransaction}
+   */
   DistributedTransaction start(Isolation isolation);
 
+  /**
+   * Starts a new transaction with the specified transaction ID and {@link Isolation} level. It is a
+   * users' responsibility to guarantee uniqueness of the ID so it is not recommended to use this
+   * method unless you know exactly what you are doing.
+   *
+   * @param isolation
+   * @return {@link DistributedTransaction}
+   */
   DistributedTransaction start(String txId, Isolation isolation);
 
   /**
