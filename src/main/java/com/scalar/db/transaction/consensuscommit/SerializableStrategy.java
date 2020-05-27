38fa86478d672d3cdda022ebe84f1d752c3c9b3c
Hiroyuki Yamada <mogwaing@gmail.com>
2020-05-27 14:28:34 +0900

    remove extra >

--- src/main/java/com/scalar/db/transaction/consensuscommit/SerializableStrategy.java
+++ src/main/java/com/scalar/db/transaction/consensuscommit/SerializableStrategy.java
@@ -8,7 +8,7 @@ package com.scalar.db.transaction.consensuscommit;
  * <p>Extra-write strategy converts all read set into write set when preparing records to remove
  * anti-dependencies.
  *
- * <p>>Extra-read strategy checks all read set after preparing records and before committing a state
+ * <p>Extra-read strategy checks all read set after preparing records and before committing a state
  * to actually find out if there is an anti-dependency.
  */
 public enum SerializableStrategy implements com.scalar.db.api.SerializableStrategy {
