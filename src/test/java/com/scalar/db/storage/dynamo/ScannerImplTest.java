2355a0d78009695812565adf8d60da526e8324ad
yito88 <yuji@phact-columba.com>
2020-10-16 17:50:28 +0900

    add ItemSorter

--- src/test/java/com/scalar/db/storage/dynamo/ScannerImplTest.java
+++ src/test/java/com/scalar/db/storage/dynamo/ScannerImplTest.java
@@ -2,7 +2,6 @@ package com.scalar.db.storage.dynamo;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.scalar.db.api.Result;
 import com.scalar.db.api.Scan;
 import com.scalar.db.io.IntValue;
 import com.scalar.db.io.Key;
@@ -11,7 +10,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
@@ -24,24 +22,38 @@ public class ScannerImplTest {
   private static final String ANY_TEXT_2 = "text2";
   private static final int ANY_LARGE_INT = 1000;
   private static final int ANY_SMALL_INT = 1;
-  private static final String ANY_LARGE_TEXT = "sssssss";
   private static final String ANY_SMALL_TEXT = "aa";
   private static final String ANY_COLUMN_NAME_1 = "val1";
 
-  private TableMetadata metadata;
-
   @Before
-  public void setUp() throws Exception {
+  public void setUp() throws Exception {}
+
+  private TableMetadata prepareMetadataForSingleClusteringKey() {
+    Map<String, AttributeValue> metadataMap = new HashMap<>();
+    metadataMap.put("partitionKey", AttributeValue.builder().ss(ANY_NAME_1).build());
+    metadataMap.put("clusteringKey", AttributeValue.builder().ss(ANY_NAME_2).build());
+    metadataMap.put("columns", AttributeValue.builder().m(prepareColumns()).build());
+
+    return new TableMetadata(metadataMap);
+  }
+
+  private TableMetadata prepareMetadataForMultipleClusteringKeys() {
     Map<String, AttributeValue> metadataMap = new HashMap<>();
     metadataMap.put("partitionKey", AttributeValue.builder().ss(ANY_NAME_1).build());
     metadataMap.put("clusteringKey", AttributeValue.builder().ss(ANY_NAME_2, ANY_NAME_3).build());
+    metadataMap.put("columns", AttributeValue.builder().m(prepareColumns()).build());
+
+    return new TableMetadata(metadataMap);
+  }
+
+  private Map<String, AttributeValue> prepareColumns() {
     Map<String, AttributeValue> columns = new HashMap<>();
     columns.put(ANY_NAME_1, AttributeValue.builder().s("text").build());
     columns.put(ANY_NAME_2, AttributeValue.builder().s("int").build());
     columns.put(ANY_NAME_3, AttributeValue.builder().s("text").build());
     columns.put(ANY_COLUMN_NAME_1, AttributeValue.builder().s("text").build());
-    metadataMap.put("columns", AttributeValue.builder().m(columns).build());
-    metadata = new TableMetadata(metadataMap);
+
+    return columns;
   }
 
   private Map<String, AttributeValue> prepareItem() {
@@ -57,110 +69,44 @@ public class ScannerImplTest {
   }
 
   @Test
-  public void one_MultipleItemsGivenWithoutOrderingAndLimit_ShouldReturnSmallResult() {
+  public void constructor_ItemsWithSingleClusteringKeyGiven_ShouldNotSortItems() {
     // Arrange
+    TableMetadata metadata = prepareMetadataForSingleClusteringKey();
     Map<String, AttributeValue> item1 = prepareItem();
+    item1.put(ANY_NAME_2, AttributeValue.builder().n(String.valueOf(ANY_LARGE_INT)).build());
     Map<String, AttributeValue> item2 = prepareItem();
-    item2.put(ANY_NAME_2, AttributeValue.builder().n(String.valueOf(ANY_LARGE_INT)).build());
     Scan scan = new Scan(new Key(new TextValue(ANY_NAME_1, ANY_TEXT_1)));
     List<Map<String, AttributeValue>> items = new ArrayList<>();
     items.add(item1);
     items.add(item2);
-    ScannerImpl scanner = new ScannerImpl(items, scan, metadata);
 
     // Act
-    Optional<Result> actual = scanner.one();
+    ScannerImpl actual = new ScannerImpl(items, scan, metadata);
 
     // Assert
-    assertThat(actual.get().getValue(ANY_NAME_2).get())
-        .isEqualTo(new IntValue(ANY_NAME_2, ANY_SMALL_INT));
-  }
-
-  @Test
-  public void one_MultipleItemsGivenWithDescOrdering_ShouldReturnLargeResult() {
-    // Arrange
-    Map<String, AttributeValue> item1 = prepareItem();
-    Map<String, AttributeValue> item2 = prepareItem();
-    item2.put(ANY_NAME_2, AttributeValue.builder().n(String.valueOf(ANY_LARGE_INT)).build());
-    List<Map<String, AttributeValue>> items = new ArrayList<>();
-    items.add(item1);
-    items.add(item2);
-    Scan scan =
-        new Scan(new Key(new TextValue(ANY_NAME_1, ANY_TEXT_1)))
-            .withOrdering(new Scan.Ordering(ANY_NAME_2, Scan.Ordering.Order.DESC));
-
-    ScannerImpl scanner = new ScannerImpl(items, scan, metadata);
-
-    // Act
-    Optional<Result> actual = scanner.one();
-
-    // Assert
-    assertThat(actual.get().getValue(ANY_NAME_2).get())
+    assertThat(actual.one().get().getValue(ANY_NAME_2).get())
         .isEqualTo(new IntValue(ANY_NAME_2, ANY_LARGE_INT));
   }
 
   @Test
-  public void all_MultipleItemsGivenWithoutOrderingAndLimit_ShouldReturnAllResults() {
+  public void constructor_ItemsWithMultipleClusteringKeysGiven_ShouldSortItems() {
     // Arrange
+    TableMetadata metadata = prepareMetadataForMultipleClusteringKeys();
     Map<String, AttributeValue> item1 = prepareItem();
     Map<String, AttributeValue> item2 = prepareItem();
     item2.put(ANY_NAME_2, AttributeValue.builder().n(String.valueOf(ANY_LARGE_INT)).build());
-    List<Map<String, AttributeValue>> items = new ArrayList<>();
-    items.add(item1);
-    items.add(item2);
-    Scan scan = new Scan(new Key(new TextValue(ANY_NAME_1, ANY_TEXT_1)));
-
-    ScannerImpl scanner = new ScannerImpl(items, scan, metadata);
-
-    // Act
-    List<Result> actual = scanner.all();
-
-    // Assert
-    assertThat(actual.size()).isEqualTo(2);
-  }
-
-  @Test
-  public void all_MultipleItemsGivenWithMultipleOrdering_ShouldReturnLargeResult() {
-    // Arrange
-    Map<String, AttributeValue> item1 = prepareItem();
-    Map<String, AttributeValue> item2 = prepareItem();
-    item2.put(ANY_NAME_3, AttributeValue.builder().s(String.valueOf(ANY_LARGE_TEXT)).build());
-    List<Map<String, AttributeValue>> items = new ArrayList<>();
-    items.add(item1);
-    items.add(item2);
     Scan scan =
         new Scan(new Key(new TextValue(ANY_NAME_1, ANY_TEXT_1)))
-            .withOrdering(new Scan.Ordering(ANY_NAME_2, Scan.Ordering.Order.DESC))
-            .withOrdering(new Scan.Ordering(ANY_NAME_3, Scan.Ordering.Order.DESC));
-
-    ScannerImpl scanner = new ScannerImpl(items, scan, metadata);
-
-    // Act
-    List<Result> actual = scanner.all();
-
-    // Assert
-    assertThat(actual.size()).isEqualTo(2);
-    assertThat(actual.get(0).getValue(ANY_NAME_3).get())
-        .isEqualTo(new TextValue(ANY_NAME_3, ANY_LARGE_TEXT));
-  }
-
-  @Test
-  public void all_MultipleItemsGivenWithLimit_ShouldReturnLargeResult() {
-    // Arrange
-    Map<String, AttributeValue> item1 = prepareItem();
-    Map<String, AttributeValue> item2 = prepareItem();
-    item2.put(ANY_NAME_2, AttributeValue.builder().n(String.valueOf(ANY_LARGE_INT)).build());
+            .withOrdering(new Scan.Ordering(ANY_NAME_2, Scan.Ordering.Order.DESC));
     List<Map<String, AttributeValue>> items = new ArrayList<>();
     items.add(item1);
     items.add(item2);
-    Scan scan = new Scan(new Key(new TextValue(ANY_NAME_1, ANY_TEXT_1))).withLimit(1);
-
-    ScannerImpl scanner = new ScannerImpl(items, scan, metadata);
 
     // Act
-    List<Result> actual = scanner.all();
+    ScannerImpl actual = new ScannerImpl(items, scan, metadata);
 
     // Assert
-    assertThat(actual.size()).isEqualTo(1);
+    assertThat(actual.one().get().getValue(ANY_NAME_2).get())
+        .isEqualTo(new IntValue(ANY_NAME_2, ANY_LARGE_INT));
   }
 }
