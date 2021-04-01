bb9f04164b738446d94383c7b17ecbd69edccd75
Toshihiro Suzuki <brfrn169@gmail.com>
2021-04-01 09:39:02 +0900

    Refactor some configuration names (#179)

--- src/main/java/com/scalar/db/storage/jdbc/JdbcDatabaseConfig.java
+++ src/main/java/com/scalar/db/storage/jdbc/JdbcDatabaseConfig.java
@@ -15,16 +15,16 @@ public class JdbcDatabaseConfig extends DatabaseConfig {
   private static final Logger LOGGER = LoggerFactory.getLogger(JdbcDatabaseConfig.class);
 
   public static final String PREFIX = DatabaseConfig.PREFIX + "jdbc.";
-  public static final String CONNECTION_POOL_MIN_IDLE = PREFIX + "connection.pool.min_idle";
-  public static final String CONNECTION_POOL_MAX_IDLE = PREFIX + "connection.pool.max_idle";
-  public static final String CONNECTION_POOL_MAX_TOTAL = PREFIX + "connection.pool.max_total";
+  public static final String CONNECTION_POOL_MIN_IDLE = PREFIX + "connection_pool.min_idle";
+  public static final String CONNECTION_POOL_MAX_IDLE = PREFIX + "connection_pool.max_idle";
+  public static final String CONNECTION_POOL_MAX_TOTAL = PREFIX + "connection_pool.max_total";
   public static final String PREPARED_STATEMENTS_POOL_ENABLED =
-      PREFIX + "prepared_statements.pool.enabled";
+      PREFIX + "prepared_statements_pool.enabled";
   public static final String PREPARED_STATEMENTS_POOL_MAX_OPEN =
-      PREFIX + "prepared_statements.pool.max_open";
+      PREFIX + "prepared_statements_pool.max_open";
 
-  public static final String TRANSACTION_MANAGER_TYPE = PREFIX + "transaction_manager.type";
-  public static final String TRANSACTION_MANAGER_TYPE_CONSENSUSCOMMIT = "consensuscommit";
+  public static final String TRANSACTION_MANAGER_TYPE = PREFIX + "transaction_manager_type";
+  public static final String TRANSACTION_MANAGER_TYPE_CONSENSUS_COMMIT = "consensus-commit";
   public static final String TRANSACTION_MANAGER_TYPE_JDBC = "jdbc";
 
   public static final int DEFAULT_CONNECTION_POOL_MIN_IDLE = 5;
@@ -33,7 +33,7 @@ public class JdbcDatabaseConfig extends DatabaseConfig {
   public static final boolean DEFAULT_PREPARED_STATEMENTS_POOL_ENABLED = false;
   public static final int DEFAULT_PREPARED_STATEMENTS_POOL_MAX_OPEN = -1;
   public static final String DEFAULT_TRANSACTION_MANAGER_TYPE =
-      TRANSACTION_MANAGER_TYPE_CONSENSUSCOMMIT;
+      TRANSACTION_MANAGER_TYPE_CONSENSUS_COMMIT;
 
   private int connectionPoolMinIdle;
   private int connectionPoolMaxIdle;
@@ -73,7 +73,7 @@ public class JdbcDatabaseConfig extends DatabaseConfig {
 
     transactionManagerType =
         getProperties().getProperty(TRANSACTION_MANAGER_TYPE, DEFAULT_TRANSACTION_MANAGER_TYPE);
-    if (!transactionManagerType.equals(TRANSACTION_MANAGER_TYPE_CONSENSUSCOMMIT)
+    if (!transactionManagerType.equals(TRANSACTION_MANAGER_TYPE_CONSENSUS_COMMIT)
         && !transactionManagerType.equals(TRANSACTION_MANAGER_TYPE_JDBC)) {
       LOGGER.warn(
           "the specified value of '{}' is invalid. using the default value: {}",
