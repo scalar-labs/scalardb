17cbc2eb460008aa6f4688b59f46f91b5b5262ed
Toshihiro Suzuki <brfrn169@gmail.com>
2021-07-15 10:46:35 +0900

    Support Prometheus in Scalar DB Server

--- server/src/main/java/com/scalar/db/server/service/ServerModule.java
+++ server/src/main/java/com/scalar/db/server/service/ServerModule.java
@@ -15,8 +15,18 @@ import com.scalar.db.server.Metrics;
 import com.scalar.db.server.Pauser;
 import com.scalar.db.service.StorageModule;
 import com.scalar.db.service.TransactionModule;
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.dropwizard.DropwizardExports;
+import io.prometheus.client.exporter.MetricsServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ServerModule extends AbstractModule {
+  private static final Logger LOGGER = LoggerFactory.getLogger(ServerModule.class);
+
   private final Injector storageInjector;
   private final Injector transactionInjector;
 
@@ -53,12 +63,30 @@ public class ServerModule extends AbstractModule {
   @Singleton
   Metrics provideMetrics() {
     MetricRegistry metricRegistry = new MetricRegistry();
+    startJmxReporter(metricRegistry);
+    startPrometheusHttpEndpoint(metricRegistry);
+    return new Metrics(metricRegistry);
+  }
 
-    // start JMX reporter
+  private void startJmxReporter(MetricRegistry metricRegistry) {
     JmxReporter reporter = JmxReporter.forRegistry(metricRegistry).build();
     reporter.start();
     Runtime.getRuntime().addShutdownHook(new Thread(reporter::stop));
+  }
 
-    return new Metrics(metricRegistry);
+  private void startPrometheusHttpEndpoint(MetricRegistry metricRegistry) {
+    CollectorRegistry.defaultRegistry.register(new DropwizardExports(metricRegistry));
+
+    Server server = new Server(8080);
+    ServletContextHandler context = new ServletContextHandler();
+    context.setContextPath("/");
+    server.setHandler(context);
+    context.addServlet(new ServletHolder(new MetricsServlet()), "/metrics");
+    server.setStopAtShutdown(true);
+    try {
+      server.start();
+    } catch (Exception e) {
+      LOGGER.error("failed to start Jetty server", e);
+    }
   }
 }
