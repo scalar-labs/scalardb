2b15a58532edcff25943e7f67ff2d82abdf9d30a
Toshihiro Suzuki <brfrn169@gmail.com>
2021-08-02 20:51:06 +0900

    Disable Prometheus HTTP endpoint when the port is specified to zero in Scalar DB server (#260)

--- server/src/integration-test/java/com/scalar/db/server/DistributedStorageServiceIntegrationTest.java
+++ server/src/integration-test/java/com/scalar/db/server/DistributedStorageServiceIntegrationTest.java
@@ -13,6 +13,7 @@ import com.scalar.db.api.TableMetadata;
 import com.scalar.db.config.DatabaseConfig;
 import com.scalar.db.io.DataType;
 import com.scalar.db.io.Key;
+import com.scalar.db.server.config.ServerConfig;
 import com.scalar.db.storage.IntegrationTestBase;
 import com.scalar.db.storage.jdbc.test.TestEnv;
 import com.scalar.db.storage.rpc.GrpcConfig;
@@ -116,7 +117,9 @@ public class DistributedStorageServiceIntegrationTest extends IntegrationTestBas
     testEnv.createTables();
     testEnv.insertMetadata();
 
-    server = new ScalarDbServer(testEnv.getJdbcConfig().getProperties());
+    Properties serverProperties = new Properties(testEnv.getJdbcConfig().getProperties());
+    serverProperties.setProperty(ServerConfig.PROMETHEUS_HTTP_ENDPOINT_PORT, "0");
+    server = new ScalarDbServer(serverProperties);
     server.start();
 
     Properties properties = new Properties();
