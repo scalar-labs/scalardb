a4b425647b916421a9172d3d88a289d9b96ddf68
Toshihiro Suzuki <brfrn169@gmail.com>
2021-08-26 14:12:16 +0900

    Update the Getting Started (#283)

--- docs/getting-started/src/main/java/sample/ElectronicMoneyWithTransaction.java
+++ docs/getting-started/src/main/java/sample/ElectronicMoneyWithTransaction.java
@@ -1,48 +1,44 @@
 package sample;
 
-import com.google.inject.Guice;
-import com.google.inject.Injector;
 import com.scalar.db.api.DistributedTransaction;
+import com.scalar.db.api.DistributedTransactionManager;
 import com.scalar.db.api.Get;
 import com.scalar.db.api.Put;
 import com.scalar.db.api.Result;
 import com.scalar.db.exception.transaction.TransactionException;
-import com.scalar.db.io.IntValue;
 import com.scalar.db.io.Key;
-import com.scalar.db.io.TextValue;
-import com.scalar.db.service.TransactionModule;
-import com.scalar.db.service.TransactionService;
-
+import com.scalar.db.service.TransactionFactory;
 import java.io.IOException;
 import java.util.Optional;
 
 public class ElectronicMoneyWithTransaction extends ElectronicMoney {
-  private final TransactionService service;
+
+  private final DistributedTransactionManager manager;
 
   public ElectronicMoneyWithTransaction() throws IOException {
-    Injector injector = Guice.createInjector(new TransactionModule(dbConfig));
-    service = injector.getInstance(TransactionService.class);
-    service.with(NAMESPACE, TABLENAME);
+    TransactionFactory factory = new TransactionFactory(dbConfig);
+    manager = factory.getTransactionManager();
+    manager.with(NAMESPACE, TABLENAME);
   }
 
   @Override
   public void charge(String id, int amount) throws TransactionException {
     // Start a transaction
-    DistributedTransaction tx = service.start();
+    DistributedTransaction tx = manager.start();
 
     // Retrieve the current balance for id
-    Get get = new Get(new Key(new TextValue(ID, id)));
+    Get get = new Get(new Key(ID, id));
     Optional<Result> result = tx.get(get);
 
     // Calculate the balance
     int balance = amount;
     if (result.isPresent()) {
-      int current = ((IntValue) result.get().getValue(BALANCE).get()).get();
+      int current = result.get().getValue(BALANCE).get().getAsInt();
       balance += current;
     }
 
     // Update the balance
-    Put put = new Put(new Key(new TextValue(ID, id))).withValue(new IntValue(BALANCE, balance));
+    Put put = new Put(new Key(ID, id)).withValue(BALANCE, balance);
     tx.put(put);
 
     // Commit the transaction (records are automatically recovered in case of failure)
@@ -52,27 +48,24 @@ public class ElectronicMoneyWithTransaction extends ElectronicMoney {
   @Override
   public void pay(String fromId, String toId, int amount) throws TransactionException {
     // Start a transaction
-    DistributedTransaction tx = service.start();
+    DistributedTransaction tx = manager.start();
 
     // Retrieve the current balances for ids
-    Get fromGet = new Get(new Key(new TextValue(ID, fromId)));
-    Get toGet = new Get(new Key(new TextValue(ID, toId)));
+    Get fromGet = new Get(new Key(ID, fromId));
+    Get toGet = new Get(new Key(ID, toId));
     Optional<Result> fromResult = tx.get(fromGet);
     Optional<Result> toResult = tx.get(toGet);
 
     // Calculate the balances (it assumes that both accounts exist)
-    int newFromBalance = ((IntValue) (fromResult.get().getValue(BALANCE).get())).get() - amount;
-    int newToBalance = ((IntValue) (toResult.get().getValue(BALANCE).get())).get() + amount;
+    int newFromBalance = fromResult.get().getValue(BALANCE).get().getAsInt() - amount;
+    int newToBalance = toResult.get().getValue(BALANCE).get().getAsInt() + amount;
     if (newFromBalance < 0) {
       throw new RuntimeException(fromId + " doesn't have enough balance.");
     }
 
     // Update the balances
-    Put fromPut =
-        new Put(new Key(new TextValue(ID, fromId)))
-            .withValue(new IntValue(BALANCE, newFromBalance));
-    Put toPut =
-        new Put(new Key(new TextValue(ID, toId))).withValue(new IntValue(BALANCE, newToBalance));
+    Put fromPut = new Put(new Key(ID, fromId)).withValue(BALANCE, newFromBalance);
+    Put toPut = new Put(new Key(ID, toId)).withValue(BALANCE, newToBalance);
     tx.put(fromPut);
     tx.put(toPut);
 
@@ -82,6 +75,6 @@ public class ElectronicMoneyWithTransaction extends ElectronicMoney {
 
   @Override
   public void close() {
-    service.close();
+    manager.close();
   }
 }
