624270f9a2ebd5954185846b5e216919ca7c69c9
Vincent Guilpain <vincent.guilpain@scalar-labs.com>
2021-08-30 18:23:02 +0900

    Finalize Cosmos DB Admin implementation (#290)

--- core/src/integration-test/java/com/scalar/db/storage/cosmos/CosmosIntegrationTest.java
+++ core/src/integration-test/java/com/scalar/db/storage/cosmos/CosmosIntegrationTest.java
@@ -1,125 +1,33 @@
 package com.scalar.db.storage.cosmos;
 
-import com.azure.cosmos.CosmosClient;
-import com.azure.cosmos.CosmosClientBuilder;
-import com.azure.cosmos.CosmosDatabase;
-import com.azure.cosmos.models.CosmosContainerProperties;
-import com.azure.cosmos.models.CosmosItemRequestOptions;
-import com.azure.cosmos.models.CosmosQueryRequestOptions;
-import com.azure.cosmos.models.CosmosStoredProcedureProperties;
-import com.azure.cosmos.models.CosmosStoredProcedureRequestOptions;
-import com.azure.cosmos.models.ThroughputProperties;
-import com.azure.cosmos.util.CosmosPagedIterable;
+import com.google.common.collect.ImmutableMap;
 import com.scalar.db.api.DistributedStorage;
 import com.scalar.db.config.DatabaseConfig;
+import com.scalar.db.exception.storage.ExecutionException;
 import com.scalar.db.storage.IntegrationTestBase;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
 import java.util.Optional;
 import java.util.Properties;
-import java.util.stream.Stream;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 
 public class CosmosIntegrationTest extends IntegrationTestBase {
+  private static final String PROP_COSMOSDB_URI = "scalardb.cosmos.uri";
+  private static final String PROP_COSMOSDB_USERNAME = "scalardb.cosmos.username";
+  private static final String PROP_COSMOSDB_PASSWORD = "scalardb.cosmos.password";
+  private static final String PROP_NAMESPACE_PREFIX = "scalardb.namespace_prefix";
 
-  private static final String METADATA_DATABASE = "scalardb";
-  private static final String METADATA_CONTAINER = "metadata";
-  private static final String STORED_PROCEDURE_PATH =
-      "tools/scalar-schema/stored_procedure/mutate.js";
-  private static final String PARTITION_KEY = "/concatenatedPartitionKey";
-
-  private static Optional<String> namespacePrefix;
-  private static CosmosClient client;
   private static DistributedStorage storage;
 
-  @Before
-  public void setUp() {
-    storage.with(NAMESPACE, TABLE);
-    setUp(storage);
-  }
-
-  @After
-  public void tearDown() {
-    // delete all the data of the container
-    CosmosPagedIterable<Record> records =
-        client
-            .getDatabase(database(NAMESPACE))
-            .getContainer(TABLE)
-            .queryItems("SELECT * FROM Record", new CosmosQueryRequestOptions(), Record.class);
-    for (Record record : records) {
-      client
-          .getDatabase(database(NAMESPACE))
-          .getContainer(TABLE)
-          .deleteItem(record, new CosmosItemRequestOptions());
-    }
-  }
-
   @BeforeClass
-  public static void setUpBeforeClass() throws IOException {
-    String contactPoint = System.getProperty("scalardb.cosmos.uri");
-    String username = System.getProperty("scalardb.cosmos.username");
-    String password = System.getProperty("scalardb.cosmos.password");
-    namespacePrefix = Optional.ofNullable(System.getProperty("scalardb.namespace_prefix"));
-
-    client =
-        new CosmosClientBuilder().endpoint(contactPoint).key(password).directMode().buildClient();
-
-    ThroughputProperties autoscaledThroughput =
-        ThroughputProperties.createAutoscaledThroughput(4000);
-
-    // create the metadata database and container
-    client.createDatabaseIfNotExists(database(METADATA_DATABASE), autoscaledThroughput);
-    CosmosContainerProperties containerProperties =
-        new CosmosContainerProperties(METADATA_CONTAINER, "/id");
-    client.getDatabase(database(METADATA_DATABASE)).createContainerIfNotExists(containerProperties);
-
-    // insert metadata
-    CosmosTableMetadata metadata = new CosmosTableMetadata();
-    metadata.setId(table(NAMESPACE, TABLE));
-    metadata.setPartitionKeyNames(Collections.singletonList(COL_NAME1));
-    metadata.setClusteringKeyNames(Collections.singletonList(COL_NAME4));
-    metadata.setSecondaryIndexNames(new HashSet<>(Collections.singletonList(COL_NAME3)));
-    Map<String, String> columns = new HashMap<>();
-    columns.put(COL_NAME1, "int");
-    columns.put(COL_NAME2, "text");
-    columns.put(COL_NAME3, "int");
-    columns.put(COL_NAME4, "int");
-    columns.put(COL_NAME5, "boolean");
-    metadata.setColumns(columns);
-    client
-        .getDatabase(database(METADATA_DATABASE))
-        .getContainer(METADATA_CONTAINER)
-        .createItem(metadata);
-
-    // create the user database
-    client.createDatabaseIfNotExists(database(NAMESPACE), autoscaledThroughput);
-
-    // create the user container
-    containerProperties = new CosmosContainerProperties(TABLE, PARTITION_KEY);
-    client.getDatabase(database(NAMESPACE)).createContainerIfNotExists(containerProperties);
-
-    String storedProcedure;
-    try (Stream<String> lines =
-        Files.lines(Paths.get(STORED_PROCEDURE_PATH), StandardCharsets.UTF_8)) {
-      storedProcedure =
-          lines.reduce("", (prev, cur) -> prev + cur + System.getProperty("line.separator"));
-    }
-    CosmosStoredProcedureProperties properties =
-        new CosmosStoredProcedureProperties("mutate.js", storedProcedure);
-    client
-        .getDatabase(database(NAMESPACE))
-        .getContainer(TABLE)
-        .getScripts()
-        .createStoredProcedure(properties, new CosmosStoredProcedureRequestOptions());
+  public static void setUpBeforeClass() throws IOException, ExecutionException {
+    String contactPoint = System.getProperty(PROP_COSMOSDB_URI);
+    String username = System.getProperty(PROP_COSMOSDB_USERNAME);
+    String password = System.getProperty(PROP_COSMOSDB_PASSWORD);
+    Optional<String> namespacePrefix =
+        Optional.ofNullable(System.getProperty(PROP_NAMESPACE_PREFIX));
 
     // reuse this storage instance through the tests
     Properties props = new Properties();
@@ -127,31 +35,28 @@ public class CosmosIntegrationTest extends IntegrationTestBase {
     props.setProperty(DatabaseConfig.USERNAME, username);
     props.setProperty(DatabaseConfig.PASSWORD, password);
     namespacePrefix.ifPresent(n -> props.setProperty(DatabaseConfig.NAMESPACE_PREFIX, n));
-    storage = new Cosmos(new DatabaseConfig(props));
+
+    DatabaseConfig config = new DatabaseConfig(props);
+    admin = new CosmosAdmin(config);
+    storage = new Cosmos(config);
+    createTable(ImmutableMap.of(CosmosAdmin.RU, "4000"));
   }
 
   @AfterClass
-  public static void tearDownAfterClass() {
-    CosmosDatabase database = client.getDatabase(database(METADATA_DATABASE));
-    database.getContainer(METADATA_CONTAINER).delete();
-    database.delete();
-
-    client.getDatabase(database(NAMESPACE)).delete();
-
-    client.close();
-
+  public static void tearDownAfterClass() throws ExecutionException {
+    deleteTable();
+    admin.close();
     storage.close();
   }
 
-  private static String namespacePrefix() {
-    return namespacePrefix.map(n -> n + "_").orElse("");
+  @Before
+  public void setUp() throws Exception {
+    storage.with(NAMESPACE, TABLE);
+    setUp(storage);
   }
 
-  private static String database(String database) {
-    return namespacePrefix() + database;
-  }
-
-  private static String table(String database, String table) {
-    return database(database) + "." + table;
+  @After
+  public void tearDown() throws ExecutionException {
+    deleteData();
   }
 }
---
+++ core/src/main/resources/cosmosdb_stored_procedure/mutate.js
@@ -0,0 +1,110 @@
+function mutate(num, ...args) {
+  let mutations = args.slice(0, num);
+  let baseRecords = args.slice(num, num * 2);
+  let queries = args.slice(num * 2);
+
+  const MUTATION = {
+    PUT: 0,
+    PUT_IF_NOT_EXISTS: 1,
+    PUT_IF: 2,
+    DELETE_IF: 3,
+  };
+  const ERROR_CODE = {
+    PRECONDITION_FAILED: 412,
+  };
+
+  function merge(source, update) {
+    for (var column in update) {
+      if (update[column].constructor == Object) {
+        source[column] = merge(source[column], update[column]);
+      } else {
+        source[column] = update[column];
+      }
+    }
+
+    return source;
+  }
+
+  function mutateWithQuery(mutation, record, query) {
+    return new Promise((resolve, reject) => {
+      const isAccepted = __.queryDocuments(__.getSelfLink(), query,
+        (error, reads, options) => {
+          if (error) {
+            reject(error);
+            return;
+          }
+
+          const records = checkQueryResult(mutation, record, reads);
+          if (!records.length) {
+            reject(new Error(ERROR_CODE.PRECONDITION_FAILED, "no mutation"));
+            return;
+          }
+
+          if (mutation == MUTATION.DELETE_IF) {
+            for (let i = 0; i < records.length; i++) {
+              const isMutationAccepted = __.deleteDocument(records[i]._self,
+                (error, result, options) => {
+                  if (error) {
+                    reject(error);
+                  } else {
+                    resolve(result);
+                  }
+                });
+              if (!isMutationAccepted) {
+                reject(new Error("The query was not accepted by the server."));
+              }
+            }
+          } else {
+            const isMutationAccepted = __.upsertDocument(__.getSelfLink(), records[0],
+              (error, result, options) => {
+                if (error) {
+                  reject(error);
+                } else {
+                  resolve(result);
+                }
+              });
+            if (!isMutationAccepted) {
+              reject(new Error("The query was not accepted by the server."));
+            }
+          }
+        });
+      if (!isAccepted) reject(new Error("The query was not accepted by the server."));
+    });
+  }
+
+  function checkQueryResult(mutation, record, reads) {
+    if (!reads || !reads.length) {
+      // The specified record does not exist
+      if (mutation == MUTATION.PUT || mutation == MUTATION.PUT_IF_NOT_EXISTS) {
+        return [record];
+      } else {
+        // For other conditional mutations, the condition isn't satisfied
+        return [];
+      }
+    }
+
+    if (mutation == MUTATION.PUT_IF_NOT_EXISTS) {
+      return [];
+    } else if (mutation == MUTATION.PUT || mutation == MUTATION.PUT_IF) {
+      if (reads.length != 1) {
+        throw new Error("Unable to update multiple records.");
+      } else {
+        return [merge(reads[0], record)];
+      }
+    } else {
+      // DELETE_IF
+      return reads;
+    }
+  }
+
+  let promises = [];
+  for (let i = 0; i < num; i++) {
+    let mutation = mutations[i];
+    let record = baseRecords[i];
+    let query = queries[i];
+
+    promises.push(mutateWithQuery(mutation, record, query));
+  }
+
+  Promise.all(promises).catch(error => getContext().abort(error));
+}
