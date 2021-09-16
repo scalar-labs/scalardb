09626dc944ad7fc9610f4cf91a31078311250b7c
Toshihiro Suzuki <brfrn169@gmail.com>
2021-09-16 13:48:23 +0900

    Add Javadoc for DistributedStorageAdmin (#309)

--- core/src/main/java/com/scalar/db/api/DistributedStorageAdmin.java
+++ core/src/main/java/com/scalar/db/api/DistributedStorageAdmin.java
@@ -5,7 +5,41 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 
-/** An administrative interface for distributed storage implementations. */
+/**
+ * An administrative interface for distributed storage implementations. The user can execute
+ * administrative operations with it like createNamespace/createTable/getTableMetadata.
+ *
+ * <h3>Usage Examples</h3>
+ *
+ * Here is a simple example to demonstrate how to use it. (Exception handling is omitted for
+ * readability.)
+ *
+ * <pre>{@code
+ * StorageFactory factory = new StorageFactory(databaseConfig);
+ * DistributedStorageAdmin admin = factory.getAdmin();
+ *
+ * // Create a namespace
+ * admin.createNamespace(NAMESPACE);
+ *
+ * // Create a table
+ * TableMetadata tableMetadata = TableMetadata.newBuilder()
+ *     ...
+ *     .build();
+ * admin.createTable(NAMESPACE, TABLE, tableMetadata);
+ *
+ * // Get a table metadata
+ * tableMetadata = admin.getTableMetadata(NAMESPACE, TABLE);
+ *
+ * // Truncate a table
+ * admin.truncateTable(NAMESPACE, TABLE);
+ *
+ * // Drop a table
+ * admin.dropTable(NAMESPACE, TABLE);
+ *
+ * // Drop a namespace
+ * admin.dropNamespace(NAMESPACE);
+ * }</pre>
+ */
 public interface DistributedStorageAdmin {
 
   String INDEX_NAME_PREFIX = "index";
