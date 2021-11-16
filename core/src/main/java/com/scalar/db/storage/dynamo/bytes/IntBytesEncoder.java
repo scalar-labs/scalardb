d77527139942c9eca6c4492cbff30225d89d8c45
Toshihiro Suzuki <brfrn169@gmail.com>
2021-11-16 12:39:01 +0900

    Add KeyBytesEncoder that converts a key to bytes while preserving the sort order (#369)

---
+++ core/src/main/java/com/scalar/db/storage/dynamo/bytes/IntBytesEncoder.java
@@ -0,0 +1,26 @@
+package com.scalar.db.storage.dynamo.bytes;
+
+import static com.scalar.db.storage.dynamo.bytes.BytesUtils.mask;
+
+import com.scalar.db.api.Scan.Ordering.Order;
+import com.scalar.db.io.IntValue;
+import java.nio.ByteBuffer;
+
+public class IntBytesEncoder implements BytesEncoder<IntValue> {
+
+  IntBytesEncoder() {}
+
+  @Override
+  public int encodedLength(IntValue value, Order order) {
+    return 4;
+  }
+
+  @Override
+  public void encode(IntValue value, Order order, ByteBuffer dst) {
+    int v = value.getAsInt();
+    dst.put(mask((byte) ((v >> 24) ^ 0x80), order)); // Flip a sign bit to make it binary comparable
+    dst.put(mask((byte) (v >> 16), order));
+    dst.put(mask((byte) (v >> 8), order));
+    dst.put(mask((byte) v, order));
+  }
+}
---
+++ core/src/main/java/com/scalar/db/storage/dynamo/bytes/KeyBytesEncodedLengthCalculator.java
@@ -0,0 +1,76 @@
+package com.scalar.db.storage.dynamo.bytes;
+
+import com.scalar.db.api.Scan.Ordering.Order;
+import com.scalar.db.io.BigIntValue;
+import com.scalar.db.io.BlobValue;
+import com.scalar.db.io.BooleanValue;
+import com.scalar.db.io.DoubleValue;
+import com.scalar.db.io.FloatValue;
+import com.scalar.db.io.IntValue;
+import com.scalar.db.io.Key;
+import com.scalar.db.io.TextValue;
+import com.scalar.db.io.Value;
+import com.scalar.db.io.ValueVisitor;
+import java.util.Map;
+import javax.annotation.concurrent.NotThreadSafe;
+
+@NotThreadSafe
+public class KeyBytesEncodedLengthCalculator implements ValueVisitor {
+
+  private int length;
+  private Map<String, Order> keyOrders;
+
+  public int calculate(Key key, Map<String, Order> keyOrders) {
+    this.keyOrders = keyOrders;
+    for (Value<?> value : key) {
+      value.accept(this);
+    }
+    return length;
+  }
+
+  @Override
+  public void visit(BooleanValue value) {
+    length +=
+        BytesEncoders.BOOLEAN.encodedLength(
+            value, keyOrders.getOrDefault(value.getName(), Order.ASC));
+  }
+
+  @Override
+  public void visit(IntValue value) {
+    length +=
+        BytesEncoders.INT.encodedLength(value, keyOrders.getOrDefault(value.getName(), Order.ASC));
+  }
+
+  @Override
+  public void visit(BigIntValue value) {
+    length +=
+        BytesEncoders.BIGINT.encodedLength(
+            value, keyOrders.getOrDefault(value.getName(), Order.ASC));
+  }
+
+  @Override
+  public void visit(FloatValue value) {
+    length +=
+        BytesEncoders.FLOAT.encodedLength(
+            value, keyOrders.getOrDefault(value.getName(), Order.ASC));
+  }
+
+  @Override
+  public void visit(DoubleValue value) {
+    length +=
+        BytesEncoders.DOUBLE.encodedLength(
+            value, keyOrders.getOrDefault(value.getName(), Order.ASC));
+  }
+
+  @Override
+  public void visit(TextValue value) {
+    length +=
+        BytesEncoders.TEXT.encodedLength(value, keyOrders.getOrDefault(value.getName(), Order.ASC));
+  }
+
+  @Override
+  public void visit(BlobValue value) {
+    length +=
+        BytesEncoders.BLOB.encodedLength(value, keyOrders.getOrDefault(value.getName(), Order.ASC));
+  }
+}
