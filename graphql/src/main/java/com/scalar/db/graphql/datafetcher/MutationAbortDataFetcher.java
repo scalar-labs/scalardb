2e2a7cb5c681a05059864fd1ff91a531ff68521e
Yusuke Morimoto <yusuke.morimoto@scalar-labs.com>
2021-12-15 11:23:56 +0900

    Update MutationAbortDataFetcher

--- graphql/src/main/java/com/scalar/db/graphql/datafetcher/MutationAbortDataFetcher.java
+++ graphql/src/main/java/com/scalar/db/graphql/datafetcher/MutationAbortDataFetcher.java
@@ -1,21 +1,35 @@
 package com.scalar.db.graphql.datafetcher;
 
 import com.scalar.db.api.DistributedTransaction;
+import com.scalar.db.exception.transaction.AbortException;
 import com.scalar.db.graphql.schema.Constants;
 import graphql.GraphQLContext;
+import graphql.execution.AbortExecutionException;
+import graphql.execution.DataFetcherResult;
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
 
-public class MutationAbortDataFetcher implements DataFetcher<Boolean> {
+public class MutationAbortDataFetcher implements DataFetcher<DataFetcherResult<Boolean>> {
   @Override
-  public Boolean get(DataFetchingEnvironment environment) throws Exception {
+  public DataFetcherResult<Boolean> get(DataFetchingEnvironment environment) throws Exception {
     GraphQLContext graphQLContext = environment.getGraphQlContext();
     DistributedTransaction transaction = graphQLContext.get(Constants.CONTEXT_TRANSACTION_KEY);
     if (transaction == null) {
-      throw new IllegalStateException("Transaction not found.");
+      return DataFetcherResult.<Boolean>newResult()
+          .data(false)
+          .error(new AbortExecutionException("Transaction not found."))
+          .build();
     }
     graphQLContext.delete(Constants.CONTEXT_TRANSACTION_KEY);
-    transaction.abort();
-    return true;
+
+    DataFetcherResult.Builder<Boolean> result = DataFetcherResult.newResult();
+    try {
+      transaction.abort();
+      result.data(true);
+    } catch (AbortException e) {
+      result.data(false).error(new AbortExecutionException(e));
+    }
+
+    return result.build();
   }
 }
--- graphql/src/test/java/com/scalar/db/graphql/datafetcher/MutationAbortDataFetcherTest.java
+++ graphql/src/test/java/com/scalar/db/graphql/datafetcher/MutationAbortDataFetcherTest.java
@@ -1,5 +1,77 @@
 package com.scalar.db.graphql.datafetcher;
 
-public class MutationAbortDataFetcherTest {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import com.scalar.db.exception.transaction.AbortException;
+import com.scalar.db.graphql.schema.Constants;
+import graphql.execution.AbortExecutionException;
+import graphql.execution.DataFetcherResult;
+import org.junit.Test;
+
+public class MutationAbortDataFetcherTest extends DataFetcherTestBase {
+  private MutationAbortDataFetcher dataFetcher;
+
+  @Override
+  protected void doSetUp() {
+    // Arrange
+    dataFetcher = new MutationAbortDataFetcher();
+  }
+
+  @Test
+  public void get_WhenTransactionFound_ShouldAbortTransactionAndReturnTrue() throws Exception {
+    // Act
+    DataFetcherResult<Boolean> result = dataFetcher.get(environment);
+
+    // Assert
+    verify(transaction).abort();
+    assertThat(result.getData()).isTrue();
+    assertThat(result.getErrors()).isEmpty();
+  }
+
+  @Test
+  public void get_WhenTransactionFound_ShouldRemoveTransactionFromContext() throws Exception {
+    // Act
+    DataFetcherResult<Boolean> result = dataFetcher.get(environment);
+
+    // Assert
+    assertThat(graphQlContext.hasKey(Constants.CONTEXT_TRANSACTION_KEY)).isFalse();
+  }
+
+  @Test
+  public void get_WhenTransactionNotFound_ShouldReturnFalseWithErrors() throws Exception {
+    // Arrange
+    when(graphQlContext.get(Constants.CONTEXT_TRANSACTION_KEY)).thenReturn(null);
+
+    // Act
+    DataFetcherResult<Boolean> result = dataFetcher.get(environment);
+
+    // Assert
+    assertThat(result.getData()).isFalse();
+    assertThat(result.getErrors())
+        .hasSize(1)
+        .element(0)
+        .isInstanceOf(AbortExecutionException.class);
+  }
+
+  @Test
+  public void get_WhenAbortExceptionThrown_ShouldReturnFalseWithErrors() throws Exception {
+    // Arrange
+    AbortException exception = new AbortException("error");
+    doThrow(exception).when(transaction).abort();
+
+    // Act
+    DataFetcherResult<Boolean> result = dataFetcher.get(environment);
+
+    // Assert
+    assertThat(result.getData()).isFalse();
+    assertThat(result.getErrors())
+        .hasSize(1)
+        .element(0)
+        .isInstanceOf(AbortExecutionException.class);
+    assertThat(((AbortExecutionException) result.getErrors().get(0)).getCause())
+        .isSameAs(exception);
+  }
 }
