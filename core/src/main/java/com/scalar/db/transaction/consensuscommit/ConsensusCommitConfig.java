e307600d7ce648cfdc060f80439818f4dbc4ec5e
Toshihiro Suzuki <brfrn169@gmail.com>
2021-12-27 11:17:18 +0900

    Support parallel preparation/commit/rollback in Consensus Commit (#438)

--- core/src/main/java/com/scalar/db/transaction/consensuscommit/ConsensusCommitConfig.java
+++ core/src/main/java/com/scalar/db/transaction/consensuscommit/ConsensusCommitConfig.java
@@ -1,6 +1,7 @@
 package com.scalar.db.transaction.consensuscommit;
 
 import static com.scalar.db.config.ConfigUtils.getBoolean;
+import static com.scalar.db.config.ConfigUtils.getInt;
 import static com.scalar.db.config.ConfigUtils.getString;
 
 import com.scalar.db.config.DatabaseConfig;
@@ -26,11 +27,23 @@ public class ConsensusCommitConfig {
   public static final String SERIALIZABLE_STRATEGY = PREFIX + "serializable_strategy";
   public static final String COORDINATOR_NAMESPACE = PREFIX + "coordinator.namespace";
 
+  public static final String PARALLEL_EXECUTOR_COUNT = PREFIX + "parallel_executor_count";
+  public static final String PARALLEL_PREPARATION_ENABLED = PREFIX + "parallel_preparation.enabled";
+  public static final String PARALLEL_COMMIT_ENABLED = PREFIX + "parallel_commit.enabled";
+  public static final String PARALLEL_ROLLBACK_ENABLED = PREFIX + "parallel_rollback.enabled";
+
+  public static final int DEFAULT_PARALLEL_EXECUTOR_COUNT = 30;
+
   private final Properties props;
 
   private Isolation isolation;
   private SerializableStrategy strategy;
   @Nullable private String coordinatorNamespace;
+
+  private int parallelExecutorCount;
+  private boolean parallelPreparationEnabled;
+  private boolean parallelCommitEnabled;
+  private boolean parallelRollbackEnabled;
 
   // for two-phase consensus commit
   public static final String TWO_PHASE_CONSENSUS_COMMIT_PREFIX = PREFIX + "2pcc.";
@@ -84,9 +97,18 @@ public class ConsensusCommitConfig {
                     SERIALIZABLE_STRATEGY,
                     SerializableStrategy.EXTRA_READ.toString())
                 .toUpperCase());
+
     activeTransactionsManagementEnabled =
         getBoolean(getProperties(), ACTIVE_TRANSACTIONS_MANAGEMENT_ENABLED, true);
+
     coordinatorNamespace = getString(getProperties(), COORDINATOR_NAMESPACE, null);
+
+    parallelExecutorCount =
+        getInt(getProperties(), PARALLEL_EXECUTOR_COUNT, DEFAULT_PARALLEL_EXECUTOR_COUNT);
+    parallelPreparationEnabled = getBoolean(getProperties(), PARALLEL_PREPARATION_ENABLED, false);
+    parallelCommitEnabled = getBoolean(getProperties(), PARALLEL_COMMIT_ENABLED, false);
+    parallelRollbackEnabled =
+        getBoolean(getProperties(), PARALLEL_ROLLBACK_ENABLED, parallelCommitEnabled);
   }
 
   public Isolation getIsolation() {
@@ -103,5 +125,21 @@ public class ConsensusCommitConfig {
 
   public Optional<String> getCoordinatorNamespace() {
     return Optional.ofNullable(coordinatorNamespace);
+  }
+
+  public int getParallelExecutorCount() {
+    return parallelExecutorCount;
+  }
+
+  public boolean isParallelPreparationEnabled() {
+    return parallelPreparationEnabled;
+  }
+
+  public boolean isParallelCommitEnabled() {
+    return parallelCommitEnabled;
+  }
+
+  public boolean isParallelRollbackEnabled() {
+    return parallelRollbackEnabled;
   }
 }
