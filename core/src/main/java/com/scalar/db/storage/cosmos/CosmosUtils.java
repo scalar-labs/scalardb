98887657e08c864bc5b44f587ff7da1b17a2be93
thongdk8 <thong8111996@gmail.com>
2022-01-13 18:28:28 +0900

    add constructor

--- core/src/main/java/com/scalar/db/storage/cosmos/CosmosUtils.java
+++ core/src/main/java/com/scalar/db/storage/cosmos/CosmosUtils.java
@@ -1,6 +1,9 @@
 package com.scalar.db.storage.cosmos;
 
 public final class CosmosUtils {
+
+  private CosmosUtils() {}
+
   public static String quoteKeyword(String keyword) {
     return "[\"" + keyword + "\"]";
   }
