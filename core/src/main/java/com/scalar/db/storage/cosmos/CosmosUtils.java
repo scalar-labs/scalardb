c67fd6d22d859b6d3b2b75b9b48a5dc8b5e7577e
thongdk8 <thong8111996@gmail.com>
2022-01-13 15:11:32 +0900

    fix based on review

--- core/src/main/java/com/scalar/db/storage/cosmos/CosmosUtils.java
+++ core/src/main/java/com/scalar/db/storage/cosmos/CosmosUtils.java
@@ -1,6 +1,6 @@
 package com.scalar.db.storage.cosmos;
 
-public class CosmosUtils {
+public final class CosmosUtils {
   public static String quoteKeyword(String keyword) {
     return "[\"" + keyword + "\"]";
   }
