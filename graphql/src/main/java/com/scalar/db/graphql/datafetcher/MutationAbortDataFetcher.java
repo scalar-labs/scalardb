a99624a4c8b72dac8d7e376e0a5567da04b10cc9
Yusuke Morimoto <yusuke.morimoto@scalar-labs.com>
2022-01-19 12:02:14 +0900

    Add logger to MutationAbortDataFetcher

--- graphql/src/main/java/com/scalar/db/graphql/datafetcher/MutationAbortDataFetcher.java
+++ graphql/src/main/java/com/scalar/db/graphql/datafetcher/MutationAbortDataFetcher.java
@@ -8,24 +8,31 @@ import graphql.execution.AbortExecutionException;
 import graphql.execution.DataFetcherResult;
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MutationAbortDataFetcher implements DataFetcher<DataFetcherResult<Boolean>> {
+  private static final Logger LOGGER = LoggerFactory.getLogger(MutationAbortDataFetcher.class);
+
   @Override
   public DataFetcherResult<Boolean> get(DataFetchingEnvironment environment) throws Exception {
     GraphQLContext graphQLContext = environment.getGraphQlContext();
     DistributedTransaction transaction = graphQLContext.get(Constants.CONTEXT_TRANSACTION_KEY);
     if (transaction == null) {
+      LOGGER.warn("got abort mutation, but transaction was not found");
       return DataFetcherResult.<Boolean>newResult()
           .data(false)
-          .error(new AbortExecutionException("The transaction to abort is not specified"))
+          .error(new AbortExecutionException("the transaction to abort is not found"))
           .build();
     }
 
+    LOGGER.debug("aborting transaction {}", transaction.getId());
     DataFetcherResult.Builder<Boolean> result = DataFetcherResult.newResult();
     try {
       transaction.abort();
       result.data(true);
     } catch (AbortException e) {
+      LOGGER.warn("abort failed", e);
       result.data(false).error(new AbortExecutionException(e));
     }
 
