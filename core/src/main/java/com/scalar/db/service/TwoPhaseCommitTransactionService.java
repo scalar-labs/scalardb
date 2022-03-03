73849074ea103270d0e87ff938a500296dfd4570
Toshihiro Suzuki <brfrn169@gmail.com>
2022-03-03 21:16:46 +0900

    Deprecate the methods for default namespace and table (#518)

--- core/src/main/java/com/scalar/db/service/TwoPhaseCommitTransactionService.java
+++ core/src/main/java/com/scalar/db/service/TwoPhaseCommitTransactionService.java
@@ -19,26 +19,31 @@ public class TwoPhaseCommitTransactionService implements TwoPhaseCommitTransacti
     this.manager = manager;
   }
 
+  @Deprecated
   @Override
   public void with(String namespace, String tableName) {
     manager.with(namespace, tableName);
   }
 
+  @Deprecated
   @Override
   public void withNamespace(String namespace) {
     manager.withNamespace(namespace);
   }
 
+  @Deprecated
   @Override
   public Optional<String> getNamespace() {
     return manager.getNamespace();
   }
 
+  @Deprecated
   @Override
   public void withTable(String tableName) {
     manager.withTable(tableName);
   }
 
+  @Deprecated
   @Override
   public Optional<String> getTable() {
     return manager.getTable();
