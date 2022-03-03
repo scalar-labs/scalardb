a41912f99135410b05789d8fa6dc5e2a245488db
Toshihiro Suzuki <brfrn169@gmail.com>
2022-03-03 21:16:46 +0900

    Deprecate the methods for default namespace and table (#518)

--- core/src/main/java/com/scalar/db/transaction/common/AbstractDistributedTransactionManager.java
+++ core/src/main/java/com/scalar/db/transaction/common/AbstractDistributedTransactionManager.java
@@ -14,27 +14,32 @@ public abstract class AbstractDistributedTransactionManager
     tableName = Optional.empty();
   }
 
+  @Deprecated
   @Override
   public void with(String namespace, String tableName) {
     this.namespace = Optional.ofNullable(namespace);
     this.tableName = Optional.ofNullable(tableName);
   }
 
+  @Deprecated
   @Override
   public void withNamespace(String namespace) {
     this.namespace = Optional.ofNullable(namespace);
   }
 
+  @Deprecated
   @Override
   public Optional<String> getNamespace() {
     return namespace;
   }
 
+  @Deprecated
   @Override
   public void withTable(String tableName) {
     this.tableName = Optional.ofNullable(tableName);
   }
 
+  @Deprecated
   @Override
   public Optional<String> getTable() {
     return tableName;
