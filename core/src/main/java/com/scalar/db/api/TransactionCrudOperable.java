341569bb924028d09c24b30a2481000a141be602
Toshihiro Suzuki <brfrn169@gmail.com>
2022-03-25 16:11:41 +0900

    Extract an interface for transaction CRUD operations (#533)

---
+++ core/src/main/java/com/scalar/db/api/TransactionCrudOperable.java
@@ -0,0 +1,87 @@
+package com.scalar.db.api;
+
+import com.scalar.db.exception.transaction.CrudConflictException;
+import com.scalar.db.exception.transaction.CrudException;
+import java.util.List;
+import java.util.Optional;
+
+/** An interface for transaction CRUD operations. */
+public interface TransactionCrudOperable {
+
+  /**
+   * Retrieves a result from the storage through a transaction with the specified {@link Get}
+   * command with a primary key and returns the result.
+   *
+   * @param get a {@code Get} command
+   * @return an {@code Optional} with the returned result
+   * @throws CrudConflictException if conflicts happened. You can retry the transaction in this case
+   * @throws CrudException if the operation failed
+   */
+  Optional<Result> get(Get get) throws CrudConflictException, CrudException;
+
+  /**
+   * Retrieves results from the storage through a transaction with the specified {@link Scan}
+   * command with a partition key and returns a list of {@link Result}. Results can be filtered by
+   * specifying a range of clustering keys.
+   *
+   * @param scan a {@code Scan} command
+   * @return a list of {@link Result}
+   * @throws CrudConflictException if conflicts happened. You can retry the transaction in this case
+   * @throws CrudException if the operation failed
+   */
+  List<Result> scan(Scan scan) throws CrudConflictException, CrudException;
+
+  /**
+   * Inserts/Updates an entry to the storage through a transaction with the specified {@link Put}
+   * command. Note that the conditions set in Put will be ignored. Please program such conditions in
+   * a transaction if you want to implement conditional mutation.
+   *
+   * @param put a {@code Put} command
+   * @throws CrudConflictException if conflicts happened. You can retry the transaction in this case
+   * @throws CrudException if the operation failed
+   */
+  void put(Put put) throws CrudConflictException, CrudException;
+
+  /**
+   * Inserts/Updates multiple entries to the storage through a transaction with the specified list
+   * of {@link Put} commands. Note that the conditions set in Put will be ignored. Please program
+   * such conditions in a transaction if you want to implement conditional mutation.
+   *
+   * @param puts a list of {@code Put} commands
+   * @throws CrudConflictException if conflicts happened. You can retry the transaction in this case
+   * @throws CrudException if the operation failed
+   */
+  void put(List<Put> puts) throws CrudConflictException, CrudException;
+
+  /**
+   * Deletes an entry from the storage through a transaction with the specified {@link Delete}
+   * command. Note that the conditions set in Delete will be ignored. Please program such conditions
+   * in a transaction if you want to implement conditional mutation.
+   *
+   * @param delete a {@code Delete} command
+   * @throws CrudConflictException if conflicts happened. You can retry the transaction in this case
+   * @throws CrudException if the operation failed
+   */
+  void delete(Delete delete) throws CrudConflictException, CrudException;
+
+  /**
+   * Deletes entries from the storage through a transaction with the specified list of {@link
+   * Delete} commands. Note that the conditions set in Delete will be ignored. Please program such
+   * conditions in a transaction if you want to implement conditional mutation.
+   *
+   * @param deletes a list of {@code Delete} commands
+   * @throws CrudConflictException if conflicts happened. You can retry the transaction in this case
+   * @throws CrudException if the operation failed
+   */
+  void delete(List<Delete> deletes) throws CrudConflictException, CrudException;
+
+  /**
+   * Mutates entries of the storage through a transaction with the specified list of {@link
+   * Mutation} commands.
+   *
+   * @param mutations a list of {@code Mutation} commands
+   * @throws CrudConflictException if conflicts happened. You can retry the transaction in this case
+   * @throws CrudException if the operation failed
+   */
+  void mutate(List<? extends Mutation> mutations) throws CrudConflictException, CrudException;
+}
