b58b109263ebd8e979c611360fe7c67bb45d09d3
Toshihiro Suzuki <brfrn169@gmail.com>
2022-04-21 18:27:13 +0900

    Add transaction integration tests (#559)

--- core/src/integration-test/java/com/scalar/db/storage/multistorage/ConsensusCommitSpecificIntegrationTestWithMultiStorage.java
+++ core/src/integration-test/java/com/scalar/db/storage/multistorage/ConsensusCommitSpecificIntegrationTestWithMultiStorage.java
@@ -1,12 +1,12 @@
 package com.scalar.db.storage.multistorage;
 
 import com.scalar.db.config.DatabaseConfig;
-import com.scalar.db.transaction.consensuscommit.ConsensusCommitIntegrationTestBase;
+import com.scalar.db.transaction.consensuscommit.ConsensusCommitSpecificIntegrationTestBase;
 import com.scalar.db.transaction.consensuscommit.Coordinator;
 import java.util.Properties;
 
-public class ConsensusCommitWithMultiStorageIntegrationTest
-    extends ConsensusCommitIntegrationTestBase {
+public class ConsensusCommitSpecificIntegrationTestWithMultiStorage
+    extends ConsensusCommitSpecificIntegrationTestBase {
 
   @Override
   protected DatabaseConfig getDatabaseConfig() {
