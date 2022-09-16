22c3482136f8db7e87b992617b175d59a59c2b5b
Toshihiro Suzuki <brfrn169@gmail.com>
2022-09-16 11:42:29 +0900

    Refactor ServiceLoader related classes (#698)

--- core/src/main/java/com/scalar/db/storage/cassandra/CassandraProvider.java
+++ core/src/main/java/com/scalar/db/storage/cassandra/CassandraProvider.java
@@ -2,22 +2,22 @@ package com.scalar.db.storage.cassandra;
 
 import com.scalar.db.api.DistributedStorage;
 import com.scalar.db.api.DistributedStorageAdmin;
-import com.scalar.db.api.DistributedStorageFactory;
+import com.scalar.db.api.DistributedStorageProvider;
 import com.scalar.db.config.DatabaseConfig;
 
-public class CassandraFactory implements DistributedStorageFactory {
+public class CassandraProvider implements DistributedStorageProvider {
   @Override
   public String getName() {
     return "cassandra";
   }
 
   @Override
-  public DistributedStorage getDistributedStorage(DatabaseConfig config) {
+  public DistributedStorage createDistributedStorage(DatabaseConfig config) {
     return new Cassandra(config);
   }
 
   @Override
-  public DistributedStorageAdmin getDistributedStorageAdmin(DatabaseConfig config) {
+  public DistributedStorageAdmin createDistributedStorageAdmin(DatabaseConfig config) {
     return new CassandraAdmin(config);
   }
 }
--- core/src/main/java/com/scalar/db/storage/dynamo/DynamoProvider.java
+++ core/src/main/java/com/scalar/db/storage/dynamo/DynamoProvider.java
@@ -2,22 +2,22 @@ package com.scalar.db.storage.dynamo;
 
 import com.scalar.db.api.DistributedStorage;
 import com.scalar.db.api.DistributedStorageAdmin;
-import com.scalar.db.api.DistributedStorageFactory;
+import com.scalar.db.api.DistributedStorageProvider;
 import com.scalar.db.config.DatabaseConfig;
 
-public class DynamoFactory implements DistributedStorageFactory {
+public class DynamoProvider implements DistributedStorageProvider {
   @Override
   public String getName() {
     return "dynamo";
   }
 
   @Override
-  public DistributedStorage getDistributedStorage(DatabaseConfig config) {
+  public DistributedStorage createDistributedStorage(DatabaseConfig config) {
     return new Dynamo(config);
   }
 
   @Override
-  public DistributedStorageAdmin getDistributedStorageAdmin(DatabaseConfig config) {
+  public DistributedStorageAdmin createDistributedStorageAdmin(DatabaseConfig config) {
     return new DynamoAdmin(config);
   }
 }
