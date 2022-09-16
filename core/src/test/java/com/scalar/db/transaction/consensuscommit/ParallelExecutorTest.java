f9607150bc7b14ad2194939ca56af78ebda1509f
Toshihiro Suzuki <brfrn169@gmail.com>
2022-09-16 10:29:49 +0900

    Commit/rollback records should always be done for all records even on error (#680)

--- core/src/test/java/com/scalar/db/transaction/consensuscommit/ParallelExecutorTest.java
+++ core/src/test/java/com/scalar/db/transaction/consensuscommit/ParallelExecutorTest.java
@@ -1,6 +1,13 @@
 package com.scalar.db.transaction.consensuscommit;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.atMost;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -10,22 +17,23 @@ import com.scalar.db.exception.transaction.CommitConflictException;
 import com.scalar.db.transaction.consensuscommit.ParallelExecutor.ParallelExecutorTask;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 public class ParallelExecutorTest {
 
+  private static final String TX_ID = "id";
+
   @Mock private ConsensusCommitConfig config;
-  @Mock private ExecutorService parallelExecutorService;
-  @Mock private Future<Void> future;
   @Mock private ParallelExecutorTask task;
 
+  private ExecutorService parallelExecutorService;
   private ParallelExecutor parallelExecutor;
   private List<ParallelExecutorTask> tasks;
 
@@ -34,178 +42,358 @@ public class ParallelExecutorTest {
     MockitoAnnotations.openMocks(this).close();
 
     // Arrange
-    when(parallelExecutorService.submit(ArgumentMatchers.<Callable<Void>>any())).thenReturn(future);
-    parallelExecutor = new ParallelExecutor(config, parallelExecutorService);
     tasks = Arrays.asList(task, task, task);
+    parallelExecutorService = spy(Executors.newFixedThreadPool(tasks.size()));
+    parallelExecutor = new ParallelExecutor(config, parallelExecutorService);
+  }
+
+  @AfterEach
+  protected void afterEach() throws Exception {
+    parallelExecutorService.shutdown();
+    if (!parallelExecutorService.awaitTermination(10, TimeUnit.SECONDS)) {
+      parallelExecutorService.shutdownNow();
+    }
   }
 
   @Test
   public void prepare_ParallelPreparationNotEnabled_ShouldExecuteTasksSerially()
-      throws ExecutionException, java.util.concurrent.ExecutionException, InterruptedException,
-          CommitConflictException {
+      throws ExecutionException, CommitConflictException {
     // Arrange
     when(config.isParallelPreparationEnabled()).thenReturn(false);
 
     // Act
-    parallelExecutor.prepare(tasks);
+    parallelExecutor.prepare(tasks, TX_ID);
 
     // Assert
     verify(task, times(tasks.size())).run();
-    verify(parallelExecutorService, never()).submit(ArgumentMatchers.<Callable<Void>>any());
-    verify(future, never()).get();
-  }
-
-  @Test
-  public void prepare_ParallelPreparationEnabled_ShouldExecuteTasksInParallel()
-      throws ExecutionException, java.util.concurrent.ExecutionException, InterruptedException,
-          CommitConflictException {
-    // Arrange
-    when(config.isParallelPreparationEnabled()).thenReturn(true);
-
-    // Act
-    parallelExecutor.prepare(tasks);
-
-    // Assert
-    verify(task, never()).run();
-    verify(parallelExecutorService, times(tasks.size()))
-        .submit(ArgumentMatchers.<Callable<Void>>any());
-    verify(future, times(tasks.size())).get();
-  }
-
-  @Test
-  public void validate_ParallelValidationNotEnabled_ShouldExecuteTasksSerially()
-      throws ExecutionException, java.util.concurrent.ExecutionException, InterruptedException,
-          CommitConflictException {
-    // Arrange
-    when(config.isParallelValidationEnabled()).thenReturn(false);
-
-    // Act
-    parallelExecutor.validate(tasks);
-
-    // Assert
-    verify(task, times(tasks.size())).run();
-    verify(parallelExecutorService, never()).submit(ArgumentMatchers.<Callable<Void>>any());
-    verify(future, never()).get();
-  }
-
-  @Test
-  public void validate_ParallelValidationEnabled_ShouldExecuteTasksInParallel()
-      throws ExecutionException, java.util.concurrent.ExecutionException, InterruptedException,
-          CommitConflictException {
-    // Arrange
-    when(config.isParallelValidationEnabled()).thenReturn(true);
-
-    // Act
-    parallelExecutor.validate(tasks);
-
-    // Assert
-    verify(task, never()).run();
-    verify(parallelExecutorService, times(tasks.size()))
-        .submit(ArgumentMatchers.<Callable<Void>>any());
-    verify(future, times(tasks.size())).get();
-  }
-
-  @Test
-  public void commit_ParallelCommitNotEnabled_ShouldExecuteTasksSerially()
-      throws ExecutionException, java.util.concurrent.ExecutionException, InterruptedException,
-          CommitConflictException {
-    // Arrange
-    when(config.isParallelCommitEnabled()).thenReturn(false);
-
-    // Act
-    parallelExecutor.commit(tasks);
-
-    // Assert
-    verify(task, times(tasks.size())).run();
-    verify(parallelExecutorService, never()).submit(ArgumentMatchers.<Callable<Void>>any());
-    verify(future, never()).get();
-  }
-
-  @Test
-  public void commit_ParallelCommitEnabled_ShouldExecuteTasksInParallel()
-      throws ExecutionException, java.util.concurrent.ExecutionException, InterruptedException,
-          CommitConflictException {
-    // Arrange
-    when(config.isParallelCommitEnabled()).thenReturn(true);
-
-    // Act
-    parallelExecutor.commit(tasks);
-
-    // Assert
-    verify(task, never()).run();
-    verify(parallelExecutorService, times(tasks.size()))
-        .submit(ArgumentMatchers.<Callable<Void>>any());
-    verify(future, times(tasks.size())).get();
+    verify(parallelExecutorService, never()).execute(any());
   }
 
   @Test
   public void
-      commit_ParallelCommitEnabledAndAsyncCommitEnabled_ShouldExecuteTasksInParallelAndAsynchronously()
-          throws ExecutionException, java.util.concurrent.ExecutionException, InterruptedException,
-              CommitConflictException {
+      prepare_ParallelPreparationNotEnabled_ExecutionExceptionThrownByTask_ShouldStopRunningTasks()
+          throws ExecutionException, CommitConflictException {
+    // Arrange
+    when(config.isParallelPreparationEnabled()).thenReturn(false);
+    doThrow(ExecutionException.class).when(task).run();
+
+    // Act Assert
+    assertThatThrownBy(() -> parallelExecutor.prepare(tasks, TX_ID))
+        .isInstanceOf(ExecutionException.class);
+
+    verify(task, only()).run();
+    verify(parallelExecutorService, never()).execute(any());
+  }
+
+  @Test
+  public void prepare_ParallelPreparationEnabled_ShouldExecuteTasksInParallel()
+      throws ExecutionException, CommitConflictException {
+    // Arrange
+    when(config.isParallelPreparationEnabled()).thenReturn(true);
+
+    // Act
+    parallelExecutor.prepare(tasks, TX_ID);
+
+    // Assert
+    verify(task, times(tasks.size())).run();
+    verify(parallelExecutorService, times(tasks.size())).execute(any());
+  }
+
+  @Test
+  public void
+      prepare_ParallelPreparationEnabled_ExecutionExceptionThrownByTask_ShouldNotStopRunningTasks()
+          throws ExecutionException, CommitConflictException {
+    // Arrange
+    when(config.isParallelPreparationEnabled()).thenReturn(true);
+    doThrow(ExecutionException.class).when(task).run();
+
+    // Act Assert
+    assertThatThrownBy(() -> parallelExecutor.prepare(tasks, TX_ID))
+        .isInstanceOf(ExecutionException.class);
+
+    verify(task, times(tasks.size())).run();
+    verify(parallelExecutorService, times(tasks.size())).execute(any());
+  }
+
+  @Test
+  public void validate_ParallelValidationNotEnabled_ShouldExecuteTasksSerially()
+      throws ExecutionException, CommitConflictException {
+    // Arrange
+    when(config.isParallelValidationEnabled()).thenReturn(false);
+
+    // Act
+    parallelExecutor.validate(tasks, TX_ID);
+
+    // Assert
+    verify(task, times(tasks.size())).run();
+    verify(parallelExecutorService, never()).execute(any());
+  }
+
+  @Test
+  public void
+      validate_ParallelValidationNotEnabled_ExecutionExceptionThrownByTask_ShouldStopRunningTasks()
+          throws ExecutionException, CommitConflictException {
+    // Arrange
+    when(config.isParallelValidationEnabled()).thenReturn(false);
+    doThrow(ExecutionException.class).when(task).run();
+
+    // Act Assert
+    assertThatThrownBy(() -> parallelExecutor.validate(tasks, TX_ID))
+        .isInstanceOf(ExecutionException.class);
+
+    verify(task, only()).run();
+    verify(parallelExecutorService, never()).execute(any());
+  }
+
+  @Test
+  public void
+      validate_ParallelValidationNotEnabled_CommitConflictExceptionThrownByTask_ShouldStopRunningTasks()
+          throws ExecutionException, CommitConflictException {
+    // Arrange
+    when(config.isParallelValidationEnabled()).thenReturn(false);
+    doThrow(CommitConflictException.class).when(task).run();
+
+    // Act Assert
+    assertThatThrownBy(() -> parallelExecutor.validate(tasks, TX_ID))
+        .isInstanceOf(CommitConflictException.class);
+
+    verify(task, only()).run();
+    verify(parallelExecutorService, never()).execute(any());
+  }
+
+  @Test
+  public void validate_ParallelValidationEnabled_ShouldExecuteTasksInParallel()
+      throws ExecutionException, CommitConflictException {
+    // Arrange
+    when(config.isParallelValidationEnabled()).thenReturn(true);
+
+    // Act
+    parallelExecutor.validate(tasks, TX_ID);
+
+    // Assert
+    verify(task, times(tasks.size())).run();
+    verify(parallelExecutorService, times(tasks.size())).execute(any());
+  }
+
+  @Test
+  public void
+      validate_ParallelValidationEnabled_ExecutionExceptionThrownByTask_ShouldStopRunningTasks()
+          throws ExecutionException, CommitConflictException {
+    // Arrange
+    when(config.isParallelValidationEnabled()).thenReturn(true);
+    doThrow(ExecutionException.class).when(task).run();
+
+    // Act Assert
+    assertThatThrownBy(() -> parallelExecutor.validate(tasks, TX_ID))
+        .isInstanceOf(ExecutionException.class);
+
+    verify(task, atMost(tasks.size())).run();
+    verify(parallelExecutorService, times(tasks.size())).execute(any());
+  }
+
+  @Test
+  public void
+      validate_ParallelValidationEnabled_CommitConflictExceptionThrownByTask_ShouldStopRunningTasks()
+          throws ExecutionException, CommitConflictException {
+    // Arrange
+    when(config.isParallelValidationEnabled()).thenReturn(true);
+    doThrow(CommitConflictException.class).when(task).run();
+
+    // Act Assert
+    assertThatThrownBy(() -> parallelExecutor.validate(tasks, TX_ID))
+        .isInstanceOf(CommitConflictException.class);
+
+    verify(task, atMost(tasks.size())).run();
+    verify(parallelExecutorService, times(tasks.size())).execute(any());
+  }
+
+  @Test
+  public void commitRecords_ParallelCommitNotEnabled_ShouldExecuteTasksSerially()
+      throws ExecutionException, CommitConflictException {
+    // Arrange
+    when(config.isParallelCommitEnabled()).thenReturn(false);
+
+    // Act
+    parallelExecutor.commitRecords(tasks, TX_ID);
+
+    // Assert
+    verify(task, times(tasks.size())).run();
+    verify(parallelExecutorService, never()).execute(any());
+  }
+
+  @Test
+  public void
+      commitRecords_ParallelCommitNotEnabled_ExecutionExceptionThrownByTask_ShouldNotStopRunningTasks()
+          throws ExecutionException, CommitConflictException {
+    // Arrange
+    when(config.isParallelCommitEnabled()).thenReturn(false);
+    doThrow(ExecutionException.class).when(task).run();
+
+    // Act Assert
+    assertThatThrownBy(() -> parallelExecutor.commitRecords(tasks, TX_ID))
+        .isInstanceOf(ExecutionException.class);
+
+    verify(task, times(tasks.size())).run();
+    verify(parallelExecutorService, never()).execute(any());
+  }
+
+  @Test
+  public void
+      commitRecords_ParallelCommitEnabledAndAsyncCommitNotEnabled_ShouldExecuteTasksInParallel()
+          throws ExecutionException, CommitConflictException {
+    // Arrange
+    when(config.isParallelCommitEnabled()).thenReturn(true);
+    when(config.isAsyncCommitEnabled()).thenReturn(false);
+
+    // Act
+    parallelExecutor.commitRecords(tasks, TX_ID);
+
+    // Assert
+    verify(task, times(tasks.size())).run();
+    verify(parallelExecutorService, times(tasks.size())).execute(any());
+  }
+
+  @Test
+  public void
+      commitRecords_ParallelCommitEnabledAndAsyncCommitNotEnabled_ExecutionExceptionThrownByTask_ShouldNotStopRunningTasks()
+          throws ExecutionException, CommitConflictException {
+    // Arrange
+    when(config.isParallelCommitEnabled()).thenReturn(true);
+    when(config.isAsyncCommitEnabled()).thenReturn(false);
+    doThrow(ExecutionException.class).when(task).run();
+
+    // Act Assert
+    assertThatThrownBy(() -> parallelExecutor.commitRecords(tasks, TX_ID))
+        .isInstanceOf(ExecutionException.class);
+
+    verify(task, times(tasks.size())).run();
+    verify(parallelExecutorService, times(tasks.size())).execute(any());
+  }
+
+  @Test
+  public void
+      commitRecords_ParallelCommitEnabledAndAsyncCommitEnabled_ShouldExecuteTasksInParallelAndAsynchronously()
+          throws ExecutionException, CommitConflictException {
     // Arrange
     when(config.isParallelCommitEnabled()).thenReturn(true);
     when(config.isAsyncCommitEnabled()).thenReturn(true);
 
     // Act
-    parallelExecutor.commit(tasks);
+    parallelExecutor.commitRecords(tasks, TX_ID);
 
     // Assert
-    verify(task, never()).run();
-    verify(parallelExecutorService, times(tasks.size()))
-        .submit(ArgumentMatchers.<Callable<Void>>any());
-    verify(future, never()).get();
-  }
-
-  @Test
-  public void rollback_ParallelRollbackNotEnabled_ShouldExecuteTasksSerially()
-      throws ExecutionException, java.util.concurrent.ExecutionException, InterruptedException,
-          CommitConflictException {
-    // Arrange
-    when(config.isParallelRollbackEnabled()).thenReturn(false);
-
-    // Act
-    parallelExecutor.rollback(tasks);
-
-    // Assert
-    verify(task, times(tasks.size())).run();
-    verify(parallelExecutorService, never()).submit(ArgumentMatchers.<Callable<Void>>any());
-    verify(future, never()).get();
-  }
-
-  @Test
-  public void rollback_ParallelRollbackEnabled_ShouldExecuteTasksInParallel()
-      throws ExecutionException, java.util.concurrent.ExecutionException, InterruptedException,
-          CommitConflictException {
-    // Arrange
-    when(config.isParallelRollbackEnabled()).thenReturn(true);
-
-    // Act
-    parallelExecutor.rollback(tasks);
-
-    // Assert
-    verify(task, never()).run();
-    verify(parallelExecutorService, times(tasks.size()))
-        .submit(ArgumentMatchers.<Callable<Void>>any());
-    verify(future, times(tasks.size())).get();
+    verify(task, atMost(tasks.size())).run();
+    verify(parallelExecutorService, times(tasks.size())).execute(any());
   }
 
   @Test
   public void
-      rollback_ParallelRollbackEnabledAndAsyncRollbackEnabled_ShouldExecuteTasksInParallelAndAsynchronously()
-          throws ExecutionException, java.util.concurrent.ExecutionException, InterruptedException,
-              CommitConflictException {
+      commitRecords_ParallelCommitEnabledAndAsyncCommitEnabled_ExecutionExceptionThrownByTask_ShouldNotStopRunningTasks()
+          throws ExecutionException, CommitConflictException {
+    // Arrange
+    when(config.isParallelCommitEnabled()).thenReturn(true);
+    when(config.isAsyncCommitEnabled()).thenReturn(true);
+    doThrow(ExecutionException.class).when(task).run();
+
+    // Act Assert
+    assertThatCode(() -> parallelExecutor.commitRecords(tasks, TX_ID)).doesNotThrowAnyException();
+
+    verify(task, atMost(tasks.size())).run();
+    verify(parallelExecutorService, times(tasks.size())).execute(any());
+  }
+
+  @Test
+  public void rollbackRecords_ParallelRollbackNotEnabled_ShouldExecuteTasksSerially()
+      throws ExecutionException, CommitConflictException {
+    // Arrange
+    when(config.isParallelRollbackEnabled()).thenReturn(false);
+
+    // Act
+    parallelExecutor.rollbackRecords(tasks, TX_ID);
+
+    // Assert
+    verify(task, times(tasks.size())).run();
+    verify(parallelExecutorService, never()).execute(any());
+  }
+
+  @Test
+  public void
+      rollbackRecords_ParallelRollbackNotEnabled_ExecutionExceptionThrownByTask_ShouldNotStopRunningTasks()
+          throws ExecutionException, CommitConflictException {
+    // Arrange
+    when(config.isParallelRollbackEnabled()).thenReturn(false);
+    doThrow(ExecutionException.class).when(task).run();
+
+    // Act Assert
+    assertThatThrownBy(() -> parallelExecutor.rollbackRecords(tasks, TX_ID))
+        .isInstanceOf(ExecutionException.class);
+
+    verify(task, times(tasks.size())).run();
+    verify(parallelExecutorService, never()).execute(any());
+  }
+
+  @Test
+  public void
+      rollbackRecords_ParallelRollbackEnabledAndAsyncRollbackNotEnabled_ShouldExecuteTasksInParallel()
+          throws ExecutionException, CommitConflictException {
+    // Arrange
+    when(config.isParallelRollbackEnabled()).thenReturn(true);
+    when(config.isAsyncRollbackEnabled()).thenReturn(false);
+
+    // Act
+    parallelExecutor.rollbackRecords(tasks, TX_ID);
+
+    // Assert
+    verify(task, times(tasks.size())).run();
+    verify(parallelExecutorService, times(tasks.size())).execute(any());
+  }
+
+  @Test
+  public void
+      rollbackRecords_ParallelRollbackEnabledAndAsyncRollbackNotEnabled_ExecutionExceptionThrownByTask_ShouldNotStopRunningTasks()
+          throws ExecutionException, CommitConflictException {
+    // Arrange
+    when(config.isParallelRollbackEnabled()).thenReturn(true);
+    when(config.isAsyncRollbackEnabled()).thenReturn(false);
+    doThrow(ExecutionException.class).when(task).run();
+
+    // Act Assert
+    assertThatThrownBy(() -> parallelExecutor.rollbackRecords(tasks, TX_ID))
+        .isInstanceOf(ExecutionException.class);
+
+    verify(task, times(tasks.size())).run();
+    verify(parallelExecutorService, times(tasks.size())).execute(any());
+  }
+
+  @Test
+  public void
+      rollbackRecords_ParallelRollbackEnabledAndAsyncRollbackEnabled_ShouldExecuteTasksInParallelAndAsynchronously()
+          throws ExecutionException, CommitConflictException {
     // Arrange
     when(config.isParallelRollbackEnabled()).thenReturn(true);
     when(config.isAsyncRollbackEnabled()).thenReturn(true);
 
     // Act
-    parallelExecutor.rollback(tasks);
+    parallelExecutor.rollbackRecords(tasks, TX_ID);
 
     // Assert
-    verify(task, never()).run();
-    verify(parallelExecutorService, times(tasks.size()))
-        .submit(ArgumentMatchers.<Callable<Void>>any());
-    verify(future, never()).get();
+    verify(task, atMost(tasks.size())).run();
+    verify(parallelExecutorService, times(tasks.size())).execute(any());
+  }
+
+  @Test
+  public void
+      rollbackRecords_ParallelRollbackEnabledAndAsyncRollbackEnabled_ExecutionExceptionThrownByTask_ShouldNotStopRunningTasks()
+          throws ExecutionException, CommitConflictException {
+    // Arrange
+    when(config.isParallelRollbackEnabled()).thenReturn(true);
+    when(config.isAsyncRollbackEnabled()).thenReturn(true);
+    doThrow(ExecutionException.class).when(task).run();
+
+    // Act Assert
+    assertThatCode(() -> parallelExecutor.rollbackRecords(tasks, TX_ID)).doesNotThrowAnyException();
+
+    verify(task, atMost(tasks.size())).run();
+    verify(parallelExecutorService, times(tasks.size())).execute(any());
   }
 }
