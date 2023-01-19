5559c4a61751c5128a93aa17eaca932b16a6d618
Toshihiro Suzuki <brfrn169@gmail.com>
2023-01-19 16:15:29 +0900

    Update transaction documents (#804)

--- core/src/main/java/com/scalar/db/exception/transaction/PreparationConflictException.java
+++ core/src/main/java/com/scalar/db/exception/transaction/PreparationConflictException.java
@@ -1,5 +1,9 @@
 package com.scalar.db.exception.transaction;
 
+/**
+ * An exception thrown when a transaction conflict occurs at the prepare phase. You can retry the
+ * transaction from the beginning in this case.
+ */
 public class PreparationConflictException extends PreparationException {
 
   public PreparationConflictException(String message) {
