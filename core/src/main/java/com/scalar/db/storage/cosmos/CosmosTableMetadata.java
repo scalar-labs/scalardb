1bc6f6ed62eb94932efe63f4ca2d2a8db10d0c60
Vincent Guilpain <vincent.guilpain@gmail.com>
2023-08-15 17:17:17 +0900

    Remove setters of Cosmos marshalling objects

--- core/src/main/java/com/scalar/db/storage/cosmos/CosmosTableMetadata.java
+++ core/src/main/java/com/scalar/db/storage/cosmos/CosmosTableMetadata.java
@@ -2,12 +2,13 @@ package com.scalar.db.storage.cosmos;
 
 import com.google.common.base.MoreObjects;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.util.Collections;
 import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import javax.annotation.concurrent.NotThreadSafe;
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.ThreadSafe;
 
 /**
  * A metadata class for a table of ScalarDB to know the type of each column
@@ -15,39 +16,44 @@ import javax.annotation.concurrent.NotThreadSafe;
  * @author Yuji Ito
  */
 @SuppressFBWarnings({"EI_EXPOSE_REP", "EI_EXPOSE_REP2"})
-@NotThreadSafe
+@ThreadSafe
 public class CosmosTableMetadata {
-  private String id;
-  private LinkedHashSet<String> partitionKeyNames;
-  private LinkedHashSet<String> clusteringKeyNames;
-  private Map<String, String> clusteringOrders;
-  private Set<String> secondaryIndexNames;
-  private Map<String, String> columns;
+  private final String id;
+  private final LinkedHashSet<String> partitionKeyNames;
+  private final LinkedHashSet<String> clusteringKeyNames;
+  private final Map<String, String> clusteringOrders;
+  private final Set<String> secondaryIndexNames;
+  private final Map<String, String> columns;
 
-  public CosmosTableMetadata() {}
-
-  public void setId(String id) {
-    this.id = id;
+  public CosmosTableMetadata() {
+    this(null, null, null, null, null, null);
   }
 
-  public void setPartitionKeyNames(List<String> partitionKeyNames) {
-    this.partitionKeyNames = new LinkedHashSet<>(partitionKeyNames);
+  public CosmosTableMetadata(
+      @Nullable String id,
+      @Nullable LinkedHashSet<String> partitionKeyNames,
+      @Nullable LinkedHashSet<String> clusteringKeyNames,
+      @Nullable Map<String, String> clusteringOrders,
+      @Nullable Set<String> secondaryIndexNames,
+      @Nullable Map<String, String> columns) {
+    this.id = id != null ? id : "";
+    this.partitionKeyNames = partitionKeyNames != null ? partitionKeyNames : new LinkedHashSet<>();
+    this.clusteringKeyNames =
+        clusteringKeyNames != null ? clusteringKeyNames : new LinkedHashSet<>();
+    this.clusteringOrders = clusteringOrders != null ? clusteringOrders : Collections.emptyMap();
+    this.secondaryIndexNames =
+        secondaryIndexNames != null ? secondaryIndexNames : Collections.emptySet();
+    this.columns = columns != null ? columns : Collections.emptyMap();
   }
 
-  public void setClusteringKeyNames(List<String> clusteringKeyNames) {
-    this.clusteringKeyNames = new LinkedHashSet<>(clusteringKeyNames);
-  }
-
-  public void setClusteringOrders(Map<String, String> clusteringOrders) {
-    this.clusteringOrders = clusteringOrders;
-  }
-
-  public void setSecondaryIndexNames(Set<String> secondaryIndexNames) {
-    this.secondaryIndexNames = secondaryIndexNames;
-  }
-
-  public void setColumns(Map<String, String> columns) {
-    this.columns = columns;
+  private CosmosTableMetadata(Builder builder) {
+    this(
+        builder.id,
+        builder.partitionKeyNames,
+        builder.clusteringKeyNames,
+        builder.clusteringOrders,
+        builder.secondaryIndexNames,
+        builder.columns);
   }
 
   public String getId() {
@@ -107,5 +113,55 @@ public class CosmosTableMetadata {
         .add("secondaryIndexNames", secondaryIndexNames)
         .add("columns", columns)
         .toString();
+  }
+
+  public static Builder newBuilder() {
+    return new Builder();
+  }
+
+  public static final class Builder {
+
+    private String id;
+    private LinkedHashSet<String> partitionKeyNames;
+    private LinkedHashSet<String> clusteringKeyNames;
+    private Map<String, String> clusteringOrders;
+    private Set<String> secondaryIndexNames;
+    private Map<String, String> columns;
+
+    private Builder() {}
+
+    public Builder id(String val) {
+      id = val;
+      return this;
+    }
+
+    public Builder partitionKeyNames(LinkedHashSet<String> val) {
+      partitionKeyNames = val;
+      return this;
+    }
+
+    public Builder clusteringKeyNames(LinkedHashSet<String> val) {
+      clusteringKeyNames = val;
+      return this;
+    }
+
+    public Builder clusteringOrders(Map<String, String> val) {
+      clusteringOrders = val;
+      return this;
+    }
+
+    public Builder secondaryIndexNames(Set<String> val) {
+      secondaryIndexNames = val;
+      return this;
+    }
+
+    public Builder columns(Map<String, String> val) {
+      columns = val;
+      return this;
+    }
+
+    public CosmosTableMetadata build() {
+      return new CosmosTableMetadata(this);
+    }
   }
 }
