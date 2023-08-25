0a7461c5a2ce8fb9093b7d4f709780dcb004f8cf
Mitsunori Komatsu <komamitsu@gmail.com>
2023-08-25 12:38:54 +0900

    [skip ci] Make it more configurable

--- core/src/main/java/com/scalar/db/transaction/consensuscommit/replication/server/LogApplier.java
+++ core/src/main/java/com/scalar/db/transaction/consensuscommit/replication/server/LogApplier.java
@@ -1,9 +1,115 @@
 package com.scalar.db.transaction.consensuscommit.replication.server;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.scalar.db.service.StorageFactory;
+import com.scalar.db.transaction.consensuscommit.replication.repository.CoordinatorStateRepository;
+import com.scalar.db.transaction.consensuscommit.replication.repository.ReplicationRecordRepository;
+import com.scalar.db.transaction.consensuscommit.replication.repository.ReplicationTransactionRepository;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Properties;
+
 public class LogApplier {
-  // TODO: Make these configurable
-  private static final String REPLICATION_DB_NAMESPACE = "replication";
-  private static final String REPLICATION_DB_TABLE = "transactions";
+  private static final String ENV_VAR_BACKUP_SCALARDB_CONFIG = "LOG_APPLIER_BACKUP_SCALARDB_CONFIG";
+  private static final String ENV_VAR_REPLICATION_CONFIG = "LOG_APPLIER_REPLICATION_CONFIG";
+  private static final String ENV_VAR_COORDINATOR_STATE_CONFIG =
+      "LOG_APPLIER_COORDINATOR_STATE_CONFIG";
+  private static final String ENV_VAR_NUM_OF_DISTRIBUTOR_THREADS =
+      "LOG_APPLIER_NUM_OF_DISTRIBUTOR_THREADS";
+  private static final String ENV_VAR_NUM_OF_RECORD_WRITER_THREADS =
+      "LOG_APPLIER_NUM_OF_RECORD_WRITER_THREADS";
+  private static final String ENV_VAR_TRANSACTION_FETCH_SIZE =
+      "LOG_APPLIER_ENV_VAR_TRANSACTION_FETCH_SIZE";
+
   private static final int REPLICATION_DB_PARTITION_SIZE = 256;
-  private static final int REPLICATION_DB_THREAD_SIZE = 8;
+
+  public static void main(String[] args) throws IOException {
+    String backupScalarDbConfigPath = System.getenv(ENV_VAR_BACKUP_SCALARDB_CONFIG);
+    if (backupScalarDbConfigPath == null) {
+      throw new IllegalArgumentException(
+          "Backup ScalarDB config file path isn't specified. key:"
+              + ENV_VAR_BACKUP_SCALARDB_CONFIG);
+    }
+
+    String replicationDbConfigPath = System.getenv(ENV_VAR_REPLICATION_CONFIG);
+    if (replicationDbConfigPath == null) {
+      throw new IllegalArgumentException(
+          "Replication config file path isn't specified. key:" + ENV_VAR_REPLICATION_CONFIG);
+    }
+
+    String coordinatorStateConfigPath = System.getenv(ENV_VAR_COORDINATOR_STATE_CONFIG);
+    if (coordinatorStateConfigPath == null) {
+      throw new IllegalArgumentException(
+          "CoordinatorState config file path isn't specified. key:"
+              + ENV_VAR_COORDINATOR_STATE_CONFIG);
+    }
+
+    int numOfDistributorThreads = 16;
+    if (System.getenv(ENV_VAR_NUM_OF_DISTRIBUTOR_THREADS) != null) {
+      numOfDistributorThreads = Integer.parseInt(System.getenv(ENV_VAR_NUM_OF_DISTRIBUTOR_THREADS));
+    }
+
+    int numOfRecordWriterThreads = 16;
+    if (System.getenv(ENV_VAR_NUM_OF_RECORD_WRITER_THREADS) != null) {
+      numOfRecordWriterThreads =
+          Integer.parseInt(System.getenv(ENV_VAR_NUM_OF_RECORD_WRITER_THREADS));
+    }
+
+    int transactionFetchSize = 16;
+    if (System.getenv(ENV_VAR_TRANSACTION_FETCH_SIZE) != null) {
+      transactionFetchSize = Integer.parseInt(System.getenv(ENV_VAR_TRANSACTION_FETCH_SIZE));
+    }
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    CoordinatorStateRepository coordinatorStateRepository =
+        new CoordinatorStateRepository(
+            StorageFactory.create(coordinatorStateConfigPath).getStorage(), "coordinator", "state");
+
+    ReplicationTransactionRepository replicationTransactionRepository =
+        new ReplicationTransactionRepository(
+            StorageFactory.create(replicationDbConfigPath).getStorage(),
+            objectMapper,
+            "replication",
+            "transactions");
+
+    ReplicationRecordRepository replicationRecordRepository =
+        new ReplicationRecordRepository(
+            StorageFactory.create(replicationDbConfigPath).getStorage(),
+            objectMapper,
+            "replication",
+            "records");
+
+    Properties backupScalarDbProps = new Properties();
+    try (InputStream in =
+        Files.newInputStream(Paths.get(backupScalarDbConfigPath), StandardOpenOption.READ)) {
+      backupScalarDbProps.load(in);
+    }
+    RecordWriterThread recordWriter =
+        new RecordWriterThread(
+                numOfRecordWriterThreads, replicationRecordRepository, backupScalarDbProps)
+            .run();
+
+    DistributorThread distributorThread =
+        new DistributorThread(
+                REPLICATION_DB_PARTITION_SIZE,
+                numOfDistributorThreads,
+                transactionFetchSize,
+                coordinatorStateRepository,
+                replicationTransactionRepository,
+                replicationRecordRepository,
+                recordWriter.queue())
+            .run();
+
+    Runtime.getRuntime()
+        .addShutdownHook(
+            new Thread(
+                () -> {
+                  distributorThread.close();
+                  recordWriter.close();
+                }));
+  }
 }
