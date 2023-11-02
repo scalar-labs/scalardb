a011f93ff8a14bd7b00b049e434ca696054a68db
Mitsunori Komatsu <komamitsu@gmail.com>
2023-11-02 22:04:05 +0900

    [skip ci] WIP: Group commit for coordinator table

--- core/src/main/java/com/scalar/db/transaction/consensuscommit/Snapshot.java
+++ core/src/main/java/com/scalar/db/transaction/consensuscommit/Snapshot.java
@@ -51,6 +51,7 @@ import org.slf4j.LoggerFactory;
 public class Snapshot {
   private static final Logger logger = LoggerFactory.getLogger(Snapshot.class);
   private final String id;
+  private String parentId;
   private final Isolation isolation;
   private final SerializableStrategy strategy;
   private final TransactionTableMetadataManager tableMetadataManager;
@@ -101,7 +102,20 @@ public class Snapshot {
 
   @Nonnull
   public String getId() {
-    return id;
+    if (parentId == null) {
+      return id;
+    } else {
+      return parentId + ":" + id;
+    }
+  }
+
+  @Nullable
+  public String getParentId() {
+    return parentId;
+  }
+
+  public void setParentId(String parentId) {
+    this.parentId = parentId;
   }
 
   @VisibleForTesting
