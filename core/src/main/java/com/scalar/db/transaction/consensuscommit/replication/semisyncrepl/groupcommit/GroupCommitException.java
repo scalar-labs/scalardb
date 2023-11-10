f8249f070730d99198b0b5a7b07c2f4d8332a1b6
Mitsunori Komatsu <komamitsu@gmail.com>
2023-11-10 09:29:26 +0900

    [skip ci] Fix error handling

--- core/src/main/java/com/scalar/db/transaction/consensuscommit/replication/semisyncrepl/groupcommit/GroupCommitException.java
+++ core/src/main/java/com/scalar/db/transaction/consensuscommit/replication/semisyncrepl/groupcommit/GroupCommitException.java
@@ -1,4 +1,4 @@
-package com.scalar.db.transaction.consensuscommit.replication.semisyncrepl.client;
+package com.scalar.db.transaction.consensuscommit.replication.semisyncrepl.groupcommit;
 
 public class GroupCommitException extends Exception {
   public GroupCommitException(String message, Throwable cause) {
