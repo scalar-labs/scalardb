2ed109b701eef4474e0e1d17bddff1b37ea116dc
Mitsunori Komatsu <komamitsu@gmail.com>
2023-11-13 18:23:09 +0900

    [skip ci] WIP: Fixing bugs

--- core/src/main/java/com/scalar/db/transaction/consensuscommit/replication/semisyncrepl/groupcommit/GroupCommitCascadeException.java
+++
@@ -1,7 +0,0 @@
-package com.scalar.db.transaction.consensuscommit.replication.semisyncrepl.groupcommit;
-
-public class GroupCommitCascadeException extends Exception {
-  public GroupCommitCascadeException(String message, Throwable cause) {
-    super(message, cause);
-  }
-}
