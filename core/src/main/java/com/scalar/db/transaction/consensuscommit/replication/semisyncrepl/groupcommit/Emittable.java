3f2c39f2ccaa0689b09871a744694157bd26f105
Mitsunori Komatsu <komamitsu@gmail.com>
2023-11-14 22:03:43 +0900

    [skip ci] WIP: Experiment to avoid cascade abort

--- core/src/main/java/com/scalar/db/transaction/consensuscommit/replication/semisyncrepl/groupcommit/Emittable.java
+++ core/src/main/java/com/scalar/db/transaction/consensuscommit/replication/semisyncrepl/groupcommit/Emittable.java
@@ -4,5 +4,7 @@ import java.util.List;
 
 @FunctionalInterface
 public interface Emittable<V> {
+  // FIXME This should receive K key so that CommitHandler can abort the transactions when no
+  // snapshots
   void execute(List<V> values);
 }
