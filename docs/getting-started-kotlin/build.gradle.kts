336265773fce78303f8895e713f66bb7b535135a
Toshihiro Suzuki <brfrn169@gmail.com>
2023-12-04 23:23:18 +0900

    Upgrade the version to 3.11.0-alpha.1

--- docs/getting-started-kotlin/build.gradle.kts
+++ docs/getting-started-kotlin/build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.scalar-labs", "scalardb", "3.10.1")
+    implementation("com.scalar-labs", "scalardb", "3.11.0-alpha.1")
     testImplementation(kotlin("test"))
 }
 
