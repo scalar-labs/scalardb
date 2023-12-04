536f2f3e97aa7e57955e21978a679bcc9b6bc93a
Mitsunori Komatsu <komamitsu@gmail.com>
2023-12-04 15:14:49 +0900

    [skip ci] Improve GroupCommitter3Test

--- core/src/test/java/com/scalar/db/transaction/consensuscommit/replication/semisyncrepl/groupcommit/GroupCommitter3Test.java
+++ core/src/test/java/com/scalar/db/transaction/consensuscommit/replication/semisyncrepl/groupcommit/GroupCommitter3Test.java
@@ -72,100 +72,158 @@ class GroupCommitter3Test {
   // 'com.scalar.db.transaction.consensuscommit.replication.semisyncrepl.groupcommit.GroupCommitter3Test'
   @Test
   void benchmark() throws ExecutionException, InterruptedException, TimeoutException {
-    int numOfThreads = 512;
-    int numOfRequests = 100000;
-    int averagePrepareWaitInMillis = 40;
-    int multiplexerInMillis = 300;
-    int maxCommitWaitInMillis = 40;
+    // Warmup
+    benchmarkInternal(
+        // For Benchmarker:
+        // NumOfThreads,NumOfRequests
+        256,
+        4000,
+        // AveragePrepareWaitInMillis,MultiplexerInMillis,MaxCommitWaitInMillis
+        0,
+        0,
+        0,
+        // For Group Commit
+        // NumOfRetentionValues,SizeFixExpirationInMillis,TimeoutExpirationInMillis,NumOfThreads
+        2,
+        10,
+        100,
+        64);
+
+    /*
+    // Benchmark for Micro-benchmark
+    benchmarkInternal(
+        // For Benchmarker:
+        // NumOfThreads,NumOfRequests
+        4096, 100000,
+        // AveragePrepareWaitInMillis,MultiplexerInMillis,MaxCommitWaitInMillis
+        0, 0, 0,
+        // For Group Commit
+        // NumOfRetentionValues,SizeFixExpirationInMillis,TimeoutExpirationInMillis,NumOfThreads
+        32, 80, 800, 64);
+     */
+
+    // Benchmark for Production case
+    benchmarkInternal(
+        // For Benchmarker:
+        // NumOfThreads,NumOfRequests
+        4096,
+        100000,
+        // AveragePrepareWaitInMillis,MultiplexerInMillis,MaxCommitWaitInMillis
+        40,
+        400,
+        40,
+        // For Group Commit
+        // NumOfRetentionValues,SizeFixExpirationInMillis,TimeoutExpirationInMillis,NumOfThreads
+        32,
+        80,
+        800,
+        64);
+  }
+
+  void benchmarkInternal(
+      int bmNumOfThreads,
+      int bmNumOfRequests,
+      int bmAveragePrepareWaitInMillis,
+      int bmMultiplexerInMillis,
+      int bmMaxCommitWaitInMillis,
+      int gcNumOfRetentionValues,
+      int gcSizeFixExpirationInMillis,
+      int gcTimeoutExpirationInMillis,
+      int gcNumOfThreads)
+      throws ExecutionException, InterruptedException, TimeoutException {
     Random rand = new Random();
     AtomicInteger retry = new AtomicInteger();
     Map<String, Boolean> emittedKeys = new ConcurrentHashMap<>();
 
-    GroupCommitter3<String, Value> groupCommitter =
-        new GroupCommitter3<>("test", 10, 400, 32, 5, 64, new MyKeyManipulator());
-    groupCommitter.setEmitter(
-        ((parentKey, values) -> {
-          try {
-            TimeUnit.MILLISECONDS.sleep(rand.nextInt(maxCommitWaitInMillis));
-          } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-          }
-          for (Value v : values) {
-            if (emittedKeys.put(v.v, true) != null) {
-              throw new RuntimeException(v + " is already set");
+    try (GroupCommitter3<String, Value> groupCommitter =
+        new GroupCommitter3<>(
+            "test",
+            gcSizeFixExpirationInMillis,
+            gcTimeoutExpirationInMillis,
+            gcNumOfRetentionValues,
+            5,
+            gcNumOfThreads,
+            new MyKeyManipulator())) {
+      groupCommitter.setEmitter(
+          ((parentKey, values) -> {
+            try {
+              TimeUnit.MILLISECONDS.sleep(bmMaxCommitWaitInMillis);
+            } catch (InterruptedException e) {
+              throw new RuntimeException(e);
             }
-          }
-        }));
+            for (Value v : values) {
+              if (emittedKeys.put(v.v, true) != null) {
+                throw new RuntimeException(v + " is already set");
+              }
+            }
+          }));
 
-    List<KeyAndFuture> futures = new ArrayList<>();
-    ScheduledExecutorService monitor =
-        Executors.newSingleThreadScheduledExecutor(
-            new ThreadFactoryBuilder().setDaemon(true).build());
-    monitor.scheduleAtFixedRate(
-        () -> {
-          System.err.println("future.size:" + futures.size());
-        },
-        1,
-        1,
-        TimeUnit.SECONDS);
+      List<KeyAndFuture> futures = new ArrayList<>();
+      ScheduledExecutorService monitor =
+          Executors.newSingleThreadScheduledExecutor(
+              new ThreadFactoryBuilder().setDaemon(true).build());
+      monitor.scheduleAtFixedRate(
+          () -> System.err.println("future.size:" + futures.size()), 1, 1, TimeUnit.SECONDS);
 
-    ExecutorService executorService =
-        Executors.newFixedThreadPool(
-            numOfThreads, new ThreadFactoryBuilder().setDaemon(true).build());
-    long start = System.currentTimeMillis();
-    for (int i = 0; i < numOfRequests; i++) {
-      int finalI = i;
-      String childKey = String.format("%016d", finalI);
-      Value value = new Value("ORIG-KEY: " + childKey);
-      futures.add(
-          new KeyAndFuture(
-              childKey,
-              executorService.submit(
-                  () -> {
-                    while (true) {
-                      try {
-                        String fullKey = groupCommitter.reserve(childKey);
-                        int waitInMillis =
-                            (int)
-                                (averagePrepareWaitInMillis
-                                    + rand.nextGaussian() * multiplexerInMillis);
-                        if (waitInMillis > 0) {
-                          TimeUnit.MILLISECONDS.sleep(averagePrepareWaitInMillis);
+      ExecutorService executorService =
+          Executors.newFixedThreadPool(
+              bmNumOfThreads, new ThreadFactoryBuilder().setDaemon(true).build());
+      long start = System.currentTimeMillis();
+      for (int i = 0; i < bmNumOfRequests; i++) {
+        int finalI = i;
+        String childKey = String.format("%016d", finalI);
+        Value value = new Value("ORIG-KEY: " + childKey);
+        futures.add(
+            new KeyAndFuture(
+                childKey,
+                executorService.submit(
+                    () -> {
+                      while (true) {
+                        try {
+                          String fullKey = groupCommitter.reserve(childKey);
+                          int waitInMillis =
+                              (int)
+                                  (bmAveragePrepareWaitInMillis
+                                      + rand.nextGaussian() * bmMultiplexerInMillis);
+                          TimeUnit.MILLISECONDS.sleep(
+                              Math.max(waitInMillis, bmAveragePrepareWaitInMillis));
+                          groupCommitter.ready(fullKey, value);
+                          break;
+                        } catch (GroupCommitAlreadyClosedException
+                            | GroupCommitAlreadySizeFixedException e) {
+                          retry.incrementAndGet();
                         }
-                        groupCommitter.ready(fullKey, value);
-                        break;
-                      } catch (GroupCommitAlreadyClosedException
-                          | GroupCommitAlreadySizeFixedException e) {
-                        retry.incrementAndGet();
                       }
-                    }
-                    return null;
-                  })));
-    }
-
-    for (KeyAndFuture kf : futures) {
-      try {
-        System.err.println("Getting the future of " + kf.key);
-        kf.future.get(10, TimeUnit.SECONDS);
-      } catch (TimeoutException e) {
-        System.out.println("Timeout: Key=" + kf.key);
-        throw e;
+                      return null;
+                    })));
       }
-    }
-    System.err.println("Duration(ms): " + (System.currentTimeMillis() - start));
-    System.err.println("Retry: " + retry.get());
 
-    start = System.currentTimeMillis();
-    for (int i = 0; i < numOfRequests; i++) {
-      String expectedKey = "ORIG-KEY: " + String.format("%016d", i);
-      if (!emittedKeys.containsKey(expectedKey)) {
-        throw new AssertionError(expectedKey + " is not found");
+      for (KeyAndFuture kf : futures) {
+        try {
+          System.err.println("Getting the future of " + kf.key);
+          kf.future.get(10, TimeUnit.SECONDS);
+        } catch (TimeoutException e) {
+          System.out.println("Timeout: Key=" + kf.key);
+          throw e;
+        }
       }
-      // System.err.println("Confirmed the key is contained: Key=" + expectedKey);
-    }
-    assertEquals(numOfRequests, emittedKeys.size());
+      long duration = System.currentTimeMillis() - start;
+      System.err.println("Duration(ms): " + duration);
+      System.err.println("TPS:          " + (((double) bmNumOfRequests) / (duration / 1000.0)));
+      System.err.println("Retry:        " + retry.get());
 
-    System.err.println("Checked all the keys");
-    System.err.println("Duration(ms): " + (System.currentTimeMillis() - start));
+      start = System.currentTimeMillis();
+      for (int i = 0; i < bmNumOfRequests; i++) {
+        String expectedKey = "ORIG-KEY: " + String.format("%016d", i);
+        if (!emittedKeys.containsKey(expectedKey)) {
+          throw new AssertionError(expectedKey + " is not found");
+        }
+        // System.err.println("Confirmed the key is contained: Key=" + expectedKey);
+      }
+      assertEquals(bmNumOfRequests, emittedKeys.size());
+
+      System.err.println("Checked all the keys");
+      System.err.println("Duration(ms): " + (System.currentTimeMillis() - start));
+    }
   }
 }
