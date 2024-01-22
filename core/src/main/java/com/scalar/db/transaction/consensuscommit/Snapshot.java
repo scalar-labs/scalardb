3d51ca8ee0a02dd4faa76113b31b5959f316baeb
Mitsunori Komatsu <komamitsu@gmail.com>
2024-01-22 17:52:00 +0900

    [skip ci] Fix compile error

--- core/src/main/java/com/scalar/db/transaction/consensuscommit/Snapshot.java
+++ core/src/main/java/com/scalar/db/transaction/consensuscommit/Snapshot.java
@@ -1,6 +1,7 @@
 package com.scalar.db.transaction.consensuscommit;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.MoreObjects;
 import com.google.common.collect.ComparisonChain;
 import com.scalar.db.api.ConditionalExpression;
 import com.scalar.db.api.ConditionalExpression.Operator;
