87a8ecd442260c630a2fb3bbf5078d5bbc3d5079
Mitsunori Komatsu <komamitsu@gmail.com>
2024-02-02 19:49:38 +0900

    [skip ci] Make GroupCommitterBench throw exception

--- core/src/main/java/com/scalar/db/transaction/consensuscommit/replication/semisyncrepl/groupcommit/GroupCommitterBench.java
+++ core/src/main/java/com/scalar/db/transaction/consensuscommit/replication/semisyncrepl/groupcommit/GroupCommitterBench.java
@@ -22,6 +22,12 @@ import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicInteger;
 
 class GroupCommitterBench {
+  private class ExpectedException extends RuntimeException {
+    public ExpectedException(String message) {
+      super(message);
+    }
+  }
+
   static class MyKeyManipulator implements KeyManipulator<String> {
     @Override
     public String createParentKey() {
@@ -141,6 +147,8 @@ class GroupCommitterBench {
         0,
         0,
         0,
+        0,
+        0,
         // For Group Commit
         new GroupCommitParams(32, 8, 10, 100));
 
@@ -157,6 +165,8 @@ class GroupCommitterBench {
           0, // AveragePrepareWaitInMillis
           0, // MultiplexerInMillis
           0, // MaxCommitWaitInMillis
+          1, // ErrorBeforeReadyPercentage
+          1, // ErrorAfterReadyPercentage
           // For Group Commit
           new GroupCommitParams(64, 40, 20, 200));
     } else {
@@ -172,6 +182,8 @@ class GroupCommitterBench {
                 100, // AveragePrepareWaitInMillis
                 400, // MultiplexerInMillis
                 100, // MaxCommitWaitInMillis
+                1, // ErrorBeforeReadyPercentage
+                1, // ErrorAfterReadyPercentage
                 // For Group Commit
                 param);
         results.put(param, result);
@@ -189,11 +201,14 @@ class GroupCommitterBench {
       int bmAveragePrepareWaitInMillis,
       int bmMultiplexerInMillis,
       int bmMaxCommitWaitInMillis,
+      int bmErrorBeforeReadyPercentage,
+      int bmErrorAfterReadyPercentage,
       GroupCommitParams groupCommitParams)
       throws ExecutionException, InterruptedException, TimeoutException {
     Random rand = new Random();
     AtomicInteger retry = new AtomicInteger();
     Map<String, Boolean> emittedKeys = new ConcurrentHashMap<>();
+    Map<String, Boolean> failedKeys = new ConcurrentHashMap<>();
 
     try (GroupCommitter3<String, Value> groupCommitter =
         new GroupCommitter3<>(
@@ -205,13 +220,21 @@ class GroupCommitterBench {
             groupCommitParams.numOfRetentionValues,
             new MyKeyManipulator())) {
       groupCommitter.setEmitter(
-          ((parentKey, values) -> {
+          (parentKey, values) -> {
             try {
               if (bmMaxCommitWaitInMillis > 0) {
                 int waitInMillis = rand.nextInt(bmMaxCommitWaitInMillis);
                 System.out.printf(
                     "Waiting for commit. ParentKey=%s, Duration=%d ms \n", parentKey, waitInMillis);
                 TimeUnit.MILLISECONDS.sleep(waitInMillis);
+              }
+              if (bmErrorAfterReadyPercentage > rand.nextInt(100)) {
+                for (Value v : values) {
+                  if (failedKeys.put(v.v, true) != null) {
+                    throw new RuntimeException(v + " is already set");
+                  }
+                }
+                throw new ExpectedException("Error after READY");
               }
             } catch (InterruptedException e) {
               throw new RuntimeException(e);
@@ -221,7 +244,7 @@ class GroupCommitterBench {
                 throw new RuntimeException(v + " is already set");
               }
             }
-          }));
+          });
 
       List<KeyAndFuture> futures = new ArrayList<>();
       /*
@@ -259,6 +282,12 @@ class GroupCommitterBench {
                                   fullKey, waitInMillis);
                               TimeUnit.MILLISECONDS.sleep(waitInMillis);
                             }
+                            if (bmErrorBeforeReadyPercentage > rand.nextInt(100)) {
+                              if (failedKeys.put(value.v, true) != null) {
+                                throw new RuntimeException(value.v + " is already set");
+                              }
+                              throw new ExpectedException("Error before READY");
+                            }
                             groupCommitter.ready(fullKey, value);
                             break;
                           } catch (GroupCommitAlreadyClosedException
@@ -274,6 +303,16 @@ class GroupCommitterBench {
           try {
             System.err.println("Getting the future of " + kf.key);
             kf.future.get(10, TimeUnit.SECONDS);
+          } catch (ExecutionException e) {
+            if (e.getCause() instanceof ExpectedException
+                || (e.getCause() instanceof GroupCommitException
+                    && e.getCause().getCause() instanceof ExpectedException)) {
+              // Expected ???
+            } else {
+              throw e;
+            }
+          } catch (ExpectedException e) {
+            // Expected.
           } catch (TimeoutException e) {
             System.out.println("Timeout: Key=" + kf.key);
             throw e;
@@ -287,8 +326,8 @@ class GroupCommitterBench {
 
         start = System.currentTimeMillis();
         for (int i = 0; i < bmNumOfRequests; i++) {
-          String expectedKey = "ORIG-KEY: " + String.format("%016d", i);
-          if (!emittedKeys.containsKey(expectedKey)) {
+          String expectedKey = key(i);
+          if (!emittedKeys.containsKey(expectedKey) && !failedKeys.containsKey(expectedKey)) {
             throw new AssertionError(expectedKey + " is not found");
           }
           // System.err.println("Confirmed the key is contained: Key=" + expectedKey);
@@ -301,6 +340,10 @@ class GroupCommitterBench {
         MoreExecutors.shutdownAndAwaitTermination(executorService, 10, TimeUnit.SECONDS);
       }
     }
+  }
+
+  private String key(int i) {
+    return "ORIG-KEY: " + String.format("%016d", i);
   }
 
   public static void main(String[] args)
