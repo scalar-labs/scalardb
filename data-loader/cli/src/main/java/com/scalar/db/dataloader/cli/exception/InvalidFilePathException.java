444a5ebe699770607fcebe77a5321e005a1d38ff
Peckstadt Yves <peckstadt.yves@gmail.com>
2024-05-21 11:37:24 +0900

    Remove unused class

--- data-loader/cli/src/main/java/com/scalar/db/dataloader/cli/exception/InvalidFilePathException.java
+++
@@ -1,7 +0,0 @@
-package com.scalar.db.dataloader.cli.exception;
-
-public class InvalidFilePathException extends Exception {
-  public InvalidFilePathException(String message) {
-    super(message);
-  }
-}
