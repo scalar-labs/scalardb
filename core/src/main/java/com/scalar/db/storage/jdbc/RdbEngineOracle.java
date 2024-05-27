8b60defb4302e11893398b977035fa4ac18465ff
Toshihiro Suzuki <brfrn169@gmail.com>
2024-05-28 00:13:12 +0900

    Backport to branch(3.12) : Change hard-coded password for Oracle user in JDBC adapter (#1784)

--- core/src/main/java/com/scalar/db/storage/jdbc/RdbEngineOracle.java
+++ core/src/main/java/com/scalar/db/storage/jdbc/RdbEngineOracle.java
@@ -27,7 +27,7 @@ public class RdbEngineOracle implements RdbEngineStrategy {
   @Override
   public String[] createNamespaceSqls(String fullNamespace) {
     return new String[] {
-      "CREATE USER " + fullNamespace + " IDENTIFIED BY \"oracle\"",
+      "CREATE USER " + fullNamespace + " IDENTIFIED BY \"Oracle1234!@#$\"",
       "ALTER USER " + fullNamespace + " quota unlimited on USERS",
     };
   }
@@ -79,7 +79,7 @@ public class RdbEngineOracle implements RdbEngineStrategy {
   @Override
   public String[] createMetadataSchemaIfNotExistsSql(String metadataSchema) {
     return new String[] {
-      "CREATE USER " + enclose(metadataSchema) + " IDENTIFIED BY \"oracle\"",
+      "CREATE USER " + enclose(metadataSchema) + " IDENTIFIED BY \"Oracle1234!@#$\"",
       "ALTER USER " + enclose(metadataSchema) + " quota unlimited on USERS",
     };
   }
