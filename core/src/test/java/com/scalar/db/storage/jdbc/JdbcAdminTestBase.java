c3e14c08b81fc8873c09db1eadb211d2d643728e
Toshihiro Suzuki <brfrn169@gmail.com>
2024-05-28 00:13:26 +0900

    Backport to branch(3.10) : Change hard-coded password for Oracle user in JDBC adapter (#1786)

--- core/src/test/java/com/scalar/db/storage/jdbc/JdbcAdminTestBase.java
+++ core/src/test/java/com/scalar/db/storage/jdbc/JdbcAdminTestBase.java
@@ -294,7 +294,7 @@ public abstract class JdbcAdminTestBase {
       throws ExecutionException, SQLException {
     createNamespace_forX_shouldExecuteCreateNamespaceStatement(
         RdbEngine.ORACLE,
-        "CREATE USER \"my_ns\" IDENTIFIED BY \"oracle\"",
+        "CREATE USER \"my_ns\" IDENTIFIED BY \"Oracle1234!@#$\"",
         "ALTER USER \"my_ns\" quota unlimited on USERS");
   }
 
@@ -484,7 +484,7 @@ public abstract class JdbcAdminTestBase {
         "ALTER TABLE \"my_ns\".\"foo_table\" INITRANS 3 MAXTRANS 255",
         "CREATE INDEX \"index_my_ns_foo_table_c4\" ON \"my_ns\".\"foo_table\" (\"c4\")",
         "CREATE INDEX \"index_my_ns_foo_table_c1\" ON \"my_ns\".\"foo_table\" (\"c1\")",
-        "CREATE USER \"" + tableMetadataSchemaName + "\" IDENTIFIED BY \"oracle\"",
+        "CREATE USER \"" + tableMetadataSchemaName + "\" IDENTIFIED BY \"Oracle1234!@#$\"",
         "ALTER USER \"" + tableMetadataSchemaName + "\" quota unlimited on USERS",
         "CREATE TABLE \""
             + tableMetadataSchemaName
@@ -738,7 +738,7 @@ public abstract class JdbcAdminTestBase {
         "CREATE UNIQUE INDEX \"my_ns.foo_table_clustering_order_idx\" ON \"my_ns\".\"foo_table\" (\"c3\" ASC,\"c1\" DESC,\"c4\" ASC)",
         "CREATE INDEX \"index_my_ns_foo_table_c4\" ON \"my_ns\".\"foo_table\" (\"c4\")",
         "CREATE INDEX \"index_my_ns_foo_table_c1\" ON \"my_ns\".\"foo_table\" (\"c1\")",
-        "CREATE USER \"" + tableMetadataSchemaName + "\" IDENTIFIED BY \"oracle\"",
+        "CREATE USER \"" + tableMetadataSchemaName + "\" IDENTIFIED BY \"Oracle1234!@#$\"",
         "ALTER USER \"" + tableMetadataSchemaName + "\" quota unlimited on USERS",
         "CREATE TABLE \""
             + tableMetadataSchemaName
@@ -1793,7 +1793,7 @@ public abstract class JdbcAdminTestBase {
         RdbEngine.ORACLE,
         "SELECT 1 FROM \"my_ns\".\"foo_table\" FETCH FIRST 1 ROWS ONLY",
         "SELECT 1 FROM \"" + tableMetadataSchemaName + "\".\"metadata\" FETCH FIRST 1 ROWS ONLY",
-        "CREATE USER \"" + tableMetadataSchemaName + "\" IDENTIFIED BY \"oracle\"",
+        "CREATE USER \"" + tableMetadataSchemaName + "\" IDENTIFIED BY \"Oracle1234!@#$\"",
         "ALTER USER \"" + tableMetadataSchemaName + "\" quota unlimited on USERS",
         "CREATE TABLE \""
             + tableMetadataSchemaName
@@ -2607,7 +2607,7 @@ public abstract class JdbcAdminTestBase {
             "CREATE USER "
                 + rdbEngineStrategy.enclose(tableMetadataSchemaName)
                 + " IDENTIFIED BY "
-                + rdbEngineStrategy.enclose("oracle"));
+                + rdbEngineStrategy.enclose("Oracle1234!@#$"));
         statements.add(
             "ALTER USER "
                 + rdbEngineStrategy.enclose(tableMetadataSchemaName)
