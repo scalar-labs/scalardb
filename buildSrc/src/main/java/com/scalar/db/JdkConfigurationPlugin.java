1da16c7cff9ce55c7f2340b8047f60468f8ca80a
Vincent Guilpain <vincent.guilpain@gmail.com>
2024-05-29 15:39:39 +0900

    Make Java version and vendor configurable

--- buildSrc/src/main/java/com/scalar/db/JdkConfigurationPlugin.java
+++ buildSrc/src/main/java/com/scalar/db/JdkConfigurationPlugin.java
@@ -1,10 +1,11 @@
 package com.scalar.db;
 
 import java.util.Objects;
+import java.util.function.Consumer;
 import javax.annotation.Nullable;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
-import org.gradle.api.tasks.compile.JavaCompile;
+import org.gradle.api.plugins.JavaPluginExtension;
 import org.gradle.api.tasks.testing.Test;
 import org.gradle.jvm.toolchain.JavaLanguageVersion;
 import org.gradle.jvm.toolchain.JavaToolchainService;
@@ -14,59 +15,66 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Plugin configuring compilation tasks to use JDK 8 (temurin) and integration test tasks to use a
- * given SDK defined by the following Gradle properties:
+ * Plugin configuring all tasks and integration test tasks to use a given JDK defined by the
+ * following Gradle properties:
  *
  * <ul>
- *   <li>`integrationTestJavaVersion` : configure all tasks name starting with "integrationTest" to
- *       run with the given SDK version
- *   <li>`integrationTestJavaVendor` : configure all tasks name starting with "integrationTest" to
- *       run with the given SDK vendor
+ *   <li>`javaVersion` : configure all Java related tasks to use the given JDK version. The default
+ *       java version 8.
+ *   <li>`javaVendor` : configure all Java related tasks to use the given JDK vendor. The default
+ *       java vendor is Adoptium (also known as Temurin)
+ *   <li>`integrationTestJavaRuntimeVersion` : configure all test tasks name starting with
+ *       "integrationTest" to run with the given JDK version. The default Java runtime version is 8.
+ *   <li>`integrationTestJavaRuntimeVendor` : configure all test tasks name starting with
+ *       "integrationTest" to run with the given JDK vendor. The default Java runtime vendor is
+ *       Adoptium (also known as Temurin)
  * </ul>
  *
- * Usage example using the CLI :
+ * <p>Usage example using the CLI:
+ *
+ * <p>1. To use JDK 11 (amazon) for all Java tasks including integration tests
  *
  * <pre><code>
- *   gradle integrationTestJdbc -PintegrationTestJavaVersion=11 -PintegrationTestJavaVendor=amazon
+ *   gradle integrationTestJdbc -PjavaVersion=11 -PjavaVendor=amazon
+ * </code></pre>
+ *
+ * 2. To use JDK 11 (amazon) for all Java tasks while having integration test use JDK 17 (microsoft)
+ *
+ * <pre><code>
+ *   gradle integrationTestJdbc -PjavaVersion=11 -PjavaVendor=amazon -PintegrationTestJavaRuntimeVersion=17 -PintegrationTestJavaRuntimeVendor=microsoft
  * </code></pre>
  */
 public class JdkConfigurationPlugin implements Plugin<Project> {
-  private static final Logger logger = LoggerFactory.getLogger(JdkConfigurationPlugin.class);
 
-  private static final JavaLanguageVersion COMPILATION_JAVA_VERSION = JavaLanguageVersion.of(8);
-  private static final JvmVendorSpec COMPILATION_JAVA_VENDOR = JvmVendorSpec.ADOPTIUM;
-  private static final String INTEGRATION_TEST_JAVA_VERSION_PROP = "integrationTestJavaVersion";
-  private static final String INTEGRATION_TEST_JAVA_VENDOR_PROP = "integrationTestJavaVendor";
+  private static final Logger logger = LoggerFactory.getLogger(JdkConfigurationPlugin.class);
+  // JDK 8 (temurin) is used as the default compiler
+  private static final JavaLanguageVersion DEFAULT_JAVA_VERSION = JavaLanguageVersion.of(8);
+  private static final JvmVendorSpec DEFAULT_JAVA_VENDOR = JvmVendorSpec.ADOPTIUM;
+
+  private static final String JAVA_VERSION_PROP = "javaVersion";
+  private static final String JAVA_VENDOR_PROP = "javaVendor";
+  private static final String INTEGRATION_TEST_JAVA_RUNTIME_VERSION_PROP =
+      "integrationTestJavaRuntimeVersion";
+  private static final String INTEGRATION_TEST_JAVA_RUNTIME_VENDOR_PROP =
+      "integrationTestJavaRuntimeVendor";
+
+  private JavaLanguageVersion javaVersion = DEFAULT_JAVA_VERSION;
+  private JvmVendorSpec javaVendor = DEFAULT_JAVA_VENDOR;
   @Nullable private JavaLanguageVersion integrationTestJavaVersion;
   @Nullable private JvmVendorSpec integrationTestJavaVendor;
 
   @Override
   public void apply(@NotNull Project project) {
     parseIntegrationTestInputProperties(project);
-    configureJdkForCompilationTasks(project);
+    configureJdkForAllJavaTasks(project);
     configureJdkForIntegrationTestTasks(project);
   }
 
-  private void configureJdkForCompilationTasks(Project project) {
-    project
-        .getTasks()
-        .withType(JavaCompile.class)
-        .configureEach(
-            javaCompileTask ->
-                javaCompileTask
-                    .getJavaCompiler()
-                    .set(
-                        getJavaToolchainService(project)
-                            .compilerFor(
-                                config -> {
-                                  config.getLanguageVersion().set(COMPILATION_JAVA_VERSION);
-                                  config.getVendor().set(COMPILATION_JAVA_VENDOR);
-                                  logger.debug(
-                                      "Configure task '{}' to use JDK {} ({})",
-                                      javaCompileTask.getName(),
-                                      COMPILATION_JAVA_VERSION,
-                                      COMPILATION_JAVA_VENDOR);
-                                })));
+  private void configureJdkForAllJavaTasks(Project project) {
+    JavaPluginExtension javaPlugin = project.getExtensions().getByType(JavaPluginExtension.class);
+    javaPlugin.getToolchain().getLanguageVersion().set(javaVersion);
+    javaPlugin.getToolchain().getVendor().set(javaVendor);
+    logger.debug("Configure JDK {} ({}) for Java tasks", javaVersion, javaVendor);
   }
 
   private void configureJdkForIntegrationTestTasks(Project project) {
@@ -100,43 +108,45 @@ public class JdkConfigurationPlugin implements Plugin<Project> {
                                 })));
   }
 
-  private static @NotNull JavaToolchainService getJavaToolchainService(Project project) {
-    return project.getExtensions().getByType(JavaToolchainService.class);
-  }
-
   private void parseIntegrationTestInputProperties(Project project) {
-    parseIntegrationTestVersionInputProperty(project);
-    parseIntegrationTestVendorInputProperty(project);
-    if (integrationTestJavaVersion == null && integrationTestJavaVendor != null) {
-      throw new IllegalArgumentException(
-          "When configuring the JDK vendor to run integration test, you also need to set the JDK version using the \""
-              + INTEGRATION_TEST_JAVA_VERSION_PROP
-              + "\" project property.");
+    parseVersionInputProperty(project, JAVA_VERSION_PROP, (version) -> javaVersion = version);
+    parseVendorInputProperty(project, JAVA_VENDOR_PROP, (vendor) -> javaVendor = vendor);
+    parseVersionInputProperty(
+        project,
+        INTEGRATION_TEST_JAVA_RUNTIME_VERSION_PROP,
+        (version) -> integrationTestJavaVersion = version);
+    parseVendorInputProperty(
+        project,
+        INTEGRATION_TEST_JAVA_RUNTIME_VENDOR_PROP,
+        (vendor) -> integrationTestJavaVendor = vendor);
+  }
+
+  private void parseVersionInputProperty(
+      Project project, String property, Consumer<JavaLanguageVersion> attributeSetter) {
+    if (project.hasProperty(property)) {
+      attributeSetter.accept(JavaLanguageVersion.of(Objects.toString(project.property(property))));
     }
   }
 
-  private void parseIntegrationTestVersionInputProperty(Project project) {
-    if (project.hasProperty(INTEGRATION_TEST_JAVA_VERSION_PROP)) {
-      integrationTestJavaVersion =
-          JavaLanguageVersion.of(
-              Objects.toString(project.property(INTEGRATION_TEST_JAVA_VERSION_PROP)));
-    }
-  }
-
-  private void parseIntegrationTestVendorInputProperty(Project project) {
-    if (!project.getRootProject().hasProperty(INTEGRATION_TEST_JAVA_VENDOR_PROP)) {
+  private void parseVendorInputProperty(
+      Project project, String property, Consumer<JvmVendorSpec> attributeSetter) {
+    if (!project.getRootProject().hasProperty(property)) {
       return;
     }
-    String propertyValue = Objects.toString(project.property(INTEGRATION_TEST_JAVA_VENDOR_PROP));
+    String propertyValue = Objects.toString(project.property(property));
     switch (propertyValue) {
       case "corretto":
-        integrationTestJavaVendor = JvmVendorSpec.AMAZON;
+        attributeSetter.accept(JvmVendorSpec.AMAZON);
         break;
       case "temurin":
-        integrationTestJavaVendor = JvmVendorSpec.ADOPTIUM;
+        attributeSetter.accept(JvmVendorSpec.ADOPTIUM);
         break;
       default:
-        integrationTestJavaVendor = JvmVendorSpec.matching(propertyValue);
+        attributeSetter.accept(JvmVendorSpec.matching(propertyValue));
     }
+  }
+
+  private @NotNull JavaToolchainService getJavaToolchainService(Project project) {
+    return project.getExtensions().getByType(JavaToolchainService.class);
   }
 }
