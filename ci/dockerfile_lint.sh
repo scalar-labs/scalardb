84ee875f03d0e78cf5b8e853c4202ab41be776ec
kota2and3kan <47254383+kota2and3kan@users.noreply.github.com>
2024-05-31 18:48:04 +0900

    Use eclipse-temurin base image in Dockerfile

--- ci/dockerfile_lint.sh
+++ ci/dockerfile_lint.sh
@@ -2,7 +2,7 @@
 
 set -e -o pipefail; [[ -n "$DEBUG" ]] && set -x
 
-HADOLINT_VERSION="v1.19.0"
+HADOLINT_VERSION="v2.12.0"
 HADOLINT="${HADOLINT:-docker run --rm -i -v "$(pwd):/mnt" -w "/mnt" "hadolint/hadolint:${HADOLINT_VERSION}" hadolint}"
 
 exec ${HADOLINT} ./Dockerfile
