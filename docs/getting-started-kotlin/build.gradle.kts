5802056f65768f8699e0beb205d3794df438466e
Hiroyuki Yamada <mogwaing@gmail.com>
2024-07-30 15:30:00 +0900

    Backport to branch(3.9) : Fix compilation error when using the JDK 21 for Getting Started with Kotlin (#2120)
    Co-authored-by: Vincent Guilpain <vincent.guilpain@scalar-labs.com>

--- docs/getting-started-kotlin/build.gradle.kts
+++ docs/getting-started-kotlin/build.gradle.kts
@@ -25,6 +25,11 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
 application {
     mainClass.set("MainKt")
 }
