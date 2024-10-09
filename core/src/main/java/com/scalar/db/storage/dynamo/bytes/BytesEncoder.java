d1adea63b2342bb74384e86cdfd66a9d38dc2d74
Vincent Guilpain <vincent.guilpain@scalar-labs.com>
2024-10-09 14:33:07 +0900

    Refactor KeyBytesEncoder to use ColumnVisitor (#2270)

--- core/src/main/java/com/scalar/db/storage/dynamo/bytes/BytesEncoder.java
+++ core/src/main/java/com/scalar/db/storage/dynamo/bytes/BytesEncoder.java
@@ -1,31 +1,31 @@
 package com.scalar.db.storage.dynamo.bytes;
 
 import com.scalar.db.api.Scan.Ordering.Order;
-import com.scalar.db.io.Value;
+import com.scalar.db.io.Column;
 import java.nio.ByteBuffer;
 
 /**
- * A bytes-encoder that encodes a value to bytes while preserving the sort order.
+ * A bytes-encoder that encodes a column to bytes while preserving the sort order.
  *
  * @param <T> the value type
  */
-public interface BytesEncoder<T extends Value<?>> {
+public interface BytesEncoder<T extends Column<?>> {
 
   /**
    * Calculates the encoded bytes length.
    *
-   * @param value a value
+   * @param column a column
    * @param order an order
    * @return the encoded bytes length
    */
-  int encodedLength(T value, Order order);
+  int encodedLength(T column, Order order);
 
   /**
-   * Encodes the value to bytes
+   * Encodes the column to bytes
    *
-   * @param value a value
+   * @param column a column
    * @param order an order
    * @param dst a ByteBuffer to write the encoded bytes
    */
-  void encode(T value, Order order, ByteBuffer dst);
+  void encode(T column, Order order, ByteBuffer dst);
 }
--- core/src/main/java/com/scalar/db/storage/dynamo/bytes/TextBytesEncoder.java
+++ core/src/main/java/com/scalar/db/storage/dynamo/bytes/TextBytesEncoder.java
@@ -4,32 +4,36 @@ import static com.scalar.db.storage.dynamo.bytes.BytesUtils.mask;
 
 import com.scalar.db.api.Scan.Ordering.Order;
 import com.scalar.db.common.error.CoreError;
-import com.scalar.db.io.TextValue;
+import com.scalar.db.io.TextColumn;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import javax.annotation.concurrent.ThreadSafe;
 
 @ThreadSafe
-public class TextBytesEncoder implements BytesEncoder<TextValue> {
+public class TextBytesEncoder implements BytesEncoder<TextColumn> {
   private static final byte TERM = 0x00;
 
   TextBytesEncoder() {}
 
   @Override
-  public int encodedLength(TextValue value, Order order) {
-    assert value.getAsString().isPresent();
-    return value.getAsString().get().getBytes(StandardCharsets.UTF_8).length + 1;
+  public int encodedLength(TextColumn column, Order order) {
+    assert column.getValue().isPresent();
+
+    return column.getValue().get().getBytes(StandardCharsets.UTF_8).length + 1;
   }
 
   @Override
-  public void encode(TextValue value, Order order, ByteBuffer dst) {
-    assert value.getAsString().isPresent();
-    if (value.getAsString().get().contains("\u0000")) {
+  public void encode(TextColumn column, Order order, ByteBuffer dst) {
+    assert column.getValue().isPresent();
+
+    String value = column.getValue().get();
+
+    if (value.contains("\u0000")) {
       throw new IllegalArgumentException(
           CoreError.DYNAMO_ENCODER_CANNOT_ENCODE_TEXT_VALUE_CONTAINING_0X0000.buildMessage());
     }
 
-    byte[] bytes = value.getAsString().get().getBytes(StandardCharsets.UTF_8);
+    byte[] bytes = value.getBytes(StandardCharsets.UTF_8);
     for (byte b : bytes) {
       dst.put(mask(b, order));
     }
