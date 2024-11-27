350c31a6dc8328b5cdebe909c8d64520c382ecbf
Hiroyuki Yamada <mogwaing@gmail.com>
2024-11-27 13:40:26 +0900

    Backport to branch(3.10) : [3.10, 3.11, 3.12] Backport data loader (#2365)
    Co-authored-by: Toshihiro Suzuki <brfrn169@gmail.com>
    Co-authored-by: Peckstadt Yves <peckstadt.yves@gmail.com>

---
+++ data-loader/core/src/main/java/com/scalar/db/dataloader/core/ColumnKeyValue.java
@@ -0,0 +1,36 @@
+package com.scalar.db.dataloader.core;
+
+/** Represents a key-value pair for a column and its corresponding value. */
+public class ColumnKeyValue {
+  private final String columnName;
+  private final String columnValue;
+
+  /**
+   * Class constructor
+   *
+   * @param columnName the column name
+   * @param columnValue the column value
+   */
+  public ColumnKeyValue(String columnName, String columnValue) {
+    this.columnName = columnName;
+    this.columnValue = columnValue;
+  }
+
+  /**
+   * Gets the column name
+   *
+   * @return the column name
+   */
+  public String getColumnName() {
+    return columnName;
+  }
+
+  /**
+   * Gets the column value
+   *
+   * @return the column value
+   */
+  public String getColumnValue() {
+    return columnValue;
+  }
+}
