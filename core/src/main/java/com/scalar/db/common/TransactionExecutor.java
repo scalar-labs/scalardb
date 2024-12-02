79d6efa256c0dce164a57072e8a301d7aa17a3a3
brfrn169 <brfrn169@gmail.com>
2024-12-02 16:14:05 +0900

    Add executeWithRetries methods to TransactionExecutor

--- core/src/main/java/com/scalar/db/common/TransactionExecutor.java
+++ core/src/main/java/com/scalar/db/common/TransactionExecutor.java
@@ -1,19 +1,34 @@
 package com.scalar.db.common;
 
+import com.google.common.util.concurrent.Uninterruptibles;
 import com.scalar.db.api.CrudOperable;
 import com.scalar.db.api.DistributedTransaction;
 import com.scalar.db.api.DistributedTransactionManager;
+import com.scalar.db.exception.transaction.CommitConflictException;
+import com.scalar.db.exception.transaction.CrudConflictException;
 import com.scalar.db.exception.transaction.RollbackException;
 import com.scalar.db.exception.transaction.TransactionException;
+import com.scalar.db.exception.transaction.UnknownTransactionStatusException;
 import com.scalar.db.transaction.singlecrudoperation.SingleCrudOperationTransactionUtils;
 import com.scalar.db.util.ThrowableConsumer;
 import com.scalar.db.util.ThrowableFunction;
+import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class TransactionExecutor {
 
   private static final Logger logger = LoggerFactory.getLogger(TransactionExecutor.class);
+
+  private static final int DEFAULT_RETRY_INITIAL_INTERVAL_MILLIS = 100;
+  private static final int DEFAULT_RETRY_MAX_INTERVAL_MILLIS = 1000;
+  private static final int DEFAULT_RETRY_MULTIPLIER = 2;
+  private static final int DEFAULT_RETRY_MAX_RETRIES = 5;
+
+  private static int retryInitialIntervalMillis = DEFAULT_RETRY_INITIAL_INTERVAL_MILLIS;
+  private static int retryMaxIntervalMillis = DEFAULT_RETRY_MAX_INTERVAL_MILLIS;
+  private static int retryMultiplier = DEFAULT_RETRY_MULTIPLIER;
+  private static int retryMaxRetries = DEFAULT_RETRY_MAX_RETRIES;
 
   private TransactionExecutor() {}
 
@@ -32,6 +47,9 @@ public final class TransactionExecutor {
       T result = throwableFunction.apply(transaction);
       transaction.commit();
       return result;
+    } catch (UnknownTransactionStatusException e) {
+      // We don't need to rollback the transaction for UnknownTransactionStatusException
+      throw e;
     } catch (Exception e) {
       if (transaction != null) {
         rollback(transaction);
@@ -43,26 +61,14 @@ public final class TransactionExecutor {
 
   public static void execute(
       DistributedTransactionManager transactionManager,
-      ThrowableConsumer<CrudOperable<?>, TransactionException> throwableFunction)
+      ThrowableConsumer<CrudOperable<?>, TransactionException> throwableConsumer)
       throws TransactionException {
-    if (SingleCrudOperationTransactionUtils.isSingleCrudOperationTransactionManager(
-        transactionManager)) {
-      throwableFunction.accept(transactionManager);
-      return;
-    }
-
-    DistributedTransaction transaction = null;
-    try {
-      transaction = transactionManager.begin();
-      throwableFunction.accept(transaction);
-      transaction.commit();
-    } catch (Exception e) {
-      if (transaction != null) {
-        rollback(transaction);
-      }
-
-      throw e;
-    }
+    execute(
+        transactionManager,
+        t -> {
+          throwableConsumer.accept(t);
+          return null;
+        });
   }
 
   private static void rollback(DistributedTransaction transaction) {
@@ -71,5 +77,108 @@ public final class TransactionExecutor {
     } catch (RollbackException e) {
       logger.warn("Failed to rollback a transaction", e);
     }
+  }
+
+  public static <T> T executeWithRetries(
+      DistributedTransactionManager transactionManager,
+      ThrowableFunction<CrudOperable<?>, T, TransactionException> throwableFunction)
+      throws TransactionException {
+    return executeWithRetries(
+        transactionManager,
+        throwableFunction,
+        retryInitialIntervalMillis,
+        retryMaxIntervalMillis,
+        retryMultiplier,
+        retryMaxRetries);
+  }
+
+  public static <T> T executeWithRetries(
+      DistributedTransactionManager transactionManager,
+      ThrowableFunction<CrudOperable<?>, T, TransactionException> throwableFunction,
+      int retryInitialIntervalMillis,
+      int retryMaxIntervalMillis,
+      int retryMultiplier,
+      int retryMaxRetries)
+      throws TransactionException {
+    TransactionException lastException;
+    int interval = retryInitialIntervalMillis;
+    int attempt = 0;
+    while (true) {
+      try {
+        return execute(transactionManager, throwableFunction);
+      } catch (CrudConflictException | CommitConflictException e) {
+        // Retry the transaction for the conflict exceptions
+        lastException = e;
+      }
+
+      if (attempt++ >= retryMaxRetries) {
+        break;
+      }
+
+      logger.warn(
+          "The transaction failed. Retrying after {} milliseconds... The current attempt count: {}.",
+          interval,
+          attempt,
+          lastException);
+
+      Uninterruptibles.sleepUninterruptibly(interval, TimeUnit.MILLISECONDS);
+
+      interval *= retryMultiplier;
+      if (interval > retryMaxIntervalMillis) {
+        interval = retryMaxIntervalMillis;
+      }
+    }
+
+    logger.error("The transaction failed after {} retries.", retryMaxRetries, lastException);
+    throw lastException;
+  }
+
+  public static void executeWithRetries(
+      DistributedTransactionManager transactionManager,
+      ThrowableConsumer<CrudOperable<?>, TransactionException> throwableConsumer)
+      throws TransactionException {
+    executeWithRetries(
+        transactionManager,
+        throwableConsumer,
+        retryInitialIntervalMillis,
+        retryMaxIntervalMillis,
+        retryMultiplier,
+        retryMaxRetries);
+  }
+
+  public static void executeWithRetries(
+      DistributedTransactionManager transactionManager,
+      ThrowableConsumer<CrudOperable<?>, TransactionException> throwableConsumer,
+      int retryInitialIntervalMillis,
+      int retryMaxIntervalMillis,
+      int retryMultiplier,
+      int retryMaxRetries)
+      throws TransactionException {
+    executeWithRetries(
+        transactionManager,
+        t -> {
+          throwableConsumer.accept(t);
+          return null;
+        },
+        retryInitialIntervalMillis,
+        retryMaxIntervalMillis,
+        retryMultiplier,
+        retryMaxRetries);
+  }
+
+  public static void setRetryInitialIntervalMillis(int retryInitialIntervalMillis) {
+    TransactionExecutor.retryInitialIntervalMillis = retryInitialIntervalMillis;
+  }
+
+  public static void setRetryMaxIntervalMillis(int retryMaxIntervalMillis) {
+    TransactionExecutor.retryMaxIntervalMillis = retryMaxIntervalMillis;
+  }
+
+  public static void setRetryMultiplier(int retryMultiplier) {
+    TransactionExecutor.retryMultiplier = retryMultiplier;
+  }
+
+  public static void setRetryMaxRetries(int retryMaxRetries) {
+    TransactionExecutor.retryMaxRetries = retryMaxRetries;
   }
 }
