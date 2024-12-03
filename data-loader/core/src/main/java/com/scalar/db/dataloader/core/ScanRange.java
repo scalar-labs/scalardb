64b03343f32b8250fa8d143917932d3e7a284d60
Jishnu J <jishnu.j@innovaturelabs.com>
2024-12-03 16:32:23 +0530

    spotless applied

--- data-loader/core/src/main/java/com/scalar/db/dataloader/core/ScanRange.java
+++ data-loader/core/src/main/java/com/scalar/db/dataloader/core/ScanRange.java
@@ -3,9 +3,7 @@ package com.scalar.db.dataloader.core;
 import com.scalar.db.io.Key;
 import lombok.Value;
 
-/***
- * The scan range which is used in data export scan filtering
- */
+/** * The scan range which is used in data export scan filtering */
 @Value
 public class ScanRange {
   /** The key for scan start filter */
