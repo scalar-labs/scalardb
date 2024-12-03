df7888120bd2081c4f4ced8ebf8eff2f32bb66e6
Jishnu J <jishnu.j@innovaturelabs.com>
2024-12-03 16:05:15 +0530

    Enum, constants for data loader

---
+++ data-loader/core/src/main/java/com/scalar/db/dataloader/core/ScalarDBMode.java
@@ -0,0 +1,7 @@
+package com.scalar.db.dataloader.core;
+
+/** The available modes a ScalarDB instance can run in */
+public enum ScalarDBMode {
+  STORAGE,
+  TRANSACTION
+}
---
+++ data-loader/core/src/main/java/com/scalar/db/dataloader/core/ScanRange.java
@@ -0,0 +1,19 @@
+package com.scalar.db.dataloader.core;
+
+import com.scalar.db.io.Key;
+import lombok.Value;
+
+/***
+ * The scan range which is used in data export scan filtering
+ */
+@Value
+public class ScanRange {
+  /** The key for scan start filter */
+  Key scanStartKey;
+  /** The key for scan end filter */
+  Key scanEndKey;
+  /** To include the scan start key value in the export data scan */
+  boolean isStartInclusive;
+  /** To include the scan end key value in the export data scan */
+  boolean isEndInclusive;
+}
