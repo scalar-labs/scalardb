2c07610974fc73cde2a38e8dea2046c417cd8d9e
Peckstadt Yves <peckstadt.yves@gmail.com>
2024-12-03 17:43:32 +0900

    Add data loader core key and column utils (#1771)

---
+++ data-loader/core/src/main/java/com/scalar/db/dataloader/core/ColumnInfo.java
@@ -0,0 +1,24 @@
+package com.scalar.db.dataloader.core;
+
+import lombok.Builder;
+import lombok.Value;
+
+/**
+ * Represents a column in a database table.
+ *
+ * <p>This class holds the metadata for a column, including the namespace (schema), table name, and
+ * the column name within the table.
+ */
+@Value
+@Builder
+public class ColumnInfo {
+
+  /** The namespace (schema) where the table is located. */
+  String namespace;
+
+  /** The name of the table where the column resides. */
+  String tableName;
+
+  /** The name of the column in the table. */
+  String columnName;
+}
