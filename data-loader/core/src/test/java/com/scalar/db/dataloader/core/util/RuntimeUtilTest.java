913eb1c069e452ff5da752fb35c8a6b50e49d758
Jishnu J <jishnu.j@innovaturelabs.com>
2024-12-06 16:19:21 +0530

    Fixed unit test failures

--- data-loader/core/src/test/java/com/scalar/db/dataloader/core/util/RuntimeUtilTest.java
+++ data-loader/core/src/test/java/com/scalar/db/dataloader/core/util/RuntimeUtilTest.java
@@ -1,6 +1,6 @@
 package com.scalar.db.dataloader.core.util;
 
-import static com.scalar.db.dataloader.core.ErrorMessage.ERROR_METHOD_NULL_ARGUMENT;
+import static com.scalar.db.common.error.CoreError.DATA_LOADER_ERROR_METHOD_NULL_ARGUMENT;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
@@ -12,7 +12,7 @@ public class RuntimeUtilTest {
   public void checkNotNull_HasNullValues_ShouldThrowException() {
     assertThatThrownBy(() -> RuntimeUtil.checkNotNull(null, null))
         .isExactlyInstanceOf(NullPointerException.class)
-        .hasMessage(ERROR_METHOD_NULL_ARGUMENT);
+        .hasMessage(DATA_LOADER_ERROR_METHOD_NULL_ARGUMENT.getMessage());
   }
 
   @Test
