be26ee2abf6346b32dc78e2550499bbf48ca2cbe
Jishnu J <jishnu.j@innovaturelabs.com>
2024-12-11 16:27:05 +0530

    Applied sportless on data loader core

--- data-loader/core/src/test/java/com/scalar/db/dataloader/core/dataexport/ExportReportTest.java
+++ data-loader/core/src/test/java/com/scalar/db/dataloader/core/dataexport/ExportReportTest.java
@@ -5,17 +5,17 @@ import org.junit.jupiter.api.Test;
 
 public class ExportReportTest {
 
-    @Test
-    void getExportedRowCount_afterInitialisation_ShouldBeZero(){
-        ExportReport exportReport = new ExportReport();
-        Assertions.assertEquals(0, exportReport.getExportedRowCount().get());
-    }
+  @Test
+  void getExportedRowCount_afterInitialisation_ShouldBeZero() {
+    ExportReport exportReport = new ExportReport();
+    Assertions.assertEquals(0, exportReport.getExportedRowCount().get());
+  }
 
-    @Test
-    void getExportedRowCount_afterIncrementingTwice_ShouldBeTwo(){
-        ExportReport exportReport = new ExportReport();
-        exportReport.increaseExportedRowCount();
-        exportReport.increaseExportedRowCount();
-        Assertions.assertEquals(2, exportReport.getExportedRowCount().get());
-    }
+  @Test
+  void getExportedRowCount_afterIncrementingTwice_ShouldBeTwo() {
+    ExportReport exportReport = new ExportReport();
+    exportReport.increaseExportedRowCount();
+    exportReport.increaseExportedRowCount();
+    Assertions.assertEquals(2, exportReport.getExportedRowCount().get());
+  }
 }
