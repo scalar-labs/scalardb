98ddd075174a8f4ddefc1315a61b52f472659770
Vincent <vincent.guilpain@gmail.com>
2024-12-12 11:46:41 +0900

    Format comment [skip ci]

--- core/src/main/java/com/scalar/db/storage/jdbc/RdbEngineMariaDB.java
+++ core/src/main/java/com/scalar/db/storage/jdbc/RdbEngineMariaDB.java
@@ -14,9 +14,8 @@ class RdbEngineMariaDB extends RdbEngineMysql {
   public DataType getDataTypeForScalarDb(
       JDBCType type, String typeName, int columnSize, int digits, String columnDescription) {
     if (type == JDBCType.BOOLEAN) {
-      // MariaDB JDBC driver maps the TINYINT(1) as a BOOLEAN JDBC type which differs from the MySQL
-      // driver which maps it to a
-      // BIT type.
+      // MariaDB JDBC driver maps TINYINT(1) type as a BOOLEAN JDBC type which differs from the
+      // MySQL driver which maps it to a BIT type.
       return DataType.BOOLEAN;
     } else {
       return super.getDataTypeForScalarDb(type, typeName, columnSize, digits, columnDescription);
