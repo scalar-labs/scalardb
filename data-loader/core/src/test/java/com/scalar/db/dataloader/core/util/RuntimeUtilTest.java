52890c8d8992f3a2f7c437b2e6df6456ffe646b8
Jishnu J <jishnu.j@innovaturelabs.com>
2024-12-16 11:07:50 +0530

    Changes

--- data-loader/core/src/test/java/com/scalar/db/dataloader/core/util/RuntimeUtilTest.java
+++ data-loader/core/src/test/java/com/scalar/db/dataloader/core/util/RuntimeUtilTest.java
@@ -6,17 +6,17 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import org.junit.jupiter.api.Test;
 
 /** RuntimeUtils unit tests */
-public class RuntimeUtilTest {
+class RuntimeUtilTest {
 
   @Test
-  public void checkNotNull_HasNullValues_ShouldThrowException() {
+  void checkNotNull_HasNullValues_ShouldThrowException() {
     assertThatThrownBy(() -> RuntimeUtil.checkNotNull(null, null))
         .isExactlyInstanceOf(NullPointerException.class)
         .hasMessage(DATA_LOADER_ERROR_METHOD_NULL_ARGUMENT.buildMessage());
   }
 
   @Test
-  public void checkNotNull_HasNoNullValues_ShouldNotThrowException() {
+  void checkNotNull_HasNoNullValues_ShouldNotThrowException() {
     String string = "1";
     Object object = new Object();
     RuntimeUtil.checkNotNull(string, object);
