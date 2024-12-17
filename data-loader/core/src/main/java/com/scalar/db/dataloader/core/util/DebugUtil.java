0a880ff4bf8cad22ec2f0214cba9701d049c378d
Hiroyuki Yamada <mogwaing@gmail.com>
2024-12-17 16:52:53 +0900

    Backport to branch(3.13) : Add Util classes for data loader (#2413)
    Co-authored-by: inv-jishnu <31100916+inv-jishnu@users.noreply.github.com>
    Co-authored-by: Peckstadt Yves <peckstadt.yves@gmail.com>

---
+++ data-loader/core/src/main/java/com/scalar/db/dataloader/core/util/DebugUtil.java
@@ -0,0 +1,30 @@
+package com.scalar.db.dataloader.core.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DebugUtil {
+
+  private static final Logger logger = LoggerFactory.getLogger(DebugUtil.class);
+
+  /**
+   * log memory usage
+   *
+   * @param stage stage of process
+   */
+  public static void logMemoryUsage(String stage) {
+    Runtime runtime = Runtime.getRuntime();
+    long usedMemory = runtime.totalMemory() - runtime.freeMemory();
+    long maxMemory = runtime.maxMemory();
+
+    logger.info(
+        "Memory usage at {}: Used Memory = {} MB, Max Memory = {} MB",
+        stage,
+        formatMemorySize(usedMemory),
+        formatMemorySize(maxMemory));
+  }
+
+  private static String formatMemorySize(long size) {
+    return String.format("%.2f", size / (1024.0 * 1024.0));
+  }
+}
