3d5d3e09f5031c5559c48c7d6f5800ff1099dd15
Jishnu J <jishnu.j@innovaturelabs.com>
2024-12-18 16:40:14 +0530

    Added unit test files [skip ci]

---
+++ data-loader/core/src/test/java/com/scalar/db/dataloader/core/tablemetadata/TableMetadataServiceTest.java
@@ -0,0 +1,53 @@
+package com.scalar.db.dataloader.core.tablemetadata;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.scalar.db.api.DistributedStorageAdmin;
+import com.scalar.db.api.TableMetadata;
+import com.scalar.db.dataloader.core.UnitTestUtils;
+import com.scalar.db.exception.storage.ExecutionException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class TableMetadataServiceTest {
+
+  DistributedStorageAdmin storageAdmin;
+  TableMetadataService tableMetadataService;
+
+  @BeforeEach
+  public void setup() throws ExecutionException {
+    storageAdmin = Mockito.mock(DistributedStorageAdmin.class);
+    Mockito.when(storageAdmin.getTableMetadata("namespace", "table"))
+        .thenReturn(UnitTestUtils.createTestTableMetadata());
+    //        Mockito.when(storageAdmin.getTableMetadata("namespace1","table1")).thenReturn(null);
+    tableMetadataService = new TableMetadataService(storageAdmin);
+  }
+
+  @Test
+  void getTableMetadata_withValidNamespaceAndTable_shouldReturnTableMetadataMap()
+      throws TableMetadataException {
+
+    Map<String, TableMetadata> expected = new HashMap<>();
+    expected.put("namespace.table", UnitTestUtils.createTestTableMetadata());
+    TableMetadataRequest tableMetadataRequest = new TableMetadataRequest("namespace", "table");
+    Map<String, TableMetadata> output =
+        tableMetadataService.getTableMetadata(Collections.singleton(tableMetadataRequest));
+    Assertions.assertEquals(expected.get("namespace.table"), output.get("namespace.table"));
+  }
+
+  @Test
+  void getTableMetadata_withInvalidNamespaceAndTable_shouldThrowException()
+      throws TableMetadataException {
+    TableMetadataRequest tableMetadataRequest = new TableMetadataRequest("namespace2", "table2");
+    assertThatThrownBy(
+            () ->
+                tableMetadataService.getTableMetadata(Collections.singleton(tableMetadataRequest)))
+        .isInstanceOf(TableMetadataException.class)
+        .hasMessage("Missing namespace or table: namespace2, table2");
+  }
+}
