90c41051e93e5df9928f13cef86611d61076f441
Jishnu J <jishnu.j@innovaturelabs.com>
2024-12-18 16:33:02 +0530

    Added a file [skip ci]

---
+++ data-loader/core/src/main/java/com/scalar/db/dataloader/core/dataimport/controlfile/ControlFileTableFieldMapping.java
@@ -0,0 +1,32 @@
+package com.scalar.db.dataloader.core.dataimport.controlfile;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+/** Represents the one field mapping for a table mapping in the control file */
+@Getter
+@Setter
+public class ControlFileTableFieldMapping {
+
+  @JsonProperty("source_field")
+  public String sourceField;
+
+  @JsonProperty("target_column")
+  public String targetColumn;
+
+  /**
+   * Class constructor
+   *
+   * @param sourceField The data field in the provided json field
+   * @param targetColumn The column in the ScalarDB table
+   */
+  @JsonCreator
+  public ControlFileTableFieldMapping(
+      @JsonProperty("source_field") String sourceField,
+      @JsonProperty("target_column") String targetColumn) {
+    this.sourceField = sourceField;
+    this.targetColumn = targetColumn;
+  }
+}
