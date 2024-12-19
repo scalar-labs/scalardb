d439f26e6e88e4397bd4cb251681f1e7a5b3fc5f
inv-jishnu <31100916+inv-jishnu@users.noreply.github.com>
2024-12-19 12:28:49 +0530

    Export options and basic export report dto (#2397)
    Co-authored-by: Peckstadt Yves <peckstadt.yves@gmail.com>

---
+++ data-loader/core/src/test/java/com/scalar/db/dataloader/core/dataexport/ExportReportTest.java
@@ -0,0 +1,21 @@
+package com.scalar.db.dataloader.core.dataexport;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ExportReportTest {
+
+  @Test
+  void getExportedRowCount_afterInitialisation_ShouldBeZero() {
+    ExportReport exportReport = new ExportReport();
+    Assertions.assertEquals(0, exportReport.getExportedRowCount());
+  }
+
+  @Test
+  void getExportedRowCount_afterIncrementingTwice_ShouldBeTwo() {
+    ExportReport exportReport = new ExportReport();
+    exportReport.increaseExportedRowCount();
+    exportReport.increaseExportedRowCount();
+    Assertions.assertEquals(2, exportReport.getExportedRowCount());
+  }
+}
