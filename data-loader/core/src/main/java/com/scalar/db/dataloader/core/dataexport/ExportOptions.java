05251a262bb37124ea2baa03520b6f30689d61fe
inv-jishnu <31100916+inv-jishnu@users.noreply.github.com>
2024-12-19 12:28:49 +0530

    Export options and basic export report dto (#2397)
    Co-authored-by: Peckstadt Yves <peckstadt.yves@gmail.com>

---
+++ data-loader/core/src/main/java/com/scalar/db/dataloader/core/dataexport/ExportOptions.java
@@ -0,0 +1,42 @@
+package com.scalar.db.dataloader.core.dataexport;
+
+import com.scalar.db.api.Scan;
+import com.scalar.db.dataloader.core.FileFormat;
+import com.scalar.db.dataloader.core.ScanRange;
+import com.scalar.db.io.Key;
+import java.util.Collections;
+import java.util.List;
+import lombok.Builder;
+import lombok.Data;
+
+/** Options for a ScalarDB export data operation */
+@SuppressWarnings("SameNameButDifferent")
+@Builder(builderMethodName = "hiddenBuilder")
+@Data
+public class ExportOptions {
+
+  private final String namespace;
+  private final String tableName;
+  private final Key scanPartitionKey;
+  private final FileFormat outputFileFormat;
+  private final ScanRange scanRange;
+  private final int limit;
+  private final int maxThreadCount;
+  private final boolean prettyPrintJson;
+
+  @Builder.Default private final int dataChunkSize = 200;
+  @Builder.Default private final String delimiter = ";";
+  @Builder.Default private final boolean excludeHeaderRow = false;
+  @Builder.Default private final boolean includeTransactionMetadata = false;
+  @Builder.Default private List<String> projectionColumns = Collections.emptyList();
+  private List<Scan.Ordering> sortOrders;
+
+  public static ExportOptionsBuilder builder(
+      String namespace, String tableName, Key scanPartitionKey, FileFormat outputFileFormat) {
+    return hiddenBuilder()
+        .namespace(namespace)
+        .tableName(tableName)
+        .scanPartitionKey(scanPartitionKey)
+        .outputFileFormat(outputFileFormat);
+  }
+}
