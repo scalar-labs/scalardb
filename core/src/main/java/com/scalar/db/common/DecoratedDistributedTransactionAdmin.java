46726d93de3c8f3c4381fa28dd254e0d7efd1d44
Toshihiro Suzuki <brfrn169@gmail.com>
2025-01-22 16:30:36 +0900

    Add importTable without overrideColumnsType to DecoratedDistributedTransactionAdmin (#2476)

--- core/src/main/java/com/scalar/db/common/DecoratedDistributedTransactionAdmin.java
+++ core/src/main/java/com/scalar/db/common/DecoratedDistributedTransactionAdmin.java
@@ -196,6 +196,12 @@ public abstract class DecoratedDistributedTransactionAdmin implements Distribute
   }
 
   @Override
+  public void importTable(String namespace, String table, Map<String, String> options)
+      throws ExecutionException {
+    distributedTransactionAdmin.importTable(namespace, table, options);
+  }
+
+  @Override
   public void importTable(
       String namespace,
       String table,
