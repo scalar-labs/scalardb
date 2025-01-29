2a96abc054e1de8ed9b35999d0067829a8900007
Jishnu J <jishnu.j@innovaturelabs.com>
2025-01-29 16:12:04 +0530

    Changes

--- data-loader/cli/src/main/java/com/scalar/db/dataloader/cli/command/dataexport/ScanOrderingConverter.java
+++ data-loader/cli/src/main/java/com/scalar/db/dataloader/cli/command/dataexport/ScanOrderingConverter.java
@@ -5,13 +5,15 @@ import picocli.CommandLine;
 
 public class ScanOrderingConverter implements CommandLine.ITypeConverter<Scan.Ordering> {
   @Override
-  public Scan.Ordering convert(String value) throws Exception {
+  public Scan.Ordering convert(String value) {
     String[] parts = value.split("=");
     if (parts.length != 2) {
       throw new IllegalArgumentException("Invalid column order format: " + value);
     }
     String columnName = parts[0].trim();
     Scan.Ordering.Order sortOrder = Scan.Ordering.Order.valueOf(parts[1].trim().toUpperCase());
-    return new Scan.Ordering(columnName, sortOrder);
+    return sortOrder.equals(Scan.Ordering.Order.ASC)
+        ? Scan.Ordering.asc(columnName)
+        : Scan.Ordering.desc(columnName);
   }
 }
