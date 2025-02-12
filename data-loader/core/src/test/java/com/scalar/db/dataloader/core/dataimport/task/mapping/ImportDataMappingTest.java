c4a4eb481a56e2acf305159f71aa89864d70487f
Jishnu J <jishnu.j@innovaturelabs.com>
2025-02-12 09:27:49 +0530

    Updated test class to be package private

--- data-loader/core/src/test/java/com/scalar/db/dataloader/core/dataimport/task/mapping/ImportDataMappingTest.java
+++ data-loader/core/src/test/java/com/scalar/db/dataloader/core/dataimport/task/mapping/ImportDataMappingTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class ImportDataMappingTest {
+class ImportDataMappingTest {
 
   ControlFileTable controlFilTable;
 
