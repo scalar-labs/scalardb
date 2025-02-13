9f2c25ae5a74beac54a7750c4042aa67966ce591
inv-jishnu <31100916+inv-jishnu@users.noreply.github.com>
2025-02-13 13:29:57 +0530

    Add dtos and other classes for task (#2446)
    Co-authored-by: Peckstadt Yves <peckstadt.yves@gmail.com>

---
+++ data-loader/core/src/main/java/com/scalar/db/dataloader/core/dataimport/ImportOptions.java
@@ -0,0 +1,38 @@
+package com.scalar.db.dataloader.core.dataimport;
+
+import com.scalar.db.dataloader.core.FileFormat;
+import com.scalar.db.dataloader.core.dataimport.controlfile.ControlFile;
+import com.scalar.db.dataloader.core.dataimport.controlfile.ControlFileValidationLevel;
+import com.scalar.db.dataloader.core.dataimport.log.LogMode;
+import lombok.Builder;
+import lombok.Data;
+
+/** Import options to import data into one or more ScalarDB tables */
+@Builder
+@Data
+public class ImportOptions {
+
+  @Builder.Default private final ImportMode importMode = ImportMode.UPSERT;
+  @Builder.Default private final boolean requireAllColumns = false;
+  @Builder.Default private final FileFormat fileFormat = FileFormat.JSON;
+  @Builder.Default private final boolean prettyPrint = false;
+  @Builder.Default private final boolean ignoreNullValues = false;
+  @Builder.Default private final LogMode logMode = LogMode.SPLIT_BY_DATA_CHUNK;
+
+  @Builder.Default
+  private final ControlFileValidationLevel controlFileValidationLevel =
+      ControlFileValidationLevel.MAPPED;
+
+  @Builder.Default private final char delimiter = ',';
+
+  @Builder.Default private final boolean logSuccessRecords = false;
+  @Builder.Default private final boolean logRawRecord = false;
+
+  private final int dataChunkSize;
+  private final int transactionBatchSize;
+  private final ControlFile controlFile;
+  private final String namespace;
+  private final String tableName;
+  private final int maxThreads;
+  private final String customHeaderRow;
+}
---
+++ data-loader/core/src/main/java/com/scalar/db/dataloader/core/dataimport/log/LogMode.java
@@ -0,0 +1,7 @@
+package com.scalar.db.dataloader.core.dataimport.log;
+
+/** Log modes available for import logging */
+public enum LogMode {
+  SINGLE_FILE,
+  SPLIT_BY_DATA_CHUNK
+}
