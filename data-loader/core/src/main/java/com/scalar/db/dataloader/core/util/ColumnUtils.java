62e60ea03e51c52fe213844e9176949cd6b0f49a
inv-jishnu <31100916+inv-jishnu@users.noreply.github.com>
2025-02-28 14:27:00 +0530

    Add support for new date, time data types (#2550)

--- data-loader/core/src/main/java/com/scalar/db/dataloader/core/util/ColumnUtils.java
+++ data-loader/core/src/main/java/com/scalar/db/dataloader/core/util/ColumnUtils.java
@@ -10,10 +10,18 @@ import com.scalar.db.io.BlobColumn;
 import com.scalar.db.io.BooleanColumn;
 import com.scalar.db.io.Column;
 import com.scalar.db.io.DataType;
+import com.scalar.db.io.DateColumn;
 import com.scalar.db.io.DoubleColumn;
 import com.scalar.db.io.FloatColumn;
 import com.scalar.db.io.IntColumn;
 import com.scalar.db.io.TextColumn;
+import com.scalar.db.io.TimeColumn;
+import com.scalar.db.io.TimestampColumn;
+import com.scalar.db.io.TimestampTZColumn;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Base64;
 import javax.annotation.Nullable;
 
@@ -75,6 +83,22 @@ public final class ColumnUtils {
           return value != null
               ? BlobColumn.of(columnName, Base64.getDecoder().decode(value))
               : BlobColumn.ofNull(columnName);
+        case DATE:
+          return value != null
+              ? DateColumn.of(columnName, LocalDate.parse(value))
+              : DateColumn.ofNull(columnName);
+        case TIME:
+          return value != null
+              ? TimeColumn.of(columnName, LocalTime.parse(value))
+              : TimeColumn.ofNull(columnName);
+        case TIMESTAMP:
+          return value != null
+              ? TimestampColumn.of(columnName, LocalDateTime.parse(value))
+              : TimestampColumn.ofNull(columnName);
+        case TIMESTAMPTZ:
+          return value != null
+              ? TimestampTZColumn.of(columnName, Instant.parse(value))
+              : TimestampTZColumn.ofNull(columnName);
         default:
           throw new AssertionError();
       }
