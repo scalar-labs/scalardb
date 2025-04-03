a232df13320a1841deef210092ce9c6a535c9160
Jishnu J <jishnu.j@innovaturelabs.com>
2025-04-03 18:06:30 +0530

    Renamed parameters from ScalarDB to ScalarDb

--- data-loader/core/src/test/java/com/scalar/db/dataloader/core/dataimport/processor/CsvImportProcessorTest.java
+++ data-loader/core/src/test/java/com/scalar/db/dataloader/core/dataimport/processor/CsvImportProcessorTest.java
@@ -8,13 +8,13 @@ import com.scalar.db.api.DistributedTransaction;
 import com.scalar.db.api.DistributedTransactionManager;
 import com.scalar.db.api.TableMetadata;
 import com.scalar.db.dataloader.core.FileFormat;
-import com.scalar.db.dataloader.core.ScalarDBMode;
+import com.scalar.db.dataloader.core.ScalarDbMode;
 import com.scalar.db.dataloader.core.UnitTestUtils;
 import com.scalar.db.dataloader.core.dataimport.ImportMode;
 import com.scalar.db.dataloader.core.dataimport.ImportOptions;
 import com.scalar.db.dataloader.core.dataimport.controlfile.ControlFileValidationLevel;
-import com.scalar.db.dataloader.core.dataimport.dao.ScalarDBDao;
-import com.scalar.db.dataloader.core.dataimport.dao.ScalarDBDaoException;
+import com.scalar.db.dataloader.core.dataimport.dao.ScalarDbDao;
+import com.scalar.db.dataloader.core.dataimport.dao.ScalarDbDaoException;
 import com.scalar.db.dataloader.core.dataimport.datachunk.ImportDataChunkStatus;
 import com.scalar.db.dataloader.core.dataimport.log.LogMode;
 import com.scalar.db.exception.transaction.TransactionException;
@@ -28,19 +28,19 @@ import org.mockito.Mockito;
 
 class CsvImportProcessorTest {
   @Mock private ImportProcessorParams params;
-  @Mock ScalarDBMode scalarDBMode;
+  @Mock ScalarDbMode scalarDbMode;
   @Mock ImportOptions importOptions;
   @Mock Map<String, TableMetadata> tableMetadataByTableName;
   @Mock TableColumnDataTypes tableColumnDataTypes;
 
-  ScalarDBDao dao;
+  ScalarDbDao dao;
   @Mock DistributedStorage distributedStorage;
   DistributedTransactionManager distributedTransactionManager;
   CsvImportProcessor csvImportProcessor;
 
   @BeforeEach
-  void setup() throws ScalarDBDaoException, TransactionException {
-    dao = Mockito.mock(ScalarDBDao.class);
+  void setup() throws ScalarDbDaoException, TransactionException {
+    dao = Mockito.mock(ScalarDbDao.class);
     distributedTransactionManager = mock(DistributedTransactionManager.class);
     DistributedTransaction distributedTransaction = mock(DistributedTransaction.class);
     when(distributedTransactionManager.start()).thenReturn(distributedTransaction);
@@ -81,12 +81,12 @@ class CsvImportProcessorTest {
   void test_importProcessWithStorage() {
     params =
         ImportProcessorParams.builder()
-            .scalarDBMode(ScalarDBMode.STORAGE)
+            .scalarDbMode(ScalarDbMode.STORAGE)
             .importOptions(importOptions)
             .dao(dao)
             .distributedStorage(distributedStorage)
             .distributedTransactionManager(distributedTransactionManager)
-            .scalarDBMode(scalarDBMode)
+            .scalarDbMode(scalarDbMode)
             .tableColumnDataTypes(tableColumnDataTypes)
             .tableMetadataByTableName(tableMetadataByTableName)
             .build();
@@ -101,12 +101,12 @@ class CsvImportProcessorTest {
   void test_importProcessWithTransaction() {
     params =
         ImportProcessorParams.builder()
-            .scalarDBMode(ScalarDBMode.TRANSACTION)
+            .scalarDbMode(ScalarDbMode.TRANSACTION)
             .importOptions(importOptions)
             .dao(dao)
             .distributedStorage(distributedStorage)
             .distributedTransactionManager(distributedTransactionManager)
-            .scalarDBMode(scalarDBMode)
+            .scalarDbMode(scalarDbMode)
             .tableColumnDataTypes(tableColumnDataTypes)
             .tableMetadataByTableName(tableMetadataByTableName)
             .build();
