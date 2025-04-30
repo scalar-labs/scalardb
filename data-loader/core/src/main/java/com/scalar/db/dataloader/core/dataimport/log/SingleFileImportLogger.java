502034ece2f62847370d8c4a63f385ff7211446e
Jishnu J <jishnu.j@innovaturelabs.com>
2025-04-30 09:39:32 +0530

    Chanaged LOGGER to logger

--- data-loader/core/src/main/java/com/scalar/db/dataloader/core/dataimport/log/SingleFileImportLogger.java
+++ data-loader/core/src/main/java/com/scalar/db/dataloader/core/dataimport/log/SingleFileImportLogger.java
@@ -33,7 +33,7 @@ public class SingleFileImportLogger extends AbstractImportLogger {
   protected static final String SUMMARY_LOG_FILE_NAME = "summary.log";
   protected static final String SUCCESS_LOG_FILE_NAME = "success.json";
   protected static final String FAILURE_LOG_FILE_NAME = "failure.json";
-  private static final Logger LOGGER = LoggerFactory.getLogger(SingleFileImportLogger.class);
+  private static final Logger logger = LoggerFactory.getLogger(SingleFileImportLogger.class);
   private volatile LogWriter summaryLogWriter;
   private final LogWriter successLogWriter;
   private final LogWriter failureLogWriter;
@@ -124,7 +124,7 @@ public class SingleFileImportLogger extends AbstractImportLogger {
    */
   @Override
   protected void logError(String errorMessage, Exception exception) {
-    LOGGER.error(errorMessage, exception);
+    logger.error(errorMessage, exception);
     throw new RuntimeException(errorMessage, exception);
   }
 
--- data-loader/core/src/main/java/com/scalar/db/dataloader/core/dataimport/log/SplitByDataChunkImportLogger.java
+++ data-loader/core/src/main/java/com/scalar/db/dataloader/core/dataimport/log/SplitByDataChunkImportLogger.java
@@ -40,7 +40,7 @@ public class SplitByDataChunkImportLogger extends AbstractImportLogger {
   protected static final String FAILURE_LOG_FILE_NAME_FORMAT = "data_chunk_%s_failure.json";
   protected static final String SUCCESS_LOG_FILE_NAME_FORMAT = "data_chunk_%s_success.json";
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(SplitByDataChunkImportLogger.class);
+  private static final Logger logger = LoggerFactory.getLogger(SplitByDataChunkImportLogger.class);
   private final Map<Integer, LogWriter> summaryLogWriters = new ConcurrentHashMap<>();
   private final Map<Integer, LogWriter> successLogWriters = new ConcurrentHashMap<>();
   private final Map<Integer, LogWriter> failureLogWriters = new ConcurrentHashMap<>();
@@ -169,7 +169,7 @@ public class SplitByDataChunkImportLogger extends AbstractImportLogger {
    */
   @Override
   protected void logError(String errorMessage, Exception exception) {
-    LOGGER.error(errorMessage, exception);
+    logger.error(errorMessage, exception);
     throw new RuntimeException(errorMessage, exception);
   }
 
