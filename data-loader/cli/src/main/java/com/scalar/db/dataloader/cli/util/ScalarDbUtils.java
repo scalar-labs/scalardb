df32a6f0d85bba9ac5d3e19ddc5634b7202f3cfc
Jishnu J <jishnu.j@innovaturelabs.com>
2025-05-07 14:42:20 +0530

    Removed unused file

--- data-loader/cli/src/main/java/com/scalar/db/dataloader/cli/util/ScalarDbUtils.java
+++
@@ -1,6 +0,0 @@
-package com.scalar.db.dataloader.cli.util;
-
-/** The utility class for ScalarDB */
-public class ScalarDbUtils {
-  public static final String SCALAR_DB_TRANSACTION_MANAGER_CLUSTER = "cluster";
-}
