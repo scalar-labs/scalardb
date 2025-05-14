8c77691a278b70f0b177d736ad770ddc0328c54c
Hiroyuki Yamada <mogwaing@gmail.com>
2025-05-14 23:40:33 +0900

    Backport to branch(3) : Add util classes for data loader CLI (#2656)
    Co-authored-by: inv-jishnu <31100916+inv-jishnu@users.noreply.github.com>

--- data-loader/cli/src/main/java/com/scalar/db/dataloader/cli/util/DirectoryUtils.java
+++ data-loader/cli/src/main/java/com/scalar/db/dataloader/cli/util/DirectoryUtils.java
@@ -16,13 +16,29 @@ public final class DirectoryUtils {
   }
 
   /**
+   * Validates the current working directory. Ensures that it is writable.
+   *
+   * @throws DirectoryValidationException if the current working directory is not writable
+   */
+  public static void validateWorkingDirectory() throws DirectoryValidationException {
+    Path workingDirectoryPath = Paths.get(System.getProperty("user.dir"));
+
+    // Check if the current working directory is writable
+    if (!Files.isWritable(workingDirectoryPath)) {
+      throw new DirectoryValidationException(
+          CoreError.DATA_LOADER_DIRECTORY_WRITE_ACCESS.buildMessage(
+              workingDirectoryPath.toAbsolutePath()));
+    }
+  }
+
+  /**
    * Validates the provided directory path. Ensures that the directory exists and is writable. If
    * the directory doesn't exist, a creation attempt is made.
    *
    * @param directoryPath the directory path to validate
    * @throws DirectoryValidationException if the directory is not writable or cannot be created
    */
-  public static void validateTargetDirectory(String directoryPath)
+  public static void validateOrCreateTargetDirectory(String directoryPath)
       throws DirectoryValidationException {
     if (StringUtils.isBlank(directoryPath)) {
       throw new IllegalArgumentException(
@@ -32,7 +48,10 @@ public final class DirectoryUtils {
     Path path = Paths.get(directoryPath);
 
     if (Files.exists(path)) {
-      // Check if the provided directory is writable
+      if (!Files.isDirectory(path)) {
+        throw new DirectoryValidationException(
+            CoreError.DATA_LOADER_PATH_IS_NOT_A_DIRECTORY.buildMessage(path));
+      }
       if (!Files.isWritable(path)) {
         throw new DirectoryValidationException(
             CoreError.DATA_LOADER_DIRECTORY_WRITE_ACCESS_NOT_ALLOWED.buildMessage(
