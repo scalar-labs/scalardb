e451f22c9bad4530791a819bbc8ce2ac888c646a
Hiroyuki Yamada <mogwaing@gmail.com>
2025-05-22 21:06:00 +0900

    Backport to branch(3.13) : Add export command for data loader CLI (#2686)
    Co-authored-by: inv-jishnu <31100916+inv-jishnu@users.noreply.github.com>

---
+++ data-loader/cli/src/test/java/com/scalar/db/dataloader/cli/command/dataexport/ScanOrderingConverterTest.java
@@ -0,0 +1,44 @@
+package com.scalar.db.dataloader.cli.command.dataexport;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.scalar.db.api.Scan;
+import com.scalar.db.common.error.CoreError;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ScanOrderingConverterTest {
+  ScanOrderingConverter scanOrderingConverter = new ScanOrderingConverter();
+
+  @Test
+  void callConvert_withInvalidValue_shouldThrowException() {
+    String value = "id ASC";
+    IllegalArgumentException thrown =
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> scanOrderingConverter.convert(value),
+            "Expected to throw exception");
+    Assertions.assertEquals(
+        CoreError.DATA_LOADER_INVALID_KEY_VALUE_INPUT.buildMessage(value), thrown.getMessage());
+  }
+
+  @Test
+  void callConvert_withValidValueAndOrderAscending_shouldReturnScanOrdering() {
+    String value = "id=ASC,age=DESC";
+    List<Scan.Ordering> expectedOrder = new ArrayList<>();
+    expectedOrder.add(new Scan.Ordering("id", Scan.Ordering.Order.ASC));
+    expectedOrder.add(new Scan.Ordering("age", Scan.Ordering.Order.DESC));
+    Assertions.assertEquals(expectedOrder, scanOrderingConverter.convert(value));
+  }
+
+  @Test
+  void callConvert_withValidValueAndOrderDescending_shouldReturnScanOrdering() {
+    String value = "id=desc";
+    List<Scan.Ordering> expectedOrder =
+        Collections.singletonList(new Scan.Ordering("id", Scan.Ordering.Order.DESC));
+    Assertions.assertEquals(expectedOrder, scanOrderingConverter.convert(value));
+  }
+}
