66839f8b580ee2ccb8159f2ef24f1b7b59382310
Hiroyuki Yamada <mogwaing@gmail.com>
2025-06-04 23:06:21 +0900

    Backport to branch(3) : Fix typo (#2735)
    Co-authored-by: Toshihiro Suzuki <brfrn169@gmail.com>

--- core/src/main/java/com/scalar/db/exception/transaction/ValidationException.java
+++ core/src/main/java/com/scalar/db/exception/transaction/ValidationException.java
@@ -5,7 +5,7 @@ import javax.annotation.Nullable;
 /**
  * An exception thrown when validating a transaction fails due to transient or nontransient faults.
  * You can try retrying the transaction from the beginning, but the transaction may still fail if
- * the cause is nontranient.
+ * the cause is nontransient.
  */
 public class ValidationException extends TransactionException {
 
