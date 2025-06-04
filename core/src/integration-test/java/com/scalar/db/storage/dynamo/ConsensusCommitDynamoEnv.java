e7c8b7a3b49c857d175cbe449d9024b93ab05e0c
Toshihiro Suzuki <brfrn169@gmail.com>
2025-06-04 23:35:24 +0900

    Merge ConsensusCommitTestUtils and ConsensusCommitIntegrationTestUtils (#2733)

--- core/src/integration-test/java/com/scalar/db/storage/dynamo/ConsensusCommitDynamoEnv.java
+++ core/src/integration-test/java/com/scalar/db/storage/dynamo/ConsensusCommitDynamoEnv.java
@@ -1,6 +1,5 @@
 package com.scalar.db.storage.dynamo;
 
-import com.scalar.db.transaction.consensuscommit.ConsensusCommitIntegrationTestUtils;
 import com.scalar.db.transaction.consensuscommit.ConsensusCommitTestUtils;
 import java.util.Map;
 import java.util.Properties;
@@ -12,7 +11,7 @@ public final class ConsensusCommitDynamoEnv {
     Properties properties = DynamoEnv.getProperties(testName);
 
     // Add testName as a coordinator schema suffix
-    ConsensusCommitIntegrationTestUtils.addSuffixToCoordinatorNamespace(properties, testName);
+    ConsensusCommitTestUtils.addSuffixToCoordinatorNamespace(properties, testName);
 
     return ConsensusCommitTestUtils.loadConsensusCommitProperties(properties);
   }
--- core/src/integration-test/java/com/scalar/db/storage/jdbc/ConsensusCommitJdbcEnv.java
+++ core/src/integration-test/java/com/scalar/db/storage/jdbc/ConsensusCommitJdbcEnv.java
@@ -1,6 +1,5 @@
 package com.scalar.db.storage.jdbc;
 
-import com.scalar.db.transaction.consensuscommit.ConsensusCommitIntegrationTestUtils;
 import com.scalar.db.transaction.consensuscommit.ConsensusCommitTestUtils;
 import java.util.Properties;
 
@@ -11,7 +10,7 @@ public final class ConsensusCommitJdbcEnv {
     Properties properties = JdbcEnv.getProperties(testName);
 
     // Add testName as a coordinator schema suffix
-    ConsensusCommitIntegrationTestUtils.addSuffixToCoordinatorNamespace(properties, testName);
+    ConsensusCommitTestUtils.addSuffixToCoordinatorNamespace(properties, testName);
 
     return ConsensusCommitTestUtils.loadConsensusCommitProperties(properties);
   }
