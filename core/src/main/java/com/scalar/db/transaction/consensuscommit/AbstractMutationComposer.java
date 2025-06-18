918cd8a76d7aa32e6db3c45f609bc27af7c9720a
Hiroyuki Yamada <mogwaing@gmail.com>
2025-06-18 23:26:39 +0900

    Backport to branch(3) : Remove before images after committing or rolling back records in Consensus Commit (#2795)
    Co-authored-by: Toshihiro Suzuki <brfrn169@gmail.com>

--- core/src/main/java/com/scalar/db/transaction/consensuscommit/AbstractMutationComposer.java
+++ core/src/main/java/com/scalar/db/transaction/consensuscommit/AbstractMutationComposer.java
@@ -3,8 +3,12 @@ package com.scalar.db.transaction.consensuscommit;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableList;
 import com.scalar.db.api.Mutation;
+import com.scalar.db.api.PutBuilder;
+import com.scalar.db.api.TableMetadata;
+import com.scalar.db.io.DataType;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import javax.annotation.concurrent.NotThreadSafe;
 
 @NotThreadSafe
@@ -33,5 +37,51 @@ public abstract class AbstractMutationComposer implements MutationComposer {
   @Override
   public List<Mutation> get() {
     return ImmutableList.copyOf(mutations);
+  }
+
+  static void setBeforeImageColumnsToNull(
+      PutBuilder.Buildable putBuilder,
+      Set<String> beforeImageColumnNames,
+      TableMetadata tableMetadata) {
+    for (String beforeImageColumnName : beforeImageColumnNames) {
+      DataType columnDataType = tableMetadata.getColumnDataType(beforeImageColumnName);
+      switch (columnDataType) {
+        case BOOLEAN:
+          putBuilder.booleanValue(beforeImageColumnName, null);
+          break;
+        case INT:
+          putBuilder.intValue(beforeImageColumnName, null);
+          break;
+        case BIGINT:
+          putBuilder.bigIntValue(beforeImageColumnName, null);
+          break;
+        case FLOAT:
+          putBuilder.floatValue(beforeImageColumnName, null);
+          break;
+        case DOUBLE:
+          putBuilder.doubleValue(beforeImageColumnName, null);
+          break;
+        case TEXT:
+          putBuilder.textValue(beforeImageColumnName, null);
+          break;
+        case BLOB:
+          putBuilder.blobValue(beforeImageColumnName, (byte[]) null);
+          break;
+        case DATE:
+          putBuilder.dateValue(beforeImageColumnName, null);
+          break;
+        case TIME:
+          putBuilder.timeValue(beforeImageColumnName, null);
+          break;
+        case TIMESTAMP:
+          putBuilder.timestampValue(beforeImageColumnName, null);
+          break;
+        case TIMESTAMPTZ:
+          putBuilder.timestampTZValue(beforeImageColumnName, null);
+          break;
+        default:
+          throw new AssertionError("Unknown data type: " + columnDataType);
+      }
+    }
   }
 }
