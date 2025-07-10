5ee09405f4da1ebb3bb2f61d57c3e9f4e22b01b6
Kodai Doki <kodai.doki@scalar-labs.com>
2025-07-10 18:02:41 +0900

    Update AdminTestUtils

--- core/src/integration-test/java/com/scalar/db/storage/jdbc/JdbcAdminTestUtils.java
+++ core/src/integration-test/java/com/scalar/db/storage/jdbc/JdbcAdminTestUtils.java
@@ -6,22 +6,26 @@ import com.scalar.db.config.DatabaseConfig;
 import com.scalar.db.util.AdminTestUtils;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 public class JdbcAdminTestUtils extends AdminTestUtils {
 
-  private final JdbcConfig config;
   private final String metadataSchema;
   private final RdbEngineStrategy rdbEngine;
+  private final BasicDataSource dataSource;
 
   public JdbcAdminTestUtils(Properties properties) {
     super(properties);
-    config = new JdbcConfig(new DatabaseConfig(properties));
+    JdbcConfig config = new JdbcConfig(new DatabaseConfig(properties));
     metadataSchema =
         config.getTableMetadataSchema().orElse(DatabaseConfig.DEFAULT_SYSTEM_NAMESPACE_NAME);
     rdbEngine = RdbEngineFactory.create(config);
+    dataSource = JdbcUtils.initDataSourceForAdmin(config, rdbEngine);
   }
 
   @Override
@@ -53,9 +57,61 @@ public class JdbcAdminTestUtils extends AdminTestUtils {
   }
 
   private void execute(String sql) throws SQLException {
-    try (BasicDataSource dataSource = JdbcUtils.initDataSourceForAdmin(config, rdbEngine);
-        Connection connection = dataSource.getConnection()) {
+    try (Connection connection = dataSource.getConnection()) {
       JdbcAdmin.execute(connection, sql);
     }
+  }
+
+  @Override
+  public boolean namespaceExists(String namespace) throws SQLException {
+    String sql;
+    if (JdbcTestUtils.isMysql(rdbEngine)) {
+      sql = "SELECT 1 FROM information_schema.schemata WHERE schema_name = ?";
+    } else if (JdbcTestUtils.isOracle(rdbEngine)) {
+      sql = "SELECT 1 FROM all_users WHERE username = ?";
+    } else if (JdbcTestUtils.isPostgresql(rdbEngine)) {
+      sql = "SELECT 1 FROM pg_namespace WHERE nspname = ?";
+    } else if (JdbcTestUtils.isSqlite(rdbEngine)) {
+      // SQLite has no concept of namespace
+      return true;
+    } else if (JdbcTestUtils.isSqlServer(rdbEngine)) {
+      sql = "SELECT 1 FROM sys.schemas WHERE name = ?";
+    } else {
+      throw new AssertionError("Unsupported engine : " + rdbEngine.getClass().getSimpleName());
+    }
+
+    try (Connection connection = dataSource.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+      preparedStatement.setString(1, namespace);
+      ResultSet resultSet = preparedStatement.executeQuery();
+
+      return resultSet.next();
+    }
+  }
+
+  @Override
+  public boolean tableExists(String namespace, String table) throws Exception {
+    String fullTableName = rdbEngine.encloseFullTableName(namespace, table);
+    String sql = rdbEngine.tableExistsInternalTableCheckSql(fullTableName);
+    try (Connection connection = dataSource.getConnection();
+        Statement statement = connection.createStatement()) {
+      statement.execute(sql);
+      return true;
+    } catch (SQLException e) {
+      // An exception will be thrown if the table does not exist when executing the select
+      // query
+      if (rdbEngine.isUndefinedTableError(e)) {
+        return false;
+      }
+      throw new Exception(
+          String.format(
+              "Checking if the %s table exists failed", getFullTableName(namespace, table)),
+          e);
+    }
+  }
+
+  @Override
+  public void close() throws SQLException {
+    dataSource.close();
   }
 }
--- server/src/integration-test/java/com/scalar/db/server/ServerAdminTestUtils.java
+++ server/src/integration-test/java/com/scalar/db/server/ServerAdminTestUtils.java
@@ -5,6 +5,7 @@ import static com.scalar.db.util.ScalarDbUtils.getFullTableName;
 import com.scalar.db.config.DatabaseConfig;
 import com.scalar.db.storage.jdbc.JdbcAdmin;
 import com.scalar.db.storage.jdbc.JdbcConfig;
+import com.scalar.db.storage.jdbc.JdbcTestUtils;
 import com.scalar.db.storage.jdbc.JdbcUtils;
 import com.scalar.db.storage.jdbc.RdbEngineFactory;
 import com.scalar.db.storage.jdbc.RdbEngineOracle;
@@ -12,6 +13,8 @@ import com.scalar.db.storage.jdbc.RdbEngineStrategy;
 import com.scalar.db.util.AdminTestUtils;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
@@ -20,17 +23,18 @@ import org.apache.commons.dbcp2.BasicDataSource;
 public class ServerAdminTestUtils extends AdminTestUtils {
 
   private final RdbEngineStrategy rdbEngine;
-  private final JdbcConfig config;
   private final String metadataNamespace;
   private final String metadataTable;
+  private final BasicDataSource dataSource;
 
   public ServerAdminTestUtils(Properties jdbcStorageProperties) {
     super(jdbcStorageProperties);
-    config = new JdbcConfig(new DatabaseConfig(jdbcStorageProperties));
+    JdbcConfig config = new JdbcConfig(new DatabaseConfig(jdbcStorageProperties));
     metadataNamespace =
         config.getTableMetadataSchema().orElse(DatabaseConfig.DEFAULT_SYSTEM_NAMESPACE_NAME);
     metadataTable = JdbcAdmin.METADATA_TABLE;
     rdbEngine = RdbEngineFactory.create(config);
+    dataSource = JdbcUtils.initDataSourceForAdmin(config, rdbEngine);
   }
 
   @Override
@@ -66,10 +70,60 @@ public class ServerAdminTestUtils extends AdminTestUtils {
   }
 
   private void execute(String sql) throws SQLException {
-    try (BasicDataSource dataSource = JdbcUtils.initDataSourceForAdmin(config, rdbEngine);
-        Connection connection = dataSource.getConnection();
+    try (Connection connection = dataSource.getConnection();
         Statement stmt = connection.createStatement()) {
       stmt.execute(sql);
     }
   }
+
+  @Override
+  public boolean namespaceExists(String namespace) throws SQLException {
+    String sql;
+    if (JdbcTestUtils.isMysql(rdbEngine)) {
+      sql = "SELECT 1 FROM information_schema.schemata WHERE schema_name = ?";
+    } else if (JdbcTestUtils.isOracle(rdbEngine)) {
+      sql = "SELECT 1 FROM all_users WHERE username = ?";
+    } else if (JdbcTestUtils.isPostgresql(rdbEngine)) {
+      sql = "SELECT 1 FROM pg_namespace WHERE nspname = ?";
+    } else if (JdbcTestUtils.isSqlite(rdbEngine)) {
+      // SQLite has no concept of namespace
+      return true;
+    } else if (JdbcTestUtils.isSqlServer(rdbEngine)) {
+      sql = "SELECT 1 FROM sys.schemas WHERE name = ?";
+    } else {
+      throw new AssertionError("Unsupported engine : " + rdbEngine.getClass().getSimpleName());
+    }
+
+    try (Connection connection = dataSource.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+      preparedStatement.setString(1, namespace);
+      ResultSet resultSet = preparedStatement.executeQuery();
+
+      return resultSet.next();
+    }
+  }
+
+  @Override
+  public boolean tableExists(String namespace, String table) throws Exception {
+    String fullTableName = rdbEngine.encloseFullTableName(namespace, table);
+    String sql = rdbEngine.tableExistsInternalTableCheckSql(fullTableName);
+    try (Connection connection = dataSource.getConnection();
+        Statement statement = connection.createStatement()) {
+      statement.execute(sql);
+      return true;
+    } catch (SQLException e) {
+      // An exception will be thrown if the table does not exist when executing the select
+      // query
+      if (rdbEngine.isUndefinedTableError(e)) {
+        return false;
+      }
+      throw new Exception(
+          String.format(
+              "Checking if the %s table exists failed", getFullTableName(namespace, table)),
+          e);
+    }
+  }
+
+  @Override
+  public void close() throws Exception {}
 }
