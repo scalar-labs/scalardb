2c01e058a58fe95baf6ad44d6ac5efdbbbdebe09
Kodai Doki <kodai.doki@scalar-labs.com>
2025-07-10 17:52:10 +0900

    Update AdminTestUtils

--- core/src/integration-test/java/com/scalar/db/storage/cassandra/CassandraAdminTestUtils.java
+++ core/src/integration-test/java/com/scalar/db/storage/cassandra/CassandraAdminTestUtils.java
@@ -1,12 +1,16 @@
 package com.scalar.db.storage.cassandra;
 
+import com.scalar.db.config.DatabaseConfig;
 import com.scalar.db.util.AdminTestUtils;
 import java.util.Properties;
 
 public class CassandraAdminTestUtils extends AdminTestUtils {
+  private final ClusterManager clusterManager;
 
   public CassandraAdminTestUtils(Properties properties) {
     super(properties);
+    DatabaseConfig databaseConfig = new DatabaseConfig(properties);
+    clusterManager = new ClusterManager(databaseConfig);
   }
 
   @Override
@@ -22,5 +26,20 @@ public class CassandraAdminTestUtils extends AdminTestUtils {
   @Override
   public void corruptMetadata(String namespace, String table) {
     // Do nothing
+  }
+
+  @Override
+  public boolean namespaceExists(String namespace) {
+    return clusterManager.getSession().getCluster().getMetadata().getKeyspace(namespace) != null;
+  }
+
+  @Override
+  public boolean tableExists(String namespace, String table) {
+    return clusterManager.getMetadata(namespace, table) != null;
+  }
+
+  @Override
+  public void close() {
+    clusterManager.close();
   }
 }
