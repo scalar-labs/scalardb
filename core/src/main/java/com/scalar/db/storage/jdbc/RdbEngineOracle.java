d73103dc3f24a225f7b387ce80ca48768222ef5e
Hiroyuki Yamada <mogwaing@gmail.com>
2025-07-18 22:44:25 +0900

    Backport to branch(3.15) : Fix Oracle engine to support multiple Oracle users for index operations (#2890)
    Co-authored-by: Kodai Doki <52027276+KodaiD@users.noreply.github.com>

--- core/src/main/java/com/scalar/db/storage/jdbc/RdbEngineOracle.java
+++ core/src/main/java/com/scalar/db/storage/jdbc/RdbEngineOracle.java
@@ -149,8 +149,22 @@ public class RdbEngineOracle extends AbstractRdbEngine {
   }
 
   @Override
+  public String createIndexSql(
+      String schema, String table, String indexName, String indexedColumn) {
+    return "CREATE INDEX "
+        + enclose(schema)
+        + "."
+        + enclose(indexName)
+        + " ON "
+        + encloseFullTableName(schema, table)
+        + " ("
+        + enclose(indexedColumn)
+        + ")";
+  }
+
+  @Override
   public String dropIndexSql(String schema, String table, String indexName) {
-    return "DROP INDEX " + enclose(indexName);
+    return "DROP INDEX " + enclose(schema) + "." + enclose(indexName);
   }
 
   @Override
