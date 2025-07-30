f20f7674681d7f47a63ab210ae3130826d254917
Kodai Doki <kodai.doki@scalar-labs.com>
2025-07-30 18:39:49 +0900

    Add JDBC database permission test for MariaDB, YugabyteDB and Db2

--- core/src/integration-test/java/com/scalar/db/storage/jdbc/JdbcAdminPermissionIntegrationTest.java
+++ core/src/integration-test/java/com/scalar/db/storage/jdbc/JdbcAdminPermissionIntegrationTest.java
@@ -1,15 +1,24 @@
 package com.scalar.db.storage.jdbc;
 
+import static com.scalar.db.storage.jdbc.JdbcPermissionTestUtils.DDL_WAIT_SECONDS;
+
+import com.google.common.util.concurrent.Uninterruptibles;
 import com.scalar.db.api.DistributedStorageAdminPermissionIntegrationTestBase;
+import com.scalar.db.config.DatabaseConfig;
 import com.scalar.db.util.AdminTestUtils;
 import com.scalar.db.util.PermissionTestUtils;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 public class JdbcAdminPermissionIntegrationTest
     extends DistributedStorageAdminPermissionIntegrationTestBase {
+  private RdbEngineStrategy rdbEngine;
+
   @Override
   protected Properties getProperties(String testName) {
-    return JdbcEnv.getProperties(testName);
+    Properties properties = JdbcEnv.getProperties(testName);
+    rdbEngine = RdbEngineFactory.create(new JdbcConfig(new DatabaseConfig(properties)));
+    return properties;
   }
 
   @Override
@@ -25,5 +34,45 @@ public class JdbcAdminPermissionIntegrationTest
   @Override
   protected PermissionTestUtils getPermissionTestUtils(String testName) {
     return new JdbcPermissionTestUtils(getProperties(testName));
+  }
+
+  @Override
+  protected void waitForTableCreation() {
+    if (JdbcTestUtils.isYugabyte(rdbEngine)) {
+      // This is needed to avoid schema or catalog version mismatch database errors.
+      Uninterruptibles.sleepUninterruptibly(DDL_WAIT_SECONDS, TimeUnit.SECONDS);
+    }
+  }
+
+  @Override
+  protected void waitForNamespaceCreation() {
+    if (JdbcTestUtils.isYugabyte(rdbEngine)) {
+      // This is needed to avoid schema or catalog version mismatch database errors.
+      Uninterruptibles.sleepUninterruptibly(DDL_WAIT_SECONDS, TimeUnit.SECONDS);
+    }
+  }
+
+  @Override
+  protected void waitForTableDeletion() {
+    if (JdbcTestUtils.isYugabyte(rdbEngine)) {
+      // This is needed to avoid schema or catalog version mismatch database errors.
+      Uninterruptibles.sleepUninterruptibly(DDL_WAIT_SECONDS, TimeUnit.SECONDS);
+    }
+  }
+
+  @Override
+  protected void waitForNamespaceDeletion() {
+    if (JdbcTestUtils.isYugabyte(rdbEngine)) {
+      // This is needed to avoid schema or catalog version mismatch database errors.
+      Uninterruptibles.sleepUninterruptibly(DDL_WAIT_SECONDS, TimeUnit.SECONDS);
+    }
+  }
+
+  @Override
+  protected void sleepBetweenTests() {
+    if (JdbcTestUtils.isYugabyte(rdbEngine)) {
+      // This is needed to avoid schema or catalog version mismatch database errors.
+      Uninterruptibles.sleepUninterruptibly(DDL_WAIT_SECONDS, TimeUnit.SECONDS);
+    }
   }
 }
