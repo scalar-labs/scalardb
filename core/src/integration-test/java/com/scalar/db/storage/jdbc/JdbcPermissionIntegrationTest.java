20774b4a88eb677531fde733458c924051db801d
Hiroyuki Yamada <mogwaing@gmail.com>
2025-08-05 14:03:14 +0900

    Backport to branch(3) : Add JDBC database permission test (Part 2) (#2944)
    Co-authored-by: Kodai Doki <52027276+KodaiD@users.noreply.github.com>
    Co-authored-by: Mitsunori Komatsu <komamitsu@gmail.com>
    Co-authored-by: Toshihiro Suzuki <brfrn169@gmail.com>

--- core/src/integration-test/java/com/scalar/db/storage/jdbc/JdbcPermissionIntegrationTest.java
+++ core/src/integration-test/java/com/scalar/db/storage/jdbc/JdbcPermissionIntegrationTest.java
@@ -1,14 +1,23 @@
 package com.scalar.db.storage.jdbc;
 
+import static com.scalar.db.storage.jdbc.JdbcPermissionTestUtils.DDL_WAIT_SECONDS;
+
+import com.google.common.util.concurrent.Uninterruptibles;
 import com.scalar.db.api.DistributedStoragePermissionIntegrationTestBase;
+import com.scalar.db.config.DatabaseConfig;
 import com.scalar.db.util.AdminTestUtils;
 import com.scalar.db.util.PermissionTestUtils;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 public class JdbcPermissionIntegrationTest extends DistributedStoragePermissionIntegrationTestBase {
+  private RdbEngineStrategy rdbEngine;
+
   @Override
   protected Properties getProperties(String testName) {
-    return JdbcEnv.getProperties(testName);
+    Properties properties = JdbcEnv.getProperties(testName);
+    rdbEngine = RdbEngineFactory.create(new JdbcConfig(new DatabaseConfig(properties)));
+    return properties;
   }
 
   @Override
@@ -24,5 +33,13 @@ public class JdbcPermissionIntegrationTest extends DistributedStoragePermissionI
   @Override
   protected AdminTestUtils getAdminTestUtils(String testName) {
     return new JdbcAdminTestUtils(getProperties(testName));
+  }
+
+  @Override
+  protected void waitForDdlCompletion() {
+    if (JdbcTestUtils.isYugabyte(rdbEngine)) {
+      // This is needed to avoid schema or catalog version mismatch database errors.
+      Uninterruptibles.sleepUninterruptibly(DDL_WAIT_SECONDS, TimeUnit.SECONDS);
+    }
   }
 }
