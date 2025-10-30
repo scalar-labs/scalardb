8938ee98017b9f1f179be94dadc751c39908b6de
Kodai Doki <kodai.doki@scalar-labs.com>
2025-10-30 23:48:16 +0900

    Add integration tests for Wrapper

---
+++ core/src/integration-test/java/com/scalar/db/storage/objectstorage/BlobWrapperIntegrationTest.java
@@ -0,0 +1,10 @@
+package com.scalar.db.storage.objectstorage;
+
+import java.util.Properties;
+
+public class BlobWrapperIntegrationTest extends ObjectStorageWrapperIntegrationTestBase {
+  @Override
+  protected Properties getProperties(String testName) {
+    return ObjectStorageEnv.getProperties(testName);
+  }
+}
