33ec6e7ef3219a4f710636959d8bc765588ddb07
Jishnu J <jishnu.j@innovaturelabs.com>
2025-11-20 12:32:02 +0530

    Fixed spotless issue

--- data-loader/core/src/test/java/com/scalar/db/dataloader/core/dataimport/log/SingleFileImportLoggerTest.java
+++ data-loader/core/src/test/java/com/scalar/db/dataloader/core/dataimport/log/SingleFileImportLoggerTest.java
@@ -2,7 +2,6 @@ package com.scalar.db.dataloader.core.dataimport.log;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
