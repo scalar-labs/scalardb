3df7d06ce9f6d6415a31e125cf46d4ef3b45e63f
Mitsunori Komatsu <komamitsu@gmail.com>
2025-12-01 16:22:24 +0900

    Add AuthAdmin.getRole(roleName)

--- core/src/main/java/com/scalar/db/api/AuthAdmin.java
+++ core/src/main/java/com/scalar/db/api/AuthAdmin.java
@@ -198,6 +198,16 @@ public interface AuthAdmin {
   }
 
   /**
+   * Retrieves a {@link Role}.
+   *
+   * @return a {@link Role}
+   * @throws ExecutionException if the operation fails
+   */
+  default Optional<Role> getRole(String roleName) throws ExecutionException {
+    throw new UnsupportedOperationException(CoreError.AUTH_NOT_ENABLED.buildMessage());
+  }
+
+  /**
    * Retrieves a list of {@link Role}s.
    *
    * @return a list of {@link Role}s
