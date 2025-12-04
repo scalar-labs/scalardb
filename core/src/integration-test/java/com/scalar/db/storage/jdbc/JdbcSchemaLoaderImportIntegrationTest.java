885ebc3a41c8c2741aa5d9a855ab61bb8dbae90a
Toshihiro Suzuki <brfrn169@gmail.com>
2025-12-04 23:27:43 +0900

    Add integration test for virtual tables with imported tables having various primary key types (#3240)

--- core/src/integration-test/java/com/scalar/db/storage/jdbc/JdbcSchemaLoaderImportIntegrationTest.java
+++ core/src/integration-test/java/com/scalar/db/storage/jdbc/JdbcSchemaLoaderImportIntegrationTest.java
@@ -16,7 +16,7 @@ import org.junit.jupiter.api.condition.DisabledIf;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@DisabledIf("isSqlite")
+@DisabledIf("com.scalar.db.storage.jdbc.JdbcEnv#isSqlite")
 public class JdbcSchemaLoaderImportIntegrationTest extends SchemaLoaderImportIntegrationTestBase {
 
   private static final Logger logger =
@@ -197,11 +197,6 @@ public class JdbcSchemaLoaderImportIntegrationTest extends SchemaLoaderImportInt
     } catch (Exception e) {
       logger.warn("Failed to call super.afterAll", e);
     }
-  }
-
-  @SuppressWarnings("unused")
-  private static boolean isSqlite() {
-    return JdbcEnv.isSqlite();
   }
 
   @Override
