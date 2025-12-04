ef18662a1b1a22b5c3259dce1711f0af645f6a9c
Hiroyuki Yamada <mogwaing@gmail.com>
2025-12-04 23:26:52 +0900

    Backport to branch(3.17) : Add configuration option to enable/disable active transaction management (#3255)
    Co-authored-by: Toshihiro Suzuki <brfrn169@gmail.com>

--- core/src/main/java/com/scalar/db/common/TwoPhaseCommitTransactionExpirationHandlerSettable.java
+++
@@ -1,9 +0,0 @@
-package com.scalar.db.common;
-
-import com.scalar.db.api.TwoPhaseCommitTransaction;
-import java.util.function.BiConsumer;
-
-@FunctionalInterface
-public interface TwoPhaseCommitTransactionExpirationHandlerSettable {
-  void setTransactionExpirationHandler(BiConsumer<String, TwoPhaseCommitTransaction> handler);
-}
