9a3e00d51febd53afa0995e8bfe6fd84a4be3690
Kodai Doki <kodai.doki@scalar-labs.com>
2025-12-05 09:52:15 +0900

    Remove integraition test

--- core/src/integration-test/java/com/scalar/db/storage/objectstorage/ObjectStorageIntegrationTest.java
+++ core/src/integration-test/java/com/scalar/db/storage/objectstorage/ObjectStorageIntegrationTest.java
@@ -1,20 +1,11 @@
 package com.scalar.db.storage.objectstorage;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.scalar.db.api.DistributedStorageIntegrationTestBase;
-import com.scalar.db.api.Get;
-import com.scalar.db.api.Put;
-import com.scalar.db.api.Result;
 import com.scalar.db.api.TableMetadata;
-import com.scalar.db.exception.storage.ExecutionException;
 import com.scalar.db.io.DataType;
-import java.util.Arrays;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Properties;
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 
 public class ObjectStorageIntegrationTest extends DistributedStorageIntegrationTestBase {
 
@@ -66,25 +57,4 @@ public class ObjectStorageIntegrationTest extends DistributedStorageIntegrationT
   @Override
   @Disabled("Object Storage does not support index-related operations")
   public void scan_ScanGivenForNonIndexedColumn_ShouldThrowIllegalArgumentException() {}
-
-  @Test
-  public void put_PutWithLargeTextValue_ShouldStoreProperly() throws ExecutionException {
-    // Arrange
-    int payloadSizeBytes = 20000000; // The default string length limit of Jackson
-    char[] chars = new char[payloadSizeBytes + 1];
-    Arrays.fill(chars, 'a');
-    String largeText = new String(chars);
-    Put put = preparePuts().get(0);
-    put = Put.newBuilder(put).textValue(getColumnName2(), largeText).build();
-
-    // Act
-    storage.put(put);
-
-    // Assert
-    Get get = prepareGet(0, 0);
-    Optional<Result> actual = storage.get(get);
-    assertThat(actual.isPresent()).isTrue();
-    assertThat(actual.get().contains(getColumnName2())).isTrue();
-    assertThat(actual.get().getText(getColumnName2())).isEqualTo(largeText);
-  }
 }
