5c9f2310df0e6ba9ca10383e853ad7c294cb6f94
Vincent Guilpain <vincent.guilpain@scalar-labs.com>
2026-01-06 14:29:10 +0900

    Change vars in wf dispatch script

--- ci/generate-test-matrix-for-wf-dispatch.groovy
+++ ci/generate-test-matrix-for-wf-dispatch.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-// This script generates a test matrix for the node integration tests based on the environment variables
+// This script generates a test matrix for the integration tests based on the environment variables
 
 @Grab('org.json:json:20250517')
 
@@ -11,12 +11,12 @@ import groovy.transform.SourceURI
 // Check for required environment variables
 def requiredEnvVars = [
         'ALL',
-        'ABAC',
-        'AUTH',
-        'BASIC',
-        'EMBEDDING',
-        'ENCRYPTION',
-        'REPLICATION'
+        'BLOBSTORAGE',
+        'CASSANDRA',
+        'COSMOS',
+        'DYNAMO',
+        'JDBC',
+        'MULTISTORAGE',
 ]
 
 def missingVars = []
@@ -33,12 +33,12 @@ if (!missingVars.isEmpty()) {
 
 // Read input parameters from environment variables
 def allTest = Boolean.parseBoolean(System.getenv('ALL'))
-def abacTest = Boolean.parseBoolean(System.getenv('ABAC'))
-def authTest = Boolean.parseBoolean(System.getenv('AUTH'))
-def basicTest = Boolean.parseBoolean(System.getenv('BASIC'))
-def embeddingTest = Boolean.parseBoolean(System.getenv('EMBEDDING'))
-def encryptionTest = Boolean.parseBoolean(System.getenv('ENCRYPTION'))
-def replicationTest = Boolean.parseBoolean(System.getenv('REPLICATION'))
+def blobstorageTest = Boolean.parseBoolean(System.getenv('BLOBSTORAGE'))
+def cassandraTest = Boolean.parseBoolean(System.getenv('CASSANDRA'))
+def cosmosTest = Boolean.parseBoolean(System.getenv('COSMOS'))
+def dynamoTest = Boolean.parseBoolean(System.getenv('DYNAMO'))
+def jdbcTest = Boolean.parseBoolean(System.getenv('JDBC'))
+def multistorageTest = Boolean.parseBoolean(System.getenv('MULTISTORAGE'))
 
 // Load the tests organized by category from JSON file
 @SourceURI
@@ -47,23 +47,23 @@ def testsByCategory = new JsonSlurper().parse(new File(scriptBasePath.resolve("n
 
 // Filter the tests based on inputs - remove the disabled categories
 if (!allTest) {
-    if (!abacTest) {
-        testsByCategory.remove("abac")
+    if (!blobstorageTest) {
+        testsByCategory.remove("blobstorage")
     }
-    if (!authTest) {
-        testsByCategory.remove("auth")
+    if (!cassandraTest) {
+        testsByCategory.remove("cassandra")
     }
-    if (!basicTest) {
-        testsByCategory.remove("basic")
+    if (!cosmosTest) {
+        testsByCategory.remove("cosmos")
     }
-    if (!embeddingTest) {
-        testsByCategory.remove("embedding")
+    if (!dynamoTest) {
+        testsByCategory.remove("dynamo")
     }
-    if (!encryptionTest) {
-        testsByCategory.remove("encryption")
+    if (!jdbcTest) {
+        testsByCategory.remove("jdbc")
     }
-    if (!replicationTest) {
-        testsByCategory.remove("replication")
+    if (!multistorageTest) {
+        testsByCategory.remove("multistorage")
     }
 }
 
