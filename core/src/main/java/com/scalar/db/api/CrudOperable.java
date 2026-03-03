ebdc9762d1b5df3cc0e8a4d47a039cbcd4055f03
Toshihiro Suzuki <brfrn169@gmail.com>
2026-03-03 09:53:40 +0900

    Improve Javadoc for get and scan methods in DistributedStorage and CrudOperable (#3383)

--- core/src/main/java/com/scalar/db/api/CrudOperable.java
+++ core/src/main/java/com/scalar/db/api/CrudOperable.java
@@ -19,43 +19,52 @@ import java.util.Optional;
 public interface CrudOperable<E extends TransactionException> {
 
   /**
-   * Retrieves a result from the storage through a transaction with the specified {@link Get}
-   * command with a primary key and returns the result.
+   * Retrieves a result from the storage through a transaction with the specified {@link Get} or
+   * {@link GetWithIndex} command.
    *
-   * @param get a {@code Get} command
+   * <ul>
+   *   <li>{@link Get} : retrieves a result by specifying a primary key (partition key and optional
+   *       clustering key).
+   *   <li>{@link GetWithIndex} : retrieves a result by specifying an index key.
+   * </ul>
+   *
+   * @param get a {@link Get} or {@link GetWithIndex} command
    * @return an {@code Optional} with the returned result
    * @throws E if the transaction CRUD operation fails
    */
   Optional<Result> get(Get get) throws E;
 
   /**
-   * Retrieves results from the storage through a transaction with the specified {@link Scan} or
-   * {@link ScanAll} or {@link ScanWithIndex} command with a partition key and returns a list of
-   * {@link Result}. Results can be filtered by specifying a range of clustering keys.
+   * Retrieves results from the storage through a transaction with the specified {@link Scan},
+   * {@link ScanAll}, or {@link ScanWithIndex} command and returns a list of {@link Result}.
    *
    * <ul>
-   *   <li>{@link Scan} : by specifying a partition key, it will return results within the
-   *       partition. Results can be filtered by specifying a range of clustering keys.
-   *   <li>{@link ScanAll} : for a given table, it will return all its records even if they span
-   *       several partitions.
-   *   <li>{@link ScanWithIndex} : by specifying an index key, it will return results within the
-   *       index.
+   *   <li>{@link Scan} : retrieves results within a partition by specifying a partition key.
+   *       Results can be filtered by specifying a range of clustering keys.
+   *   <li>{@link ScanAll} : retrieves all records in a table even if they span several partitions.
+   *   <li>{@link ScanWithIndex} : retrieves results by specifying an index key.
    * </ul>
    *
-   * @param scan a {@code Scan} command
+   * @param scan a {@link Scan}, {@link ScanAll}, or {@link ScanWithIndex} command
    * @return a list of {@link Result}
    * @throws E if the transaction CRUD operation fails
    */
   List<Result> scan(Scan scan) throws E;
 
   /**
-   * Retrieves results from the storage through a transaction with the specified {@link Scan} or
-   * {@link ScanAll} or {@link ScanWithIndex} command with a partition key and returns a {@link
-   * Scanner} to iterate over the results. Results can be filtered by specifying a range of
-   * clustering keys.
+   * Retrieves results from the storage through a transaction with the specified {@link Scan},
+   * {@link ScanAll}, or {@link ScanWithIndex} command and returns a {@link Scanner} to iterate over
+   * the results.
    *
-   * @param scan a {@code Scan} command
-   * @return a {@code Scanner} to iterate over the results
+   * <ul>
+   *   <li>{@link Scan} : retrieves results within a partition by specifying a partition key.
+   *       Results can be filtered by specifying a range of clustering keys.
+   *   <li>{@link ScanAll} : retrieves all records in a table even if they span several partitions.
+   *   <li>{@link ScanWithIndex} : retrieves results by specifying an index key.
+   * </ul>
+   *
+   * @param scan a {@link Scan}, {@link ScanAll}, or {@link ScanWithIndex} command
+   * @return a {@link Scanner} to iterate over the results
    * @throws E if the transaction CRUD operation fails
    */
   Scanner<E> getScanner(Scan scan) throws E;
