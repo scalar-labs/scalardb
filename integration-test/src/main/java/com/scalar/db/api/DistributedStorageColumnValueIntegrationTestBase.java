21b040cebf419a115e8dd93ea261b22cf7fc3732
Vincent Guilpain <vincent.guilpain@scalar-labs.com>
2026-03-09 11:20:51 +0900

    Add test for all storages

--- integration-test/src/main/java/com/scalar/db/api/DistributedStorageColumnValueIntegrationTestBase.java
+++ integration-test/src/main/java/com/scalar/db/api/DistributedStorageColumnValueIntegrationTestBase.java
@@ -30,6 +30,7 @@ import java.util.Optional;
 import java.util.Properties;
 import java.util.Random;
 import java.util.TimeZone;
+import java.util.stream.Stream;
 import javax.annotation.Nullable;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.AfterAll;
@@ -38,7 +39,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -683,6 +686,160 @@ public abstract class DistributedStorageColumnValueIntegrationTestBase {
       // Reset JVM default time zone to the original value
       TimeZone.setDefault(originalDefaultTimezone);
     }
+  }
+
+  @ParameterizedTest()
+  @MethodSource("provideBlobSizes")
+  public void put_largeBlobData_ShouldWorkCorrectly(int blobSize, String humanReadableBlobSize)
+      throws ExecutionException {
+    String tableName = TABLE + "_large_single_blob";
+    try {
+      // Arrange
+      TableMetadata.Builder metadata =
+          TableMetadata.newBuilder()
+              .addColumn(COL_NAME1, DataType.INT)
+              .addColumn(COL_NAME2, DataType.BLOB)
+              .addPartitionKey(COL_NAME1);
+
+      admin.createTable(namespace, tableName, metadata.build(), true, getCreationOptions());
+      byte[] blobData = createLargeBlob(blobSize);
+      Put put =
+          Put.newBuilder()
+              .namespace(namespace)
+              .table(tableName)
+              .partitionKey(Key.ofInt(COL_NAME1, 1))
+              .blobValue(COL_NAME2, blobData)
+              .build();
+
+      // Act
+      storage.put(put);
+
+      // Assert
+      Optional<Result> optionalResult =
+          storage.get(
+              Get.newBuilder()
+                  .namespace(namespace)
+                  .table(tableName)
+                  .partitionKey(Key.ofInt(COL_NAME1, 1))
+                  .build());
+      assertThat(optionalResult).isPresent();
+      Result result = optionalResult.get();
+      assertThat(result.getColumns().get(COL_NAME2).getBlobValueAsBytes()).isEqualTo(blobData);
+    } finally {
+      admin.dropTable(namespace, tableName, true);
+    }
+  }
+
+  @Test
+  public void put_largeBlobData_WithMultipleBlobColumnsShouldWorkCorrectly()
+      throws ExecutionException {
+    String tableName = TABLE + "_large_multiples_blob";
+    try {
+      // Arrange
+      TableMetadata.Builder metadata =
+          TableMetadata.newBuilder()
+              .addColumn(COL_NAME1, DataType.INT)
+              .addColumn(COL_NAME2, DataType.BLOB)
+              .addColumn(COL_NAME3, DataType.BLOB)
+              .addPartitionKey(COL_NAME1);
+
+      admin.createTable(namespace, tableName, metadata.build(), true, getCreationOptions());
+      byte[] blobDataCol2 = createLargeBlob(32_766);
+      byte[] blobDataCol3 = createLargeBlob(5000);
+      Put put =
+          Put.newBuilder()
+              .namespace(namespace)
+              .table(tableName)
+              .partitionKey(Key.ofInt(COL_NAME1, 1))
+              .blobValue(COL_NAME2, blobDataCol2)
+              .blobValue(COL_NAME3, blobDataCol3)
+              .build();
+
+      // Act
+      storage.put(put);
+
+      // Assert
+      Optional<Result> optionalResult =
+          storage.get(
+              Get.newBuilder()
+                  .namespace(namespace)
+                  .table(tableName)
+                  .partitionKey(Key.ofInt(COL_NAME1, 1))
+                  .build());
+      assertThat(optionalResult).isPresent();
+      Result result = optionalResult.get();
+      assertThat(result.getColumns().get(COL_NAME2).getBlobValueAsBytes()).isEqualTo(blobDataCol2);
+      assertThat(result.getColumns().get(COL_NAME3).getBlobValueAsBytes()).isEqualTo(blobDataCol3);
+    } finally {
+      admin.dropTable(namespace, tableName, true);
+    }
+  }
+
+  @Test
+  public void put_largeBlobData_WithAllColumnsTypesShouldWorkCorrectly() throws ExecutionException {
+    // Arrange
+    IntColumn partitionKeyValue = (IntColumn) getColumnWithMaxValue(PARTITION_KEY, DataType.INT);
+    BooleanColumn col1Value = (BooleanColumn) getColumnWithMaxValue(COL_NAME1, DataType.BOOLEAN);
+    IntColumn col2Value = (IntColumn) getColumnWithMaxValue(COL_NAME2, DataType.INT);
+    BigIntColumn col3Value = (BigIntColumn) getColumnWithMaxValue(COL_NAME3, DataType.BIGINT);
+    FloatColumn col4Value = (FloatColumn) getColumnWithMaxValue(COL_NAME4, DataType.FLOAT);
+    DoubleColumn col5Value = (DoubleColumn) getColumnWithMaxValue(COL_NAME5, DataType.DOUBLE);
+    TextColumn col6Value = (TextColumn) getColumnWithMaxValue(COL_NAME6, DataType.TEXT);
+    BlobColumn col7Value = BlobColumn.of(COL_NAME7, createLargeBlob(32_766));
+    DateColumn col8Value = (DateColumn) getColumnWithMaxValue(COL_NAME8, DataType.DATE);
+    TimeColumn col9Value = (TimeColumn) getColumnWithMaxValue(COL_NAME9, DataType.TIME);
+    TimestampTZColumn col10Value =
+        (TimestampTZColumn) getColumnWithMaxValue(COL_NAME10, DataType.TIMESTAMPTZ);
+    TimestampColumn column11Value = null;
+    if (isTimestampTypeSupported()) {
+      column11Value = (TimestampColumn) getColumnWithMaxValue(COL_NAME11, DataType.TIMESTAMP);
+    }
+
+    PutBuilder.Buildable put =
+        Put.newBuilder()
+            .namespace(namespace)
+            .table(TABLE)
+            .partitionKey(Key.newBuilder().add(partitionKeyValue).build())
+            .value(col1Value)
+            .value(col2Value)
+            .value(col3Value)
+            .value(col4Value)
+            .value(col5Value)
+            .value(col6Value)
+            .value(col7Value)
+            .value(col8Value)
+            .value(col9Value)
+            .value(col10Value);
+    if (isTimestampTypeSupported()) {
+      put.value(column11Value);
+    }
+    // Act
+    storage.put(put.build());
+
+    // Assert
+    assertResult(
+        partitionKeyValue,
+        col1Value,
+        col2Value,
+        col3Value,
+        col4Value,
+        col5Value,
+        col6Value,
+        col7Value,
+        col8Value,
+        col9Value,
+        col10Value,
+        column11Value);
+  }
+
+  private byte[] createLargeBlob(int size) {
+    byte[] blob = new byte[size];
+    random.nextBytes(blob);
+    return blob;
+  }
+
+  protected Stream<Arguments> provideBlobSizes() {
+    return Stream.of(Arguments.of(100_000_000, "100 MB"));
   }
 
   protected void assertResult(
