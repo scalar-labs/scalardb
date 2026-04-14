5866a1d3eeab0951e893323d0a4500f6b0b09da7
Toshihiro Suzuki <brfrn169@gmail.com>
2026-04-14 14:42:31 +0900

    Add attributes parameter to TransactionExecutor execute/executeWithRetries methods

--- core/src/test/java/com/scalar/db/common/TransactionExecutorTest.java
+++ core/src/test/java/com/scalar/db/common/TransactionExecutorTest.java
@@ -13,6 +13,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.google.common.collect.ImmutableMap;
 import com.scalar.db.api.CrudOperable;
 import com.scalar.db.api.DistributedTransaction;
 import com.scalar.db.api.DistributedTransactionManager;
@@ -22,6 +23,8 @@ import com.scalar.db.exception.transaction.UnknownTransactionStatusException;
 import com.scalar.db.transaction.singlecrudoperation.SingleCrudOperationTransactionManager;
 import com.scalar.db.util.ThrowableConsumer;
 import com.scalar.db.util.ThrowableFunction;
+import java.util.Collections;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 
@@ -57,7 +60,7 @@ public class TransactionExecutorTest {
     DistributedTransaction transaction = mock(DistributedTransaction.class);
 
     DistributedTransactionManager transactionManager = mock(DistributedTransactionManager.class);
-    when(transactionManager.begin()).thenReturn(transaction);
+    when(transactionManager.begin(Collections.emptyMap())).thenReturn(transaction);
 
     int expected = 10;
 
@@ -72,7 +75,36 @@ public class TransactionExecutorTest {
 
     assertThat(result).isEqualTo(expected);
 
-    verify(transactionManager).begin();
+    verify(transactionManager).begin(Collections.emptyMap());
+    verify(transaction).commit();
+  }
+
+  @Test
+  public void
+      execute_ThrowableFunctionGivenWithAttributes_WithDistributedTransactionManager_ShouldExecuteFunctionWithAttributes()
+          throws TransactionException {
+    // Arrange
+    DistributedTransaction transaction = mock(DistributedTransaction.class);
+
+    DistributedTransactionManager transactionManager = mock(DistributedTransactionManager.class);
+    Map<String, String> attributes = ImmutableMap.of("key", "value");
+    when(transactionManager.begin(attributes)).thenReturn(transaction);
+
+    int expected = 10;
+
+    // Act Assert
+    Integer result =
+        TransactionExecutor.execute(
+            transactionManager,
+            attributes,
+            t -> {
+              assertThat(t).isEqualTo(transaction);
+              return expected;
+            });
+
+    assertThat(result).isEqualTo(expected);
+
+    verify(transactionManager).begin(attributes);
     verify(transaction).commit();
   }
 
@@ -84,7 +116,7 @@ public class TransactionExecutorTest {
     DistributedTransaction transaction = mock(DistributedTransaction.class);
 
     DistributedTransactionManager transactionManager = mock(DistributedTransactionManager.class);
-    when(transactionManager.begin()).thenReturn(transaction);
+    when(transactionManager.begin(Collections.emptyMap())).thenReturn(transaction);
 
     TransactionException exception = mock(TransactionException.class);
 
@@ -99,7 +131,7 @@ public class TransactionExecutorTest {
                         }))
         .isEqualTo(exception);
 
-    verify(transactionManager).begin();
+    verify(transactionManager).begin(Collections.emptyMap());
     verify(transaction).rollback();
   }
 
@@ -111,7 +143,7 @@ public class TransactionExecutorTest {
     DistributedTransaction transaction = mock(DistributedTransaction.class);
 
     DistributedTransactionManager transactionManager = mock(DistributedTransactionManager.class);
-    when(transactionManager.begin()).thenReturn(transaction);
+    when(transactionManager.begin(Collections.emptyMap())).thenReturn(transaction);
 
     UnknownTransactionStatusException exception = mock(UnknownTransactionStatusException.class);
 
@@ -126,7 +158,7 @@ public class TransactionExecutorTest {
                         }))
         .isEqualTo(exception);
 
-    verify(transactionManager).begin();
+    verify(transactionManager).begin(Collections.emptyMap());
     verify(transaction, never()).rollback();
   }
 
@@ -144,7 +176,10 @@ public class TransactionExecutorTest {
       mocked
           .when(
               () ->
-                  TransactionExecutor.execute(eq(transactionManager), any(ThrowableFunction.class)))
+                  TransactionExecutor.execute(
+                      any(DistributedTransactionManager.class),
+                      any(Map.class),
+                      any(ThrowableFunction.class)))
           .thenReturn(null);
 
       // Act
@@ -152,7 +187,43 @@ public class TransactionExecutorTest {
 
       // Assert
       mocked.verify(
-          () -> TransactionExecutor.execute(eq(transactionManager), any(ThrowableFunction.class)));
+          () ->
+              TransactionExecutor.execute(
+                  eq(transactionManager),
+                  eq(Collections.emptyMap()),
+                  any(ThrowableFunction.class)));
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void execute_ThrowableConsumerGivenWithAttributes_ShouldCallExecuteWithFunction()
+      throws TransactionException {
+    try (MockedStatic<TransactionExecutor> mocked =
+        mockStatic(TransactionExecutor.class, CALLS_REAL_METHODS)) {
+      // Arrange
+      DistributedTransactionManager transactionManager = mock(DistributedTransactionManager.class);
+      Map<String, String> attributes = ImmutableMap.of("key", "value");
+      ThrowableConsumer<CrudOperable<?>, TransactionException> throwableConsumer =
+          mock(ThrowableConsumer.class);
+
+      mocked
+          .when(
+              () ->
+                  TransactionExecutor.execute(
+                      any(DistributedTransactionManager.class),
+                      any(Map.class),
+                      any(ThrowableFunction.class)))
+          .thenReturn(null);
+
+      // Act
+      TransactionExecutor.execute(transactionManager, attributes, throwableConsumer);
+
+      // Assert
+      mocked.verify(
+          () ->
+              TransactionExecutor.execute(
+                  eq(transactionManager), eq(attributes), any(ThrowableFunction.class)));
     }
   }
 
@@ -170,14 +241,51 @@ public class TransactionExecutorTest {
       Object expected = new Object();
 
       mocked
-          .when(() -> TransactionExecutor.execute(transactionManager, throwableFunction))
+          .when(
+              () ->
+                  TransactionExecutor.execute(
+                      transactionManager, Collections.emptyMap(), throwableFunction))
           .thenReturn(expected);
 
       // Act
       Object actual = TransactionExecutor.executeWithRetries(transactionManager, throwableFunction);
 
       // Assert
-      mocked.verify(() -> TransactionExecutor.execute(transactionManager, throwableFunction));
+      mocked.verify(
+          () ->
+              TransactionExecutor.execute(
+                  transactionManager, Collections.emptyMap(), throwableFunction));
+
+      assertThat(actual).isEqualTo(expected);
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void executeWithRetries_ThrowableFunctionGivenWithAttributes_ShouldReturnResult()
+      throws TransactionException {
+    try (MockedStatic<TransactionExecutor> mocked =
+        mockStatic(TransactionExecutor.class, CALLS_REAL_METHODS)) {
+      // Arrange
+      DistributedTransactionManager transactionManager = mock(DistributedTransactionManager.class);
+      Map<String, String> attributes = ImmutableMap.of("key", "value");
+      ThrowableFunction<CrudOperable<?>, Object, TransactionException> throwableFunction =
+          mock(ThrowableFunction.class);
+
+      Object expected = new Object();
+
+      mocked
+          .when(
+              () -> TransactionExecutor.execute(transactionManager, attributes, throwableFunction))
+          .thenReturn(expected);
+
+      // Act
+      Object actual =
+          TransactionExecutor.executeWithRetries(transactionManager, attributes, throwableFunction);
+
+      // Assert
+      mocked.verify(
+          () -> TransactionExecutor.execute(transactionManager, attributes, throwableFunction));
 
       assertThat(actual).isEqualTo(expected);
     }
@@ -201,7 +309,10 @@ public class TransactionExecutorTest {
       Object expected = new Object();
 
       mocked
-          .when(() -> TransactionExecutor.execute(transactionManager, throwableFunction))
+          .when(
+              () ->
+                  TransactionExecutor.execute(
+                      transactionManager, Collections.emptyMap(), throwableFunction))
           .thenThrow(exception)
           .thenThrow(exception)
           .thenReturn(expected);
@@ -210,7 +321,10 @@ public class TransactionExecutorTest {
       Object actual = TransactionExecutor.executeWithRetries(transactionManager, throwableFunction);
 
       mocked.verify(
-          () -> TransactionExecutor.execute(transactionManager, throwableFunction), times(3));
+          () ->
+              TransactionExecutor.execute(
+                  transactionManager, Collections.emptyMap(), throwableFunction),
+          times(3));
 
       assertThat(actual).isEqualTo(expected);
     }
@@ -234,7 +348,10 @@ public class TransactionExecutorTest {
       Object expected = new Object();
 
       mocked
-          .when(() -> TransactionExecutor.execute(transactionManager, throwableFunction))
+          .when(
+              () ->
+                  TransactionExecutor.execute(
+                      transactionManager, Collections.emptyMap(), throwableFunction))
           .thenThrow(exception)
           .thenThrow(exception)
           .thenReturn(expected);
@@ -243,7 +360,10 @@ public class TransactionExecutorTest {
       Object actual = TransactionExecutor.executeWithRetries(transactionManager, throwableFunction);
 
       mocked.verify(
-          () -> TransactionExecutor.execute(transactionManager, throwableFunction), times(3));
+          () ->
+              TransactionExecutor.execute(
+                  transactionManager, Collections.emptyMap(), throwableFunction),
+          times(3));
 
       assertThat(actual).isEqualTo(expected);
     }
@@ -252,7 +372,134 @@ public class TransactionExecutorTest {
   @SuppressWarnings("unchecked")
   @Test
   public void
-      executeWithRetries_ThrowableConsumerGiven_CommitConflictExceptionThrownTwoTimesThenReturnResult_ShouldRetryAndReturnResult()
+      executeWithRetries_ThrowableFunctionGivenWithRetryParams_ShouldCallExecuteWithRetriesWithAttributes()
+          throws TransactionException {
+    try (MockedStatic<TransactionExecutor> mocked =
+        mockStatic(TransactionExecutor.class, CALLS_REAL_METHODS)) {
+      // Arrange
+      DistributedTransactionManager transactionManager = mock(DistributedTransactionManager.class);
+      ThrowableFunction<CrudOperable<?>, Object, TransactionException> throwableFunction =
+          mock(ThrowableFunction.class);
+
+      mocked
+          .when(
+              () ->
+                  TransactionExecutor.executeWithRetries(
+                      any(DistributedTransactionManager.class),
+                      any(Map.class),
+                      any(ThrowableFunction.class),
+                      anyInt(),
+                      anyInt(),
+                      anyInt(),
+                      anyInt()))
+          .thenReturn(null);
+
+      // Act
+      TransactionExecutor.executeWithRetries(
+          transactionManager, throwableFunction, 100, 1000, 2, 5);
+
+      // Assert
+      mocked.verify(
+          () ->
+              TransactionExecutor.executeWithRetries(
+                  eq(transactionManager),
+                  eq(Collections.emptyMap()),
+                  eq(throwableFunction),
+                  eq(100),
+                  eq(1000),
+                  eq(2),
+                  eq(5)));
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void executeWithRetries_ThrowableConsumerGiven_ShouldCallExecuteWithRetriesWithFunction()
+      throws TransactionException {
+    try (MockedStatic<TransactionExecutor> mocked =
+        mockStatic(TransactionExecutor.class, CALLS_REAL_METHODS)) {
+      // Arrange
+      DistributedTransactionManager transactionManager = mock(DistributedTransactionManager.class);
+      ThrowableConsumer<CrudOperable<?>, TransactionException> throwableConsumer =
+          mock(ThrowableConsumer.class);
+
+      mocked
+          .when(
+              () ->
+                  TransactionExecutor.executeWithRetries(
+                      any(DistributedTransactionManager.class),
+                      any(Map.class),
+                      any(ThrowableFunction.class),
+                      anyInt(),
+                      anyInt(),
+                      anyInt(),
+                      anyInt()))
+          .thenReturn(null);
+
+      // Act
+      TransactionExecutor.executeWithRetries(transactionManager, throwableConsumer);
+
+      // Assert
+      mocked.verify(
+          () ->
+              TransactionExecutor.executeWithRetries(
+                  eq(transactionManager),
+                  eq(Collections.emptyMap()),
+                  any(ThrowableFunction.class),
+                  eq(100),
+                  eq(1000),
+                  eq(2),
+                  eq(5)));
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void
+      executeWithRetries_ThrowableConsumerGivenWithAttributes_ShouldCallExecuteWithRetriesWithFunction()
+          throws TransactionException {
+    try (MockedStatic<TransactionExecutor> mocked =
+        mockStatic(TransactionExecutor.class, CALLS_REAL_METHODS)) {
+      // Arrange
+      DistributedTransactionManager transactionManager = mock(DistributedTransactionManager.class);
+      Map<String, String> attributes = ImmutableMap.of("key", "value");
+      ThrowableConsumer<CrudOperable<?>, TransactionException> throwableConsumer =
+          mock(ThrowableConsumer.class);
+
+      mocked
+          .when(
+              () ->
+                  TransactionExecutor.executeWithRetries(
+                      any(DistributedTransactionManager.class),
+                      any(Map.class),
+                      any(ThrowableFunction.class),
+                      anyInt(),
+                      anyInt(),
+                      anyInt(),
+                      anyInt()))
+          .thenReturn(null);
+
+      // Act
+      TransactionExecutor.executeWithRetries(transactionManager, attributes, throwableConsumer);
+
+      // Assert
+      mocked.verify(
+          () ->
+              TransactionExecutor.executeWithRetries(
+                  eq(transactionManager),
+                  eq(attributes),
+                  any(ThrowableFunction.class),
+                  eq(100),
+                  eq(1000),
+                  eq(2),
+                  eq(5)));
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void
+      executeWithRetries_ThrowableConsumerGivenWithRetryParams_ShouldCallExecuteWithRetriesWithAttributes()
           throws TransactionException {
     try (MockedStatic<TransactionExecutor> mocked =
         mockStatic(TransactionExecutor.class, CALLS_REAL_METHODS)) {
@@ -265,7 +512,8 @@ public class TransactionExecutorTest {
           .when(
               () ->
                   TransactionExecutor.executeWithRetries(
-                      eq(transactionManager),
+                      any(DistributedTransactionManager.class),
+                      any(Map.class),
                       any(ThrowableFunction.class),
                       anyInt(),
                       anyInt(),
@@ -273,18 +521,65 @@ public class TransactionExecutorTest {
                       anyInt()))
           .thenReturn(null);
 
-      // Act Assert
-      TransactionExecutor.executeWithRetries(transactionManager, throwableConsumer);
+      // Act
+      TransactionExecutor.executeWithRetries(
+          transactionManager, throwableConsumer, 100, 1000, 2, 5);
 
+      // Assert
       mocked.verify(
           () ->
               TransactionExecutor.executeWithRetries(
                   eq(transactionManager),
+                  eq(Collections.emptyMap()),
                   any(ThrowableFunction.class),
-                  anyInt(),
-                  anyInt(),
-                  anyInt(),
-                  anyInt()));
+                  eq(100),
+                  eq(1000),
+                  eq(2),
+                  eq(5)));
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void
+      executeWithRetries_ThrowableConsumerGivenWithAttributesAndRetryParams_ShouldCallExecuteWithRetriesWithFunction()
+          throws TransactionException {
+    try (MockedStatic<TransactionExecutor> mocked =
+        mockStatic(TransactionExecutor.class, CALLS_REAL_METHODS)) {
+      // Arrange
+      DistributedTransactionManager transactionManager = mock(DistributedTransactionManager.class);
+      Map<String, String> attributes = ImmutableMap.of("key", "value");
+      ThrowableConsumer<CrudOperable<?>, TransactionException> throwableConsumer =
+          mock(ThrowableConsumer.class);
+
+      mocked
+          .when(
+              () ->
+                  TransactionExecutor.executeWithRetries(
+                      any(DistributedTransactionManager.class),
+                      any(Map.class),
+                      any(ThrowableFunction.class),
+                      anyInt(),
+                      anyInt(),
+                      anyInt(),
+                      anyInt()))
+          .thenReturn(null);
+
+      // Act
+      TransactionExecutor.executeWithRetries(
+          transactionManager, attributes, throwableConsumer, 100, 1000, 2, 5);
+
+      // Assert
+      mocked.verify(
+          () ->
+              TransactionExecutor.executeWithRetries(
+                  eq(transactionManager),
+                  eq(attributes),
+                  any(ThrowableFunction.class),
+                  eq(100),
+                  eq(1000),
+                  eq(2),
+                  eq(5)));
     }
   }
 }
