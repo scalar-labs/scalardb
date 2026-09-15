7255672ee35bd684888ae6e919d88bd808c55091
Toshihiro Suzuki <brfrn169@gmail.com>
2026-09-16 00:07:40 +0900

    Fix rollback throwing IllegalArgumentException when a scanner is left open

--- core/src/main/java/com/scalar/db/transaction/consensuscommit/CrudHandler.java
+++ core/src/main/java/com/scalar/db/transaction/consensuscommit/CrudHandler.java
@@ -1109,6 +1109,15 @@ public class CrudHandler {
     }
 
     @Override
+    public void discard() {
+      if (closed.get()) {
+        return;
+      }
+
+      closeScanner();
+    }
+
+    @Override
     public boolean isClosed() {
       return closed.get();
     }
@@ -1187,8 +1196,17 @@ public class CrudHandler {
 
     @Override
     public void close() {
+      if (closed) {
+        return;
+      }
+
       closed = true;
       verifyNoOverlap(scan, results, exemptKeys, context);
+    }
+
+    @Override
+    public void discard() {
+      closed = true;
     }
 
     @Override
